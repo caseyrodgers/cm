57f41a9a1ee6243fae13accdd520798ce71f5e8e
Casey Rodgers <grodgers@gmail.com>
2009-05-06 00:33:30 +0000

    no message

--- src/main/java/hotmath/gwt/cm/client/CatchupMath.java
+++ src/main/java/hotmath/gwt/cm/client/CatchupMath.java
@@ -15,8 +15,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.catalina.startup.SetAllPropertiesRule;
-
 import com.extjs.gxt.ui.client.GXT;
 import com.extjs.gxt.ui.client.Registry;
 import com.extjs.gxt.ui.client.Style.LayoutRegion;
