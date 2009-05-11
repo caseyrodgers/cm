6ff72bee2e6be5274c775a0021585177936dd9c0
Bob Hall <rfhall3@yahoo.com>
2009-05-11 17:39:18 +0000

    initial commit

---
+++ src/main/java/hotmath/gwt/shared/client/model/UserInfoBase.java
@@ -0,0 +1,90 @@
+package hotmath.gwt.shared.client.model;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+/** Class to encapsulate basic user information
+ * 
+ * @author Bob
+ *
+ */
+public class UserInfoBase implements IsSerializable {
+
+	static protected UserInfoBase __instance;
+
+	int uid;
+	int runId;
+	boolean isAdmin;
+
+	String userName;
+
+    // is this user the owner or a viewer (guest/admin) ?
+	// If not the owner, then state info should not be updated
+	boolean isOwner;
+
+	// is the user active (true) or inactive (false)
+	boolean isActive;
+
+	static public UserInfoBase getInstance() {
+	    if (__instance == null) {
+	    	__instance = new UserInfoBase();
+	    }
+	    return __instance;
+	}
+	
+	public UserInfoBase() {
+	}
+		
+	/** Is this the active (owner) of current 
+	 * session, or is this a guest/viewer/admin?
+	 * 
+	 * @return
+	 */
+	public boolean isActiveUser() {
+        return isOwner;
+    }
+
+    public void setActiveUser(boolean activeUser) {
+        this.isOwner = activeUser;
+    }
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+	public int getUid() {
+		return uid;
+	}
+
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+
+	public int getRunId() {
+		return runId;
+	}
+
+	public void setRunId(int runId) {
+		this.runId = runId;
+	}
+
+
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+
+	public void setIsAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+}
