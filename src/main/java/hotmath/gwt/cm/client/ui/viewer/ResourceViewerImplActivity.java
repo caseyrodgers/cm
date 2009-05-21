ff761e89c004d6e7e6ea4b799cb069b1572029da
Casey Rodgers <grodgers@gmail.com>
2009-05-21 15:25:42 +0000

    no message

--- src/main/java/hotmath/gwt/cm/client/ui/viewer/ResourceViewerImplActivity.java
+++ src/main/java/hotmath/gwt/cm/client/ui/viewer/ResourceViewerImplActivity.java
@@ -3,18 +3,14 @@ package hotmath.gwt.cm.client.ui.viewer;
 import hotmath.gwt.cm.client.data.InmhItemData;
 import pl.rmalinowski.gwt2swf.client.ui.SWFWidget;
 
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 
 public class ResourceViewerImplActivity extends ResourceViewerContainer {
 	
 	
 	public Widget getResourcePanel(InmhItemData resource) {
-	    SWFWidget swfWidget = new SWFWidget(resource.getFile(),500,400);
+	    SWFWidget swfWidget = new SWFWidget(resource.getFile(),"500px","100%"); 
 	    swfWidget.setStyleName("activity-widget");
-	    Label title = new Label(resource.getTitle());
-	    title.setStyleName("resource-viewer-impl-video-title");
-	    add(title);
 		addResource(swfWidget,resource.getTitle());
 		return this;
 	}
