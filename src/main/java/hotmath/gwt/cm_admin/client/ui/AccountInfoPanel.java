38bc6570d429257039e1b785ba442d1147773fce
Bob Hall <rfhall3@yahoo.com>
2009-05-27 08:57:55 +0000

    add getAccountInfoRPC, add more fields to template

--- src/main/java/hotmath/gwt/cm_admin/client/ui/AccountInfoPanel.java
+++ src/main/java/hotmath/gwt/cm_admin/client/ui/AccountInfoPanel.java
@@ -1,12 +1,21 @@
 package hotmath.gwt.cm_admin.client.ui;
 
-import hotmath.gwt.cm_admin.client.model.AccountInfoModel;
+import java.util.List;
 
+import hotmath.gwt.cm_admin.client.CatchupMathAdmin;
+import hotmath.gwt.cm_admin.client.model.AccountInfoModel;
+import hotmath.gwt.cm_admin.client.model.CmAdminModel;
+import hotmath.gwt.cm_admin.client.model.StudentModel;
+import hotmath.gwt.cm_admin.client.service.RegistrationServiceAsync;
+
+import com.extjs.gxt.ui.client.Registry;
 import com.extjs.gxt.ui.client.core.XTemplate;
+import com.extjs.gxt.ui.client.store.ListStore;
 import com.extjs.gxt.ui.client.util.Util;
 import com.extjs.gxt.ui.client.widget.HorizontalPanel;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
 import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.HTML;
 
 public class AccountInfoPanel extends LayoutContainer {
@@ -14,20 +23,41 @@ public class AccountInfoPanel extends LayoutContainer {
 	private XTemplate template;
 	private HTML html;
 	private AccountInfoModel model;
+	private CmAdminModel cmAdminModel;
 	
-	public AccountInfoPanel() {
+	public AccountInfoPanel(CmAdminModel cmAdminMdl) {
+		
+		cmAdminModel = cmAdminMdl;
+		
+		System.out.println("AccountInfoPanel: admin uid: " + cmAdminMdl.getId());
 		
 		HorizontalPanel hp = new HorizontalPanel();
+		hp.setAutoWidth(true);
 		hp.setSpacing(10);
-				
+		hp.setBorders(false);
+		hp.setStyleName("account-info");
+		// left:640px;
+		// position:absolute;
+	
 		StringBuilder sb = new StringBuilder();  
-		sb.append("<div class=text style='line-height: 13px; font-size: 11px'>");  
-		sb.append("<b>School:</b> {school-name}");
-		sb.append("</div>");
-        template = XTemplate.create(sb.toString());  
+		sb.append("<div 'style='line-height: 20px; font-size: 13px; width: 800px'>");
+		
+		sb.append("<div style='float:right; width:280px>'");
+		sb.append("<p><b>Admin:</b> {admin-user-name}</p>");
+		sb.append("<p><b>Last login:</b> {last-login}</p>");
+		sb.append("</div");
+
+		sb.append("<div style='float: left; margin-right:20px; width:280px'>");
+		sb.append("<p><b>School:</b> {school-name}</p>");
+		sb.append("<p><b>Contact:</b> {school-user-name}</p>");
+		sb.append("<p><b>Status:</b> {status}</p>");
+		sb.append("<p><b>Expires:</b> {expiration-date}</p>");
+		sb.append("<p><b>Tutoring:</b> {has-tutoring}</p>");
+		sb.append("</div></div>");
+		
+		template = XTemplate.create(sb.toString());  
 		html = new HTML();
 		html.setHTML(sb.toString());
-		html.setWidth("200px");
 		hp.add(html);
 		
 		//TODO: add "Details" button or tool tip
@@ -37,12 +67,10 @@ public class AccountInfoPanel extends LayoutContainer {
 	
 	protected void onRender(Element parent, int index) {
 		super.onRender(parent, index);
+		
+		setStyleName("account-info");
 
-		//TODO: obtain account info from DB
-		AccountInfoModel model = new AccountInfoModel();
-		model.setSchoolName("Hotmath High");
-
-		setAccountInfoModel(model);
+		getAccountInfoRPC(cmAdminModel.getId());
 	}
 
 	public void setAccountInfoModel(AccountInfoModel model) {
@@ -50,4 +78,19 @@ public class AccountInfoPanel extends LayoutContainer {
 		template.overwrite(html.getElement(), Util.getJsObject(this.model));  
 	}
 
+	protected void getAccountInfoRPC(Integer uid) {
+		RegistrationServiceAsync s = (RegistrationServiceAsync) Registry.get("registrationService");
+		
+		s.getAccountInfoForAdminUid(uid, new AsyncCallback <AccountInfoModel>() {
+
+			public void onSuccess(AccountInfoModel result) {
+				setAccountInfoModel(result);
+        	}
+
+			public void onFailure(Throwable caught) {
+        		String msg = caught.getMessage();
+        		CatchupMathAdmin.showAlert(msg);
+        	}
+        });
+	}
 }
