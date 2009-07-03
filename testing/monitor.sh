af2aac4e0bb562c05f8535a2b7f12ad8b3d181e3
sujatha <sujatha>
2009-07-03 20:11:35 +0000

    *** empty log message ***

--- testing/monitor.sh
+++ testing/monitor.sh
@@ -1,4 +1,4 @@
 while true;do
-  wget http://hotmath.kattare.com:8081/resources/util/cm_system_status.jsp -O -;
-  sleep 60;
+  wget http://hotmath.kattare.com:8081/resources/util/cm_system_status.jsp -O - 2> monitor.out | grep status;
+  sleep 10;
 done;
