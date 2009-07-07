6a4d4a39357317ae18689e8a2b7dde6410c1dbc3
Casey Rodgers <grodgers@gmail.com>
2009-07-07 23:30:34 +0000

    *** empty log message ***

---
+++ src/main/java/hotmath/cm/Log4jInit.java
@@ -0,0 +1,29 @@
+package hotmath.cm;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class Log4jInit extends HttpServlet {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -771897979715468574L;
+
+    public void init() {
+        String prefix = getServletContext().getRealPath("/");
+        String file = getInitParameter("log4j-init-file");
+        // if the log4j-init-file is not set, then no point in trying
+        if (file != null) {
+            PropertyConfigurator.configure(prefix + file);
+            Logger.getLogger(this.getClass()).info("Catchup Log4J intialized");
+        }
+    }
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) {
+    }
+}
