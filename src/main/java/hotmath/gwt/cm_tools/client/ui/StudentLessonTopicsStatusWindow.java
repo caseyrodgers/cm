6e20e7eaa2c06b9ed69f6e13e2c1bcee0cc47789
Bob Hall <rfhall3@yahoo.com>
2009-07-18 08:46:38 +0000

    display Lesson Items (Covered Standards) in scrollable grid

--- src/main/java/hotmath/gwt/cm_tools/client/ui/StudentLessonTopicsStatusWindow.java
+++ src/main/java/hotmath/gwt/cm_tools/client/ui/StudentLessonTopicsStatusWindow.java
@@ -1,129 +1,131 @@
 package hotmath.gwt.cm_tools.client.ui;
 
 import hotmath.gwt.cm_tools.client.CatchupMathTools;
-import hotmath.gwt.cm_tools.client.data.InmhItemData;
-import hotmath.gwt.cm_tools.client.model.StudentActivityModel;
 import hotmath.gwt.cm_tools.client.model.LessonItemModel;
+import hotmath.gwt.cm_tools.client.model.StudentActivityModel;
 import hotmath.gwt.cm_tools.client.model.StudentModel;
-import hotmath.gwt.cm_tools.client.model.StudentShowWorkModel;
 import hotmath.gwt.cm_tools.client.service.PrescriptionServiceAsync;
 import hotmath.gwt.cm_tools.client.ui.CmWindow.CmWindow;
-import hotmath.gwt.cm_tools.client.ui.viewer.ResourceViewer;
-import hotmath.gwt.cm_tools.client.ui.viewer.ResourceViewerFactory;
-import hotmath.gwt.cm_tools.client.ui.viewer.ShowWorkPanel;
-import hotmath.gwt.shared.client.util.UserInfo;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import com.allen_sauer.gwt.log.client.Log;
 import com.extjs.gxt.ui.client.Registry;
-import com.extjs.gxt.ui.client.Style.LayoutRegion;
+import com.extjs.gxt.ui.client.Style.SelectionMode;
 import com.extjs.gxt.ui.client.event.ButtonEvent;
-import com.extjs.gxt.ui.client.event.ComponentEvent;
-import com.extjs.gxt.ui.client.event.Events;
-import com.extjs.gxt.ui.client.event.Listener;
 import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.store.ListStore;
-import com.extjs.gxt.ui.client.widget.Html;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
-import com.extjs.gxt.ui.client.widget.ListView;
 import com.extjs.gxt.ui.client.widget.button.Button;
+import com.extjs.gxt.ui.client.widget.grid.ColumnConfig;
+import com.extjs.gxt.ui.client.widget.grid.ColumnModel;
+import com.extjs.gxt.ui.client.widget.grid.Grid;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayout;
-import com.extjs.gxt.ui.client.widget.layout.BorderLayoutData;
-import com.extjs.gxt.ui.client.widget.layout.CenterLayout;
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
+
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Widget;
 
 /**
- * Display Student's Lesson Topic Status
+ * Display Student's Lesson Topic (Prescribed Standards) Status
  *
  * @author bob
  * 
  */
 public class StudentLessonTopicsStatusWindow extends CmWindow {
 
-    StudentModel student;
-    Integer runId;
-    String programName;
-    StudentActivityModel activityModel;
-    HTML html;
+    private StudentModel student;
+    private Integer runId;
+    private String programName;
+    private StudentActivityModel activityModel;
+    private Grid<LessonItemModel> limGrid;
+    private int width = 400;
+    private int height = 300; 
 
     public StudentLessonTopicsStatusWindow(StudentModel student, StudentActivityModel activityModel) {
+    	
         setStyleName("student-lesson-topic-status-window");
         this.student = student;
         this.activityModel = activityModel;
         runId = activityModel.getRunId();
-        setSize(400, 300);
+        setSize(width, height);
         setResizable(false);
         super.setModal(true);
         
         programName = activityModel.getProgramDescr();
 
         setLayout(new BorderLayout());
-        String title = "Lesson Topics for " + student.getName();
+        StringBuffer sb = new StringBuffer();
+        sb.append("For ").append(student.getName());
         if(programName != null)
-            title += " in program " + programName;
-        setHeading(title);
+            sb.append(" in program ").append(programName);
+        setHeading(sb.toString());
         
-        html = new HTML();
-        html.setHeight("70px");
-        add(html);
+        ListStore<LessonItemModel> store = new ListStore<LessonItemModel>();
+        ColumnModel cm = defineColumns();
+        
+        limGrid = new Grid<LessonItemModel>(store, cm);
+        limGrid.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        limGrid.getSelectionModel().setFiresEvents(false);
+        limGrid.setStripeRows(true);
+        limGrid.setWidth(width - 20);
+        limGrid.setHeight(height - 70);
 
+        LayoutContainer cp = new LayoutContainer();
+        cp.setLayout(new FitLayout());
+        cp.add(limGrid);
+        add(cp);
+        
         Button closeBtn = new Button("Close");
         closeBtn.addSelectionListener(new SelectionListener<ButtonEvent>() {
             public void componentSelected(ButtonEvent ce) {
                 close();
             }
         });
+        closeBtn.setIconStyle("icon-delete");
 
         addButton(closeBtn);
         setVisible(false);
         
-        this.getStudentLessonTopicsRPC(new ListStore<LessonItemModel>());
-
-    }
-
-    private void formatLessonTopics(List<LessonItemModel> l){
-        StringBuilder sb = new StringBuilder();
-        sb.append("<div style='padding-left: 10px; padding-top: 10px; padding-bottom: 10px'>");
-        int height = 0;
-        int row = 0;
-        for (LessonItemModel item : l) {
-        	if ((row%2) != 0) {
-        		sb.append("<div style='background-color: #FAFAFA;'>");
-        	}
-        	else {
-        		sb.append("<div style='border-color: #FFFFFF #FDFDFD #FDFDFD;'>");
-        	}
-            sb.append("<div style='width: 20px; float: right;'>").append((item.isCompleted()?"OK":"&nbsp;")).append("</div>");
-            sb.append("<div>").append(item.getName()).append("</div>").append("</div>");
-            height += 15;
-            row++;
-        }
-        sb.append("</div>");
-
-        setSize(400, height + 60);
-
-        html.setHeight(String.valueOf(height)+"px");
-        html.setHTML(sb.toString());
+        this.getStudentLessonTopicsRPC(store);
     }
     
+    private ColumnModel defineColumns() {
+        List<ColumnConfig> configs = new ArrayList<ColumnConfig>();
+
+        ColumnConfig topicName = new ColumnConfig();
+        topicName.setId(LessonItemModel.NAME_KEY);  
+        topicName.setHeader("Standards Covered This Section");  
+        topicName.setWidth(240);
+        topicName.setSortable(true);
+        topicName.setMenuDisabled(true);
+        configs.add(topicName);
+        
+        ColumnConfig prescribed = new ColumnConfig();
+        prescribed.setId(LessonItemModel.PRESCRIBED_KEY);  
+        prescribed.setHeader("Prescribed for Review");  
+        prescribed.setWidth(130);
+        prescribed.setSortable(false);
+        prescribed.setMenuDisabled(true);
+        configs.add(prescribed);       
+
+        ColumnModel cm = new ColumnModel(configs);
+        return cm;
+    }
+
     protected void getStudentLessonTopicsRPC(final ListStore <LessonItemModel> store) {
         PrescriptionServiceAsync s = (PrescriptionServiceAsync) Registry.get("prescriptionService");
         s.getLessonItemsForTestRun(runId, new AsyncCallback <List<LessonItemModel>>() {
 
-        public void onSuccess(List<LessonItemModel> list) {
-            store.add(list);
-            formatLessonTopics(list);
-            setVisible(true);
-        }
+            public void onSuccess(List<LessonItemModel> list) {
+            	System.out.println("list size: " + list.size());
+                store.add(list);
+                setVisible(true);
+            }
 
-        public void onFailure(Throwable caught) {
-            String msg = caught.getMessage();
-            CatchupMathTools.showAlert(msg);
-        }
+            public void onFailure(Throwable caught) {
+                String msg = caught.getMessage();
+                CatchupMathTools.showAlert(msg);
+            }
         });
     }    
 }
