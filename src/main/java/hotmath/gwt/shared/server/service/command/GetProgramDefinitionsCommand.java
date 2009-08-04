8e22e930f9b3cdf3d638c593673d4cdc22b2739e
Bob Hall <rfhall3@yahoo.com>
2009-08-04 07:39:48 +0000

    CmArrayList --> CmList

--- src/main/java/hotmath/gwt/shared/server/service/command/GetProgramDefinitionsCommand.java
+++ src/main/java/hotmath/gwt/shared/server/service/command/GetProgramDefinitionsCommand.java
@@ -4,6 +4,7 @@ import hotmath.gwt.cm_admin.server.model.CmAdminDao;
 import hotmath.gwt.cm_tools.client.model.SubjectModel;
 import hotmath.gwt.shared.client.rpc.Action;
 import hotmath.gwt.shared.client.rpc.Response;
+import hotmath.gwt.shared.client.rpc.action.CmList;
 import hotmath.gwt.shared.client.rpc.action.CmArrayList;
 import hotmath.gwt.shared.client.rpc.action.GetProgramDefinitionsAction;
 import hotmath.gwt.shared.server.service.ActionHandler;
@@ -15,17 +16,17 @@ import java.util.ArrayList;
 import org.apache.log4j.Logger;
 
 
-public class GetProgramDefinitionsCommand implements ActionHandlerManualConnectionManagement, ActionHandler<GetProgramDefinitionsAction,  CmArrayList<SubjectModel>> {
+public class GetProgramDefinitionsCommand implements ActionHandlerManualConnectionManagement, ActionHandler<GetProgramDefinitionsAction,  CmList<SubjectModel>> {
 
     
     Logger logger = Logger.getLogger(GetProgramDefinitionsCommand.class);
 
     @Override
-    public CmArrayList<SubjectModel> execute(final Connection conn, GetProgramDefinitionsAction action) throws Exception {
-        CmArrayList<SubjectModel> cma = new CmArrayList<SubjectModel>();
+    public CmList<SubjectModel> execute(final Connection conn, GetProgramDefinitionsAction action) throws Exception {
+        CmList<SubjectModel> cml = new CmArrayList<SubjectModel>();
         CmAdminDao dao = new CmAdminDao();
-        ((ArrayList<SubjectModel>)cma).addAll(dao.getSubjectDefinitions(action.getProgId()));
-        return cma;
+        cml.addAll(dao.getSubjectDefinitions(action.getProgId()));
+        return cml;
     }
     
     @Override
