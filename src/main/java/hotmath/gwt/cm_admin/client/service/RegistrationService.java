5995d7ebd2675c9c23e08e997f220b044b7b528c
Bob Hall <rfhall3@yahoo.com>
2009-08-11 08:03:36 +0000

    remove getPrintableStudentReportId(), use CmService and GetReportDefCommand

--- src/main/java/hotmath/gwt/cm_admin/client/service/RegistrationService.java
+++ src/main/java/hotmath/gwt/cm_admin/client/service/RegistrationService.java
@@ -9,14 +9,11 @@ import com.google.gwt.user.client.rpc.RemoteService;
 
 public interface RegistrationService extends RemoteService {
 
-	  List<StudentModel> getSummariesForActiveStudents(Integer adminIUid) throws Exception; 
+    List<StudentModel> getSummariesForActiveStudents(Integer adminIUid) throws Exception; 
 
-	  List<StudentModel> getSummariesForInactiveStudents(Integer adminUid) throws Exception;
+    List<StudentModel> getSummariesForInactiveStudents(Integer adminUid) throws Exception;
 
-	  StudentModel deactivateUser(StudentModel sm) throws Exception;
+    StudentModel deactivateUser(StudentModel sm) throws Exception;
 
-	  AccountInfoModel getAccountInfoForAdminUid(Integer uid) throws Exception;
-	  
-	  String getPrintableStudentReportId(List<Integer> studentUids) throws Exception;
-
+    AccountInfoModel getAccountInfoForAdminUid(Integer uid) throws Exception;
 }
