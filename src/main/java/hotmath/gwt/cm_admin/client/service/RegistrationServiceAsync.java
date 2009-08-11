d50528eb551d8192a2232687aaa52e1a3a0ceddb
Bob Hall <rfhall3@yahoo.com>
2009-08-11 08:02:22 +0000

    remove getPrintableStudentReportId(), use CmServiceAsync and GetReportDefCommand

--- src/main/java/hotmath/gwt/cm_admin/client/service/RegistrationServiceAsync.java
+++ src/main/java/hotmath/gwt/cm_admin/client/service/RegistrationServiceAsync.java
@@ -8,14 +8,12 @@ import java.util.List;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface RegistrationServiceAsync {
-	
-      void getSummariesForActiveStudents(Integer adminUid, AsyncCallback<List<StudentModel>> callback);
-	  
-	  void getSummariesForInactiveStudents(Integer adminUid, AsyncCallback<List<StudentModel>> callback);
 
-	  void deactivateUser(StudentModel sm, AsyncCallback<StudentModel> callback);
+    void getSummariesForActiveStudents(Integer adminUid, AsyncCallback<List<StudentModel>> callback);
 
-	  void getAccountInfoForAdminUid(Integer uid, AsyncCallback<AccountInfoModel> callback);
-	  
-	  void getPrintableStudentReportId(List<Integer> studentUids, AsyncCallback<String> callback);
+	void getSummariesForInactiveStudents(Integer adminUid, AsyncCallback<List<StudentModel>> callback);
+
+    void deactivateUser(StudentModel sm, AsyncCallback<StudentModel> callback);
+
+    void getAccountInfoForAdminUid(Integer uid, AsyncCallback<AccountInfoModel> callback);
 }
