ce1729ad61db1fd042950694cfdd6ee4dbb21656
Bob Hall <rfhall3@yahoo.com>
2009-08-11 05:42:13 +0000

    wrap ext js BaseModelData

---
+++ src/main/java/hotmath/gwt/cm_tools/client/model/BaseModel.java
@@ -0,0 +1,8 @@
+package hotmath.gwt.cm_tools.client.model;
+
+import com.extjs.gxt.ui.client.data.BaseModelData;
+
+public class BaseModel extends BaseModelData {
+
+	private static final long serialVersionUID = -8370354125432687765L;
+}
