86119c3680d6179fe0c1a125024a17a8b95486d4
Bob Hall <rfhall3@yahoo.com>
2009-08-19 07:22:09 +0000

    california --> California

--- src/main/java/hotmath/gwt/cm_tools/client/ui/StudentLessonTopicsStatusWindow.java
+++ src/main/java/hotmath/gwt/cm_tools/client/ui/StudentLessonTopicsStatusWindow.java
@@ -77,7 +77,7 @@ public class StudentLessonTopicsStatusWindow extends CmWindow {
         add(cp);
 
         Button stateStandardsBtn = new Button("California Standards");
-        stateStandardsBtn.setToolTip("Show corresponding california state standards");
+        stateStandardsBtn.setToolTip("Show corresponding California state standards");
         stateStandardsBtn.addSelectionListener(new SelectionListener<ButtonEvent>() {
             public void componentSelected(ButtonEvent ce) {
                 LessonItemModel lim = limGrid.getSelectionModel().getSelectedItem();
