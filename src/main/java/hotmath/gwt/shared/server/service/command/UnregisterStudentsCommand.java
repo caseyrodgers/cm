a020d8e3696e155a506a5e204c2e29b2a8f3aa36
Bob Hall <rfhall3@yahoo.com>
2009-08-21 06:19:47 +0000

    initial commit - Unregister Students Command

---
+++ src/main/java/hotmath/gwt/shared/server/service/command/UnregisterStudentsCommand.java
@@ -0,0 +1,26 @@
+package hotmath.gwt.shared.server.service.command;
+
+import hotmath.gwt.cm_admin.server.model.CmStudentDao;
+import hotmath.gwt.cm_tools.client.model.StringHolder;
+import hotmath.gwt.shared.client.rpc.Action;
+import hotmath.gwt.shared.client.rpc.Response;
+import hotmath.gwt.shared.client.rpc.action.UnregisterStudentsAction;
+import hotmath.gwt.shared.server.service.ActionHandler;
+
+import java.sql.Connection;
+
+public class UnregisterStudentsCommand implements ActionHandler<UnregisterStudentsAction, StringHolder> {
+
+    @Override
+    public StringHolder execute(Connection conn, UnregisterStudentsAction action) throws Exception {
+        CmStudentDao dao = new CmStudentDao();
+        System.out.println("+++ execute(): action.getStudentList().size(): " + action.getStudentList().size());
+
+        return dao.unregisterStudents(conn, action.getStudentList());
+    }
+
+    @Override
+    public Class<? extends Action<? extends Response>> getActionType() {
+        return UnregisterStudentsAction.class;
+    }
+}
