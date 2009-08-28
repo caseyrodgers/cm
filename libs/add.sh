01401f6971c99c48ac2542245e53bbf52c3fa4fd
Bob Hall <rfhall3@yahoo.com>
2009-08-28 07:10:38 +0000

    add  -DgeneratePom=true to; avoids repeated download attempts with every build

--- libs/add.sh
+++ libs/add.sh
@@ -1,18 +1,14 @@
-mvn install:install-file -DgroupId=com.extjs.gxt -DartifactId=gxt -Dversion=2.0 -Dpackaging=jar -Dfile=gxt-2.0.1.jar
-mvn install:install-file -DgroupId=sb -DartifactId=sblib -Dversion=2.0 -Dpackaging=jar -Dfile=sblib.jar
-mvn install:install-file -DgroupId=htmlparser -DartifactId=htmlparser -Dversion=1.6 -Dpackaging=jar -Dfile=htmlparser.jar
-mvn install:install-file -DgroupId=org.json -DartifactId=json -Dversion=1.0 -Dpackaging=jar -Dfile=json-1.0.jar
-mvn install:install-file -DgroupId=sbserver -DartifactId=sbserver -Dversion=2.0 -Dpackaging=jar -Dfile=sqlkitchen_server.jar
-mvn install:install-file -DgroupId=gwt2swf -DartifactId=gwt2swf -Dversion=0.6.0 -Dpackaging=jar -Dfile=gwt2swf-0.6.0.jar
-mvn install:install-file -DgroupId=jade -DartifactId=jade -Dversion=1.0 -Dpackaging=jar -Dfile=jade.jar
-mvn install:install-file -DgroupId=gwt-log -DartifactId=gwt-log -Dversion=2.6.0 -Dpackaging=jar -Dfile=gwt-log-2.6.2.jar 
-mvn install:install-file -DgroupId=gwt-incubator -DartifactId=gwt-incubator -Dversion=1.6.1539 -Dpackaging=jar -Dfile=gwt-incubator-march-02-2009.jar
-mvn install:install-file -DgroupId=itext -DartifactId=iText -Dversion=2.1.6 -Dpackaging=jar -Dfile=iText-2.1.6.jar
-mvn install:install-file -DgroupId=gwt-debug-panel -DartifactId=gwt-debug-panel -Dversion=1.0 -Dpackaging=jar -Dfile=gwt-debug-panel-1.0.jar
-mvn install:install-file -DgroupId=xmlrpc -DartifactId=xmlrpc -Dversion=3.1.2 -Dpackaging=jar -Dfile=xmlrpc-client-3.1.2.jar
-mvn install:install-file -DgroupId=paypal -DartifactId=paypal_api_combined -Dversion=1.0 -Dpackaging=jar -Dfile=paypal_api_combined.jar 
-mvn install:install-file -DgroupId=hotmath -DartifactId=hotmath -Dversion=2.0 -Dpackaging=jar -Dfile=hotmath2.jar
-
-
-
-
+mvn install:install-file -DgroupId=com.extjs.gxt -DartifactId=gxt -Dversion=2.0 -Dpackaging=jar -Dfile=gxt-2.0.1.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=sb -DartifactId=sblib -Dversion=2.0 -Dpackaging=jar -Dfile=sblib.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=htmlparser -DartifactId=htmlparser -Dversion=1.6 -Dpackaging=jar -Dfile=htmlparser.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=org.json -DartifactId=json -Dversion=1.0 -Dpackaging=jar -Dfile=json-1.0.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=sbserver -DartifactId=sbserver -Dversion=2.0 -Dpackaging=jar -Dfile=sqlkitchen_server.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=gwt2swf -DartifactId=gwt2swf -Dversion=0.6.0 -Dpackaging=jar -Dfile=gwt2swf-0.6.0.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=jade -DartifactId=jade -Dversion=1.0 -Dpackaging=jar -Dfile=jade.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=gwt-log -DartifactId=gwt-log -Dversion=2.6.0 -Dpackaging=jar -Dfile=gwt-log-2.6.2.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=gwt-incubator -DartifactId=gwt-incubator -Dversion=1.6.1539 -Dpackaging=jar -Dfile=gwt-incubator-march-02-2009.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=itext -DartifactId=iText -Dversion=2.1.6 -Dpackaging=jar -Dfile=iText-2.1.6.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=gwt-debug-panel -DartifactId=gwt-debug-panel -Dversion=1.0 -Dpackaging=jar -Dfile=gwt-debug-panel-1.0.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=xmlrpc -DartifactId=xmlrpc -Dversion=3.1.2 -Dpackaging=jar -Dfile=xmlrpc-client-3.1.2.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=paypal -DartifactId=paypal_api_combined -Dversion=1.0 -Dpackaging=jar -Dfile=paypal_api_combined.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=hotmath -DartifactId=hotmath -Dversion=2.0 -Dpackaging=jar -Dfile=hotmath2.jar -DgeneratePom=true
