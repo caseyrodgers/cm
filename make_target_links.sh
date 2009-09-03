3a19f26be4c002ac2a61966d51e9a13ffe6a5382
sujatha <sujatha>
2009-09-03 18:27:50 +0000

    *** empty log message ***

---
+++ make_target_links.sh
@@ -0,0 +1,6 @@
+ln -s ~/work/hotmath2/web/js target/cm-1.0-SNAPSHOT
+ln -s ~/work/hotmath2/web/css target/cm-1.0-SNAPSHOT
+ln -s ~/work/hotmath2/web/tutor target/cm-1.0-SNAPSHOT
+ln -s ~/work/hotmath2/web/images target/cm-1.0-SNAPSHOT
+ln -s ~/work/hotmath2/web/help target/cm-1.0-SNAPSHOT
+
