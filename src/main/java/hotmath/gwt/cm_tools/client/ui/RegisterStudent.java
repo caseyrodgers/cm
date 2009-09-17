701f1e1f778014d996bf05ad2fd4cc987f548b42
Casey Rodgers <grodgers@gmail.com>
2009-09-17 21:48:48 +0000

    *** empty log message ***

--- src/main/java/hotmath/gwt/cm_tools/client/ui/RegisterStudent.java
+++ src/main/java/hotmath/gwt/cm_tools/client/ui/RegisterStudent.java
@@ -302,7 +302,6 @@ public class RegisterStudent extends LayoutContainer implements ProcessTracker {
 	        @SuppressWarnings("unchecked")
 			public void selectionChanged(SelectionChangedEvent<StudyProgram> se) {
 	        	
-	            CatchupMathTools.showAlert("Program selected: loading=" + loading);
 	        	if (loading) return;
 	        	
 	            StudyProgram sp = se.getSelectedItem();
