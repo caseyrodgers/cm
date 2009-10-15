dc01d15d5e4e3da247b2932d2df6cf6fadaaf3f9
Casey Rodgers <grodgers@gmail.com>
2009-10-15 02:28:52 +0000

    *** empty log message ***

--- src/main/java/hotmath/gwt/cm/client/WelcomePanel.java
+++ src/main/java/hotmath/gwt/cm/client/WelcomePanel.java
@@ -3,16 +3,23 @@ package hotmath.gwt.cm.client;
 import hotmath.gwt.cm.client.history.CatchupMathHistoryListener;
 import hotmath.gwt.cm_tools.client.CatchupMathTools;
 
+import com.extjs.gxt.ui.client.Style.LayoutRegion;
 import com.extjs.gxt.ui.client.event.ButtonEvent;
 import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
 import com.extjs.gxt.ui.client.widget.button.Button;
+import com.extjs.gxt.ui.client.widget.layout.BorderLayout;
+import com.extjs.gxt.ui.client.widget.layout.BorderLayoutData;
 import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.ui.Frame;
 import com.google.gwt.user.client.ui.Label;
 
 public class WelcomePanel extends LayoutContainer {
     
     public WelcomePanel() {
+        setLayout(new BorderLayout());
+        Frame f = new Frame("/gwt-resources/first-time-visitor.html");
+        add(f, new BorderLayoutData(LayoutRegion.CENTER,200));
         
         add(new Label("Welcome to Catchup Math"));
         Button go = new Button("Begin Catchup Math");
@@ -22,7 +29,7 @@ public class WelcomePanel extends LayoutContainer {
             }
         });
         
-        add(go);
+        add(go, new BorderLayoutData(LayoutRegion.SOUTH,30));
     }
     
     private void startup() {
