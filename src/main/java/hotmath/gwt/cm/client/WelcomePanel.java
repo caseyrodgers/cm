7939ce6ed4ddb89c0634662e72ddfeb0297cfc67
Casey Rodgers <grodgers@gmail.com>
2009-10-20 15:59:25 +0000

    updated the welcome sequence

--- src/main/java/hotmath/gwt/cm/client/WelcomePanel.java
+++ src/main/java/hotmath/gwt/cm/client/WelcomePanel.java
@@ -1,9 +1,7 @@
 package hotmath.gwt.cm.client;
 
-import hotmath.gwt.cm.client.history.CatchupMathHistoryListener;
 import hotmath.gwt.cm_tools.client.CatchupMathTools;
 import hotmath.gwt.shared.client.CmShared;
-import hotmath.gwt.shared.client.util.ReportCardInfoPanel;
 import hotmath.gwt.shared.client.util.UserInfo;
 
 import com.extjs.gxt.ui.client.Style.LayoutRegion;
@@ -16,11 +14,6 @@ import com.extjs.gxt.ui.client.widget.LayoutContainer;
 import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayoutData;
 import com.extjs.gxt.ui.client.widget.layout.CenterLayout;
-import com.extjs.gxt.ui.client.widget.layout.FitLayout;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.History;
-import com.google.gwt.user.client.ui.Anchor;
 
 public class WelcomePanel extends LayoutContainer {
     
@@ -40,12 +33,12 @@ public class WelcomePanel extends LayoutContainer {
             main.add(new SampleSessionInfo(), new BorderLayoutData(LayoutRegion.CENTER));
         }
         else if(CmShared.getQueryParameterValue("type").equals("2") || UserInfo.getInstance().getViewCount() == 0) {
-            main.setSize(440,250);
+            main.setSize(330,150);
             main.add(new FirstTimeInfo(), new BorderLayoutData(LayoutRegion.CENTER));
         }
         else {
-            main.setSize(350,200);
-            main.add(new ReportCardInfoPanel(UserInfo.getInstance()), new BorderLayoutData(LayoutRegion.CENTER,200));
+            main.setSize(350,150);
+            main.add(new ReturnUserInfo(), new BorderLayoutData(LayoutRegion.CENTER,200));
         }
         
         _goBtn = new Button("Begin Catchup Math");
@@ -70,6 +63,11 @@ public class WelcomePanel extends LayoutContainer {
         }
     }
 
+    /** Info for users taking the sample session
+     * 
+     * @author casey
+     *
+     */
     class SampleSessionInfo extends Html {
         
         public SampleSessionInfo() {
@@ -78,62 +76,42 @@ public class WelcomePanel extends LayoutContainer {
                           "<p>The button below begins a sample session with Catchup Math. " + 
                           "You will start with a 10-question quiz. Then, you will experience " +
                           " the review and practice of Catchup Math (be sure to get at least " + 
-                          " one quiz problem wrong! " +
+                          " one quiz problem wrong!) " +
                           "</p>";
             
             setHtml(html);
         }
     }
     
-    class FirstTimeInfo extends LayoutContainer {
-        String _html1, _html2;
+    /** Info for returning users
+     * 
+     * @author casey
+     *
+     */
+    class ReturnUserInfo extends Html {
+        
+        public ReturnUserInfo() {
+        
+            String html = "<p>Use pencil and paper to work out the quiz problems.<br/>" +
+                          "Then, you will start your personalized review and practice." +
+                          "</p>";
+            setHtml(html);
+        }
+    }
+    
+    
+    /** Info for user's first visit to CM 
+     * 
+     * @author casey
+     *
+     */
+    class FirstTimeInfo extends Html {
         public FirstTimeInfo() {
+            String html = "<p>You start with one or more quizzes.</br>" +
+                          "Then, you will begin your review and practice!</p>";
             
-            setLayout(new FitLayout());
-            _html1 =      "<h1>First time user information</h1> " +
-                          "<ul> " +
-                          "<li>Take quizzes to find out what you are ready to learn next.</li> " +
-                          "<li>Then, go through the menu items in any order you like.</li> " +
-                          "<li>You'll learn fastest if you try the problems on your own.</li> " +
-                          "<li>Use paper and pencil to work out your answers.</li> " +
-                          "</ul> ";
-            _html2 =      "<h1>First time user information, page 2</h1> " +
-                          "<ul>" +
-                          "<li>You can change your wallpaper using [Help].</li> " +
-                          "<li>You can see your historical progress on [Help].</li> " +
-                          "<li>Use the Flash Cards if you find them helpful.</li> " +
-                          "<li>We hope you like Catchup Math!</li> " +
-                          "</ul>";
-            
-            showPageOne();
+            setHtml(html);
         }
-        
-        private void showPageOne() {
-            removeAll();
-            add(new Html(_html1));
-            Anchor btn = new Anchor("More Info >>");
-            btn.addClickHandler(new ClickHandler() {
-                public void onClick(ClickEvent event) {
-                    showPageTwo();
-                }
-            });
-            add(btn);
-            layout();
-        }
-        
-        private void showPageTwo() {
-            removeAll();
-            add(new Html(_html2));
-            Anchor btn = new Anchor("<< Back");
-            btn.addClickHandler(new ClickHandler() {
-                public void onClick(ClickEvent event) {
-                    showPageOne();
-                }
-            });
-            add(btn);
-            layout();
-        }
-        
     }
 }
 
--- src/main/java/hotmath/gwt/cm/client/ui/context/ContextChangeMessage.java
+++ src/main/java/hotmath/gwt/cm/client/ui/context/ContextChangeMessage.java
@@ -2,12 +2,17 @@ package hotmath.gwt.cm.client.ui.context;
 
 import hotmath.gwt.cm_tools.client.ui.CmWindow.CmWindow;
 
-import com.extjs.gxt.ui.client.Style.HorizontalAlignment;
+import com.extjs.gxt.ui.client.Style.Direction;
 import com.extjs.gxt.ui.client.event.ButtonEvent;
 import com.extjs.gxt.ui.client.event.SelectionListener;
+import com.extjs.gxt.ui.client.fx.FxConfig;
 import com.extjs.gxt.ui.client.widget.Html;
+import com.extjs.gxt.ui.client.widget.LayoutContainer;
 import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Anchor;
 
 public class ContextChangeMessage extends CmWindow {
     
@@ -16,11 +21,29 @@ public class ContextChangeMessage extends CmWindow {
         setHeading("Current Topic");
         setModal(true);
         setSize(300,140);
+        
+        LayoutContainer lc = new LayoutContainer();
         String html = "<p>Your topic for review and practice is now:</p>" +
                       "<b>" + lesson + "</b>";
 
         setLayout(new FitLayout());
-        add(new Html(html));
+        lc.add(new Html(html));
+        
+        Anchor a = new Anchor("Suggestions>>");
+        a.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                showSuggestions();
+            }
+        });
+        
+        LayoutContainer lca = new LayoutContainer();
+        lca.addStyleName("suggest-anchor");
+        lca.add(a);
+       
+        lc.add(lca);
+        add(lc);
+        
+        
         Button close = new Button("Close");
         close.addSelectionListener(new SelectionListener<ButtonEvent>() {
             
@@ -31,10 +54,35 @@ public class ContextChangeMessage extends CmWindow {
             }
         });
         addButton(close);
-        getButtonBar().setAlignment(HorizontalAlignment.RIGHT);
         
         
         setVisible(true);
+    }
+    
+    private void showSuggestions() {
+        final CmWindow w = new CmWindow();
+        w.setSize(300,150);
+        w.setStyleName("context-change-message-suggestions");
+        w.setModal(true);
+        w.setHeading("Suggestions");
+        
+        String html = "<ul>" +
+                      "<li>The Help button has neat features</li> " +
+                      "<li>Check for new Flash Cards and Games</li> " +
+                      "<li>Use our our whiteboard to work the problems</li>" +
+                      "</ul>";
+        w.add(new Html(html));
+        
+        Button close = new Button("Close");
+        close.addSelectionListener(new SelectionListener<ButtonEvent>() {
+            
+            @Override
+            public void componentSelected(ButtonEvent ce) {
+                w.close();
+            }
+        });
+        w.addButton(close);
+        w.setVisible(true);
     }
 
 }
