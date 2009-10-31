6f679bf4c8335f1012284d8e4c3001df2180962d
Casey Rodgers <grodgers@gmail.com>
2009-10-31 01:29:28 +0000

    *** empty log message ***

---
+++ src/main/java/hotmath/cm/CmInitialzation.java
@@ -0,0 +1,40 @@
+package hotmath.cm;
+
+import hotmath.cm.util.CmWebResourceManager;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+/** Provides hooks to initialize the Catchup Math 
+ *  system when pluggined into a servlet environment.
+ *  
+ * @author casey
+ *
+ */
+public class CmInitialzation extends HttpServlet {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -771897979715468574L;
+
+    public void init() {
+        String prefix = getServletContext().getRealPath("/");
+        String file = getInitParameter("log4j-init-file");
+        // if the log4j-init-file is not set, then no point in trying
+        if (file != null) {
+            PropertyConfigurator.configure(prefix + file);
+            Logger.getLogger(this.getClass()) .info("Catchup Log4J intialized");
+        }
+        
+        
+        CmWebResourceManager.setFileBase(getServletContext().getRealPath("temp"));
+    }
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) {
+    }
+}
