30ff081047673a9f6c7d3c3438bb3f7d9e15f079
Casey Rodgers <grodgers@gmail.com>
2009-11-09 05:29:10 +0000

    port CM to gxt 2.0.4 - use addStyleName instead of setStyleName which overrides GXT default settings

--- src/main/java/hotmath/gwt/cm/client/ui/HelpWindow.java
+++ src/main/java/hotmath/gwt/cm/client/ui/HelpWindow.java
@@ -35,21 +35,18 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Label;
 
-
 public class HelpWindow extends CmWindow {
 
-    public  HelpWindow() {
+    public HelpWindow() {
         setAutoHeight(true);
         setWidth(490);
         setModal(true);
         setResizable(false);
         setStyleName("help-window");
         setHeading("Catchup-Math Help Window");
-        
-        
-        EventBus.getInstance().fireEvent(new CmEvent(EventBus.EVENT_TYPE_MODAL_WINDOW_OPEN,this));
-        
-        
+
+        EventBus.getInstance().fireEvent(new CmEvent(EventBus.EVENT_TYPE_MODAL_WINDOW_OPEN, this));
+
         Button closeBtn = new Button("Close");
         closeBtn.addSelectionListener(new SelectionListener<ButtonEvent>() {
             public void componentSelected(ButtonEvent ce) {
@@ -57,76 +54,76 @@ public class HelpWindow extends CmWindow {
             }
         });
         addButton(closeBtn);
-        
-        
+
         Html messageArea = new Html();
         try {
             messageArea.setHtml(ContextController.getInstance().getTheContext().getStatusMessage());
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             Log.info("Error getting context help", e);
             messageArea.setHtml("Catchup Math makes learning fun!");
         }
-            messageArea.setStyleName("help-window-message-area");
-        
+        messageArea.addStyleName("help-window-message-area");
+
         VerticalPanel vp = new VerticalPanel();
-        
+
         FieldSet fs = new FieldSet();
         fs.setHeading("Using Catchup Math");
-        
+
         fs.add(messageArea);
         vp.add(fs);
-        
+
         ComboBox<BackgroundModel> bgCombo = new ComboBox<BackgroundModel>();
-        bgCombo.setStore(getBackgrounds());  
+        bgCombo.setStore(getBackgrounds());
         bgCombo.setEditable(false);
-        bgCombo.setStyleName("help-window-bg-combo");
+        bgCombo.addStyleName("help-window-bg-combo");
         bgCombo.setEmptyText("-- Select Wallpaper --");
         bgCombo.addSelectionChangedListener(new SelectionChangedListener<BackgroundModel>() {
             public void selectionChanged(final SelectionChangedEvent<BackgroundModel> se) {
-                
-                /** @TODO: use better check
+
+                /**
+                 * @TODO: use better check
                  * 
                  */
-                if(!UserInfo.getInstance().isActiveUser()) {
+                if (!UserInfo.getInstance().isActiveUser()) {
                     // creating new account, do not allow changing wallpaper
-                    CatchupMathTools.showAlert("You will be able to change your wallpaper once you create your own account");
+                    CatchupMathTools
+                            .showAlert("You will be able to change your wallpaper once you create your own account");
                     return;
                 }
-                
-                
+
                 PrescriptionServiceAsync s = (PrescriptionServiceAsync) Registry.get("prescriptionService");
-                s.setUserBackground(UserInfo.getInstance().getUid(),se.getSelectedItem().getBackgroundStyle(),new AsyncCallback() {
-                    public void onSuccess(Object result) {
-                        try {
-                            CmMainPanel.__lastInstance._mainContent.setStyleName(se.getSelectedItem().getBackgroundStyle());
-                        }
-                        finally {
-                            CatchupMathTools.setBusy(false);
-                        }
-                    }
-                    public void onFailure(Throwable caught) {
-                        caught.printStackTrace();
-                    }
-                });
+                s.setUserBackground(UserInfo.getInstance().getUid(), se.getSelectedItem().getBackgroundStyle(),
+                        new AsyncCallback() {
+                            public void onSuccess(Object result) {
+                                try {
+                                    CmMainPanel.__lastInstance._mainContent.setStyleName(se.getSelectedItem()
+                                            .getBackgroundStyle());
+                                } finally {
+                                    CatchupMathTools.setBusy(false);
+                                }
+                            }
+
+                            public void onFailure(Throwable caught) {
+                                caught.printStackTrace();
+                            }
+                        });
             }
         });
-        
+
         fs = new FieldSet();
         fs.setHeading("Wallpaper");
         Label lab = new Label("Set which image to use for your Catchup Math wallpaper.");
         lab.addStyleName("bg-image-label");
         fs.add(lab);
         fs.add(bgCombo);
-        
-        vp.add(fs);
-        
 
-        if(UserInfo.getInstance().isSingleUser()) {
+        vp.add(fs);
+
+        if (UserInfo.getInstance().isSingleUser()) {
             fs = new FieldSet();
             fs.setLayout(new FlowLayout());
             fs.setHeading("Configuration");
-            fs.setStyleName("help-window-additional-options");
+            fs.addStyleName("help-window-additional-options");
             Button btn = new Button("Setup Catchup Math");
             btn.setWidth(120);
             btn.setToolTip("Modify your Catchup Math settings.");
@@ -137,39 +134,40 @@ public class HelpWindow extends CmWindow {
             });
             btn.addStyleName("button");
             fs.add(btn);
-            
+
             btn = new Button("Restart Program");
             btn.setToolTip("Restart the current program.");
-            btn.addStyleName("button");       
+            btn.addStyleName("button");
             btn.setWidth(120);
             btn.addSelectionListener(new SelectionListener<ButtonEvent>() {
                 public void componentSelected(ButtonEvent ce) {
-                    MessageBox.confirm("Restart Program", "Are you sure you would like to restart your current program?", new Listener<MessageBoxEvent>() {
-                        public void handleEvent(MessageBoxEvent be) {
-                            if (be.getButtonClicked().getText().equals("Yes")) {
-                                FooterPanel.resetProgram_Gwt();                                
-                            }
-                        }
-                    });                    
+                    MessageBox.confirm("Restart Program",
+                            "Are you sure you would like to restart your current program?",
+                            new Listener<MessageBoxEvent>() {
+                                public void handleEvent(MessageBoxEvent be) {
+                                    if (be.getButtonClicked().getText().equals("Yes")) {
+                                        FooterPanel.resetProgram_Gwt();
+                                    }
+                                }
+                            });
                 }
             });
             fs.add(btn);
-            
+
             vp.add(fs);
-        }        
+        }
 
         fs = new FieldSet();
         fs.setLayout(new FlowLayout());
-        fs.setStyleName("help-window-additional-options");
+        fs.addStyleName("help-window-additional-options");
         fs.setHeading("Additional Options");
-        
+
         SelectionListener<ButtonEvent> selList = new SelectionListener<ButtonEvent>() {
             public void componentSelected(ButtonEvent ce) {
                 CatchupMathTools.showAlert("Not available");
             }
         };
-        
-        
+
         Button btn = new Button("Technical Support");
         btn.setWidth(120);
         btn.addStyleName("button");
@@ -187,68 +185,66 @@ public class HelpWindow extends CmWindow {
                 showStudentHistory();
             }
         });
-        
+
         /** Only the owner of the account has access to history */
-        if(!UserInfo.getInstance().isActiveUser())
+        if (!UserInfo.getInstance().isActiveUser())
             btn.setEnabled(false);
-        
-        
+
         /** Do not allow Student History for demo user */
-        if(UserInfo.getInstance().isDemoUser())
+        if (UserInfo.getInstance().isDemoUser())
             btn.setEnabled(false);
-        
+
         btn.addStyleName("button");
         fs.add(btn);
-        
+
         vp.add(fs);
 
         add(vp);
     }
-    
-    
-    /** Provide method for single user to configure programs and settings
-     *  
+
+    /**
+     * Provide method for single user to configure programs and settings
+     * 
      */
     private void showStudentConfiguration() {
         PrescriptionServiceAsync s = (PrescriptionServiceAsync) Registry.get("prescriptionService");
-        s.getStudentModel(UserInfo.getInstance().getUid(), new AsyncCallback <StudentModel>() {
+        s.getStudentModel(UserInfo.getInstance().getUid(), new AsyncCallback<StudentModel>() {
 
-        public void onSuccess(StudentModel student) {
-            
-            CmAdminModel adminModel = new CmAdminModel();
-            adminModel.setId(student.getAdminUid());
-            new RegisterStudent(student, adminModel).showWindow();
-        }
+            public void onSuccess(StudentModel student) {
 
-        public void onFailure(Throwable caught) {
-            String msg = caught.getMessage();
-            CatchupMathTools.showAlert(msg);
-        }
+                CmAdminModel adminModel = new CmAdminModel();
+                adminModel.setId(student.getAdminUid());
+                new RegisterStudent(student, adminModel).showWindow();
+            }
+
+            public void onFailure(Throwable caught) {
+                String msg = caught.getMessage();
+                CatchupMathTools.showAlert(msg);
+            }
         });
     }
-    
-    /** Show this student's history.  First we must
-     *  get the student's StudentModel to make sure 
-     *  we have the current information, then call 
-     *  the StudentDetailWindow.
-     *  
-     *  The StudentModel is read before showing details
-     *  to make sure all summary information is current
-     *  and not what it was on login.
+
+    /**
+     * Show this student's history. First we must get the student's StudentModel
+     * to make sure we have the current information, then call the
+     * StudentDetailWindow.
+     * 
+     * The StudentModel is read before showing details to make sure all summary
+     * information is current and not what it was on login.
      */
     private void showStudentHistory() {
 
         PrescriptionServiceAsync s = (PrescriptionServiceAsync) Registry.get("prescriptionService");
-        s.getStudentModel(UserInfo.getInstance().getUid(), new AsyncCallback <StudentModel>() {
+        s.getStudentModel(UserInfo.getInstance().getUid(), new AsyncCallback<StudentModel>() {
 
-        public void onSuccess(StudentModel student) {
-            new StudentDetailsWindow(student);
-        }
+            public void onSuccess(StudentModel student) {
+                new StudentDetailsWindow(student);
+            }
 
-        public void onFailure(Throwable caught) {
-            String msg = caught.getMessage();
-            CatchupMathTools.showAlert(msg);
-        }
+            public void onFailure(Throwable caught) {
+                String msg = caught.getMessage();
+                CatchupMathTools.showAlert(msg);
+            }
         });
     }
 
@@ -257,75 +253,70 @@ public class HelpWindow extends CmWindow {
 
     private ListStore<BackgroundModel> getBackgrounds() {
         ListStore<BackgroundModel> backgrounds = new ListStore<BackgroundModel>();
-        
+
         BackgroundModel m = new BackgroundModel();
-        m.set("text","Snow Man");
+        m.set("text", "Snow Man");
         m.set("bg_style", "resource-container-snowman");
-        backgrounds.add( m );
-        
-        
+        backgrounds.add(m);
+
         m = new BackgroundModel();
-        m.set("text","Catchup Math");
+        m.set("text", "Catchup Math");
         m.set("bg_style", "resource-container");
         backgrounds.add(m);
-        
-        
+
         m = new BackgroundModel();
-        m.set("text","Mountain Bike");
+        m.set("text", "Mountain Bike");
         m.set("bg_style", "resource-container-bike1");
         backgrounds.add(m);
 
         m = new BackgroundModel();
-        m.set("text","Tulips");
+        m.set("text", "Tulips");
         m.set("bg_style", "resource-container-tulips");
         backgrounds.add(m);
-        
+
         m = new BackgroundModel();
-        m.set("text","Meadow");
+        m.set("text", "Meadow");
         m.set("bg_style", "resource-container-sunrise");
         backgrounds.add(m);
-        
+
         m = new BackgroundModel();
-        m.set("text","Forest");
+        m.set("text", "Forest");
         m.set("bg_style", "resource-container-forest");
         backgrounds.add(m);
 
         m = new BackgroundModel();
-        m.set("text","Clouds");
+        m.set("text", "Clouds");
         m.set("bg_style", "resource-container-clouds");
         backgrounds.add(m);
 
         m = new BackgroundModel();
-        m.set("text","Sunfield");
+        m.set("text", "Sunfield");
         m.set("bg_style", "resource-container-sunfield");
         backgrounds.add(m);
-        
 
         m = new BackgroundModel();
-        m.set("text","Neutral");
+        m.set("text", "Neutral");
         m.set("bg_style", "resource-container-neutral");
         backgrounds.add(m);
-        
+
         m = new BackgroundModel();
-        m.set("text","Redish");
+        m.set("text", "Redish");
         m.set("bg_style", "resource-container-redish");
         backgrounds.add(m);
-        
+
         m = new BackgroundModel();
-        m.set("text","No background image   ");
+        m.set("text", "No background image   ");
         m.set("bg_style", "resource-container-none");
         backgrounds.add(m);
 
-
-
-        
         return backgrounds;
-        
+
     }
-    
-    /** Return the current version number
+
+    /**
+     * Return the current version number
      * 
-     * @todo: externalize this parameter 
+     * @todo: externalize this parameter
      * 
      * 
      * @return
@@ -335,17 +326,13 @@ public class HelpWindow extends CmWindow {
     }
 }
 
+class BackgroundModel extends BaseModelData {
 
-class BackgroundModel extends BaseModelData  {
-    
     public String getBackgroundName() {
         return get("text");
     }
-    
+
     public String getBackgroundStyle() {
         return get("bg_style");
     }
 }
-
-
-
