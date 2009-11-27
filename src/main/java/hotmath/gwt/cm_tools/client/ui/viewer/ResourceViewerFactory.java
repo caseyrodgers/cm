d119c199940bc2f390ceb1b9bdebf9f2cb07c6cf
Casey Rodgers <grodgers@gmail.com>
2009-11-27 21:28:08 +0000

    moved tutor/whiteboard viewer to side by side

--- src/main/java/hotmath/gwt/cm_tools/client/ui/viewer/ResourceViewerFactory.java
+++ src/main/java/hotmath/gwt/cm_tools/client/ui/viewer/ResourceViewerFactory.java
@@ -7,64 +7,51 @@ import hotmath.gwt.shared.client.CmShared;
 
 import com.allen_sauer.gwt.log.client.Log;
 
-
-/** Create the appropriate resource viewer
+/**
+ * Create the appropriate resource viewer
  * 
  * @author Casey
- *
+ * 
  */
 public class ResourceViewerFactory {
 
-    
-    /** Create the appropriate resource viewer type
+    /**
+     * Create the appropriate resource viewer type
      * 
      * @param type
      * @return
      * @throws Exception
      */
-	static public  CmResourcePanel create(InmhItemData item) throws Exception {
-	    
-	    String type = item.getType();
-	    
-	    Log.debug("ResourceViewerFactory: creating new resource viewer: " + type);
-	    
-	    CmResourcePanel rp=null;
-		if(type.equals("practice")) {
-		    
-		    if(CmShared.getQueryParameter("tutor_view_sidebyside") != null)
-		        rp = new ResourceViewerImplTutorSideBySide();
-		    else 
-			    rp = new ResourceViewerImplTutor();
-		}
-		else if(type.equals("video")) { 
-		    rp = new ResourceViewerImplVideo();
-	    }
-		else if(type.startsWith("activity")) {
-		    rp = new ResourceViewerImplActivity();
-		}
-		else if(type.equals("workbook")) {
-		    rp =  new ResourceViewerImplWorkbook();
-		}
-		else if(type.equals("review")) {
-		    rp =  new ResourceViewerImplReview();
-		}
-		else if(type.equals("testset")) {
-		    rp =  new ResourceViewerImplQuiz();
-		}
-		else if(type.equals("results")) {
-		    rp =  new ResourceViewerImplResults();
-		}
-		else if(type.equals("cmextra")) {
-		    rp =  new ResourceViewerImplTutor();
-		}
-		else if(type.equals("flashcard")) {
-		    rp =  new ResourceViewerImplFlashCard();
-		}
-		else
-		    rp =  new CmResourcePanelImplDefault();
-		
-		rp.setResourceItem(item);
-		
-		return rp;
-	}
+    static public CmResourcePanel create(InmhItemData item) throws Exception {
+
+        String type = item.getType();
+
+        Log.debug("ResourceViewerFactory: creating new resource viewer: " + type);
+
+        CmResourcePanel rp = null;
+        if (type.equals("practice")) {
+            rp = new ResourceViewerImplTutor();
+        } else if (type.equals("video")) {
+            rp = new ResourceViewerImplVideo();
+        } else if (type.startsWith("activity")) {
+            rp = new ResourceViewerImplActivity();
+        } else if (type.equals("workbook")) {
+            rp = new ResourceViewerImplWorkbook();
+        } else if (type.equals("review")) {
+            rp = new ResourceViewerImplReview();
+        } else if (type.equals("testset")) {
+            rp = new ResourceViewerImplQuiz();
+        } else if (type.equals("results")) {
+            rp = new ResourceViewerImplResults();
+        } else if (type.equals("cmextra")) {
+            rp = new ResourceViewerImplTutor();
+        } else if (type.equals("flashcard")) {
+            rp = new ResourceViewerImplFlashCard();
+        } else
+            rp = new CmResourcePanelImplDefault();
+
+        rp.setResourceItem(item);
+
+        return rp;
+    }
 }
