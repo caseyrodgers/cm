1d568a060216db673cd169cfac729b89829197c1
Casey Rodgers <grodgers@gmail.com>
2009-12-13 18:58:18 +0000

    ''

--- src/main/java/hotmath/gwt/cm/client/ui/context/CmInmhStandardResources.java
+++ src/main/java/hotmath/gwt/cm/client/ui/context/CmInmhStandardResources.java
@@ -12,18 +12,6 @@ import java.util.List;
  * Probably, we will need to include this data during login.
  * But, since this data does not change we might as well have it static.
  * 
- *  
- *  activity_standard, /learning_activities/interactivities/diamond.swf, Diamond Problem Flash Cards
- *  activity_standard, /learning_activities/interactivities/quadratics.swf, Quadratics in Factored Form
- *  activity_standard, /learning_activities/interactivities/flashcard_addfrac_like.swf, Fraction Flash Cards: Like Denominators
- *  activity_standard, /learning_activities/interactivities/flashcard_addfrac_unlike.swf, Fraction Flash Cards: Unlike Denominators
- *  
- *  
- *  flashcard, /learning_activities/interactivities/flashcard_vocab.swf, Math Vocabulary
- *  flashcard, /learning_activities/interactivities/flashcard_addfrac_like.swf, Adding Like Fractions
- *  flashcard, /learning_activities/interactivities/flashcard_addfrac_unlike.swf, Adding Unlike Fractions
- *  flashcard, /learning_activities/interactivities/flashcard_multi.swf, Multiplication Facts
- *  
  * @author casey
  *
  */
@@ -35,16 +23,17 @@ public class CmInmhStandardResources extends ArrayList<PrescriptionSessionDataRe
         /** Add the standard Activities
          * 
          */
+    	String t = "activity_standard";
         PrescriptionSessionDataResource resourceType = new PrescriptionSessionDataResource();
-        resourceType.setType("activity_standard");
+        resourceType.setType(t);
         resourceType.setLabel("Math Games");
         resourceType.setDescription("Math games that might help you");
         
         List<InmhItemData> items = new ArrayList<InmhItemData>();
-        items.add(new InmhItemData(resourceType.getType(),"/hotmath_help/games/ctf/ctf_hotmath.swf","Catch The Fly"));
-        items.add(new InmhItemData(resourceType.getType(),"/hotmath_help/games/numbercop/numbercop_hotmath.swf", "Number Cop"));
-        items.add(new InmhItemData(resourceType.getType(),"/hotmath_help/games/kp/kp_hotmath_sound.swf","Algebra vs. the Cockroaches"));
-        items.add(new InmhItemData(resourceType.getType(),"/hotmath_help/games/factortris/factortris_hotmath_sound.swf", "Factortris"));
+        items.add(new InmhItemData(t,"/hotmath_help/games/ctf/ctf_hotmath.swf","Catch The Fly"));
+        items.add(new InmhItemData(t,"/hotmath_help/games/numbercop/numbercop_hotmath.swf", "Number Cop"));
+        items.add(new InmhItemData(t,"/hotmath_help/games/kp/kp_hotmath_sound.swf","Algebra vs. the Cockroaches"));
+        items.add(new InmhItemData(t,"/hotmath_help/games/factortris/factortris_hotmath_sound.swf", "Factortris"));
         resourceType.setItems(items);
         add(resourceType);
 
@@ -53,17 +42,21 @@ public class CmInmhStandardResources extends ArrayList<PrescriptionSessionDataRe
         /** Add the Standard Flash Cards
          * 
          */
+        t = "flashcard";
         resourceType = new PrescriptionSessionDataResource();
-        resourceType.setType("flashcard");
+        resourceType.setType(t);
         resourceType.setLabel("Flash Cards");
         resourceType.setDescription("Flash Card resources that might help you");
         
         items = new ArrayList<InmhItemData>();
-        items.add(new InmhItemData(resourceType.getType(),"/learning_activities/interactivities/flashcard_vocab.swf", "Math Vocabulary"));
-        items.add(new InmhItemData(resourceType.getType(),"/learning_activities/interactivities/flashcard_addfrac_like.swf", "Adding Like Fractions"));
-        items.add(new InmhItemData(resourceType.getType(),"/learning_activities/interactivities/flashcard_addfrac_unlike.swf", "Adding Unlike Fractions"));
-        items.add(new InmhItemData(resourceType.getType(),"/learning_activities/interactivities/flashcard_multi.swf", "Multiplication Facts"));
-        items.add(new InmhItemData(resourceType.getType(),"/learning_activities/interactivities/flashcard_oops.swf", "Order of Operations"));
+        t = resourceType.getType();
+        items.add(new InmhItemData(t,"/learning_activities/interactivities/flashcard_vocab.swf", "Math Vocabulary"));
+        items.add(new InmhItemData(t,"/learning_activities/interactivities/flashcard_addfrac_like.swf", "Adding Like Fractions"));
+        items.add(new InmhItemData(t,"/learning_activities/interactivities/flashcard_addfrac_unlike.swf", "Adding Unlike Fractions"));
+        items.add(new InmhItemData(t,"/learning_activities/interactivities/flashcard_multi.swf", "Multiplication Facts"));
+        items.add(new InmhItemData(t,"/learning_activities/interactivities/flashcard_oops.swf", "Order of Operations"));
+        items.add(new InmhItemData(t,"/learning_activities/interactivities/flashcard_oops.swf", "Order of Operations"));
+        items.add(new InmhItemData(t,"/learning_activities/interactivities/flashcard_reducingfractions.swf", "Reducing Fractions"));
         resourceType.setItems(items);        
         add(resourceType);
     }
