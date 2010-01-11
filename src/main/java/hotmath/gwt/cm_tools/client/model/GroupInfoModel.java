cf145add929bc79a3ddd0e152173ed140d76b381
Bob Hall <rfhall3@yahoo.com>
2010-01-11 08:12:10 +0000

    add is_self_reg, add static attribute 'keys' (WO 784)

--- src/main/java/hotmath/gwt/cm_tools/client/model/GroupInfoModel.java
+++ src/main/java/hotmath/gwt/cm_tools/client/model/GroupInfoModel.java
@@ -4,46 +4,66 @@ import java.io.Serializable;
 
 public class GroupInfoModel extends BaseModel implements Serializable, GroupInfoModelI {
 
-    public GroupInfoModel() {
+	private static final long serialVersionUID = 3835182712109469633L;
+	
+	public static String ADMIN_ID      = "admin_id";
+	public static String GROUP_NAME    = "group_name"; 
+	public static String ID            = "id";
+	public static String IS_SELF_REG   = "is_self_reg";
+	public static String STUDENT_COUNT = "student_count";
+
+	public GroupInfoModel() {
     }
 
-    public GroupInfoModel(Integer adminId, Integer id, String groupName, Integer studentCount) {
+    public GroupInfoModel(Integer adminId, Integer id, String groupName, Integer studentCount,
+    	Boolean isSelfReg) {
         setAdminId(adminId);
         setId(id);
         setGroupName(groupName);
         setStudentCount(studentCount);
+        setIsSelfReg(isSelfReg);
     }
     
     public void setAdminId(Integer adminId) {
-        set("admin_id", adminId);
+        set(ADMIN_ID, adminId);
     }
     
     public Integer getAdminId() {
-        return get("admin_id");
+        return get(ADMIN_ID);
     }
     public void setId(Integer id) {
-        set("id", id);
+        set(ID, id);
     }
     
     public Integer getId() {
-        return get("id");
+        return get(ID);
     }
 
     public void setGroupName(String groupName) {
-        set("group_name", groupName);
+        set(GROUP_NAME, groupName);
+    }
+
+    public void setIsSelfReg(Boolean isSelfReg) {
+    	set(IS_SELF_REG, isSelfReg);
+    }
+    
+    public Boolean getIsSelfReg() {
+    	return get(IS_SELF_REG);
     }
 
     public void setStudentCount(Integer studentCount) {
-        set("student_count", studentCount);
+        set(STUDENT_COUNT, studentCount);
     }
 
     @Override
     public Integer getCount() {
-        return get("student_count");
+        return get(STUDENT_COUNT);
     }
 
     @Override
     public String getName() {
-        return get("group_name");
+        return get(GROUP_NAME);
     }
+    
+    
 }
