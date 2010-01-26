8128d5358b018813bda476891d76b5695759a4f8
Casey Rodgers <grodgers@gmail.com>
2010-01-26 23:41:37 +0000

    *** empty log message ***

--- src/test/hotmath/cm/util/CmMultiLinePropertyReader_Test.java
+++ src/test/hotmath/cm/util/CmMultiLinePropertyReader_Test.java
@@ -1,5 +1,8 @@
 package hotmath.cm.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import junit.framework.TestCase;
 
 public class CmMultiLinePropertyReader_Test extends TestCase {
@@ -8,9 +11,30 @@ public class CmMultiLinePropertyReader_Test extends TestCase {
         super(name);
     }
     
+    @Override
+    protected void setUp() throws Exception {
+        CmMultiLinePropertyReader.getInstance().put("PROP_1", "PROPERTY_1");
+        CmMultiLinePropertyReader.getInstance().put("PROP_2", "PROPERTY_1 WITH REPLACMENT: $$THIS$$");
+    }
+    
     public void testSingleProperty() throws Exception {
-        String p = CmMultiLinePropertyReader.getInstance().getProperty("CmWebResourceManager.baseDirectory").trim();
+        String p = CmMultiLinePropertyReader.getInstance().getProperty("PROP_1").trim();
         assertNotNull(p);
+    }
+    
+    
+    public void testSinglePropertyReplacement() throws Exception {
+        Map<String,String> replace = new HashMap<String,String>();
+        replace.put("THIS", "WITHTHAT");
+        String p = CmMultiLinePropertyReader.getInstance().getProperty("PROP_2", replace).trim();
+        assertTrue(p.indexOf("WITHTHAT") > -1);
+    }
+    
+    public void testSinglePropertyNotExist() throws Exception {
+        Map<String,String> replace = new HashMap<String,String>();
+        replace.put("THIS", "WITHTHAT");
+        String p = CmMultiLinePropertyReader.getInstance().getProperty("NOT_EXIST");
+        assertTrue(p == null);
     }
 
 }
