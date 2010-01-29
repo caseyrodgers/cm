2037bae46b11b41f00181460a860c58260d62224
Bob Hall <rfhall3@yahoo.com>
2010-01-29 09:48:29 +0000

    add Group Assessment report

--- src/main/java/hotmath/gwt/shared/server/service/command/GeneratePdfCommand.java
+++ src/main/java/hotmath/gwt/shared/server/service/command/GeneratePdfCommand.java
@@ -1,6 +1,7 @@
 package hotmath.gwt.shared.server.service.command;
 
 import hotmath.cm.util.CmWebResourceManager;
+import hotmath.cm.util.report.GroupAssessmentReport;
 import hotmath.cm.util.report.StudentDetailReport;
 import hotmath.cm.util.report.StudentReportCard;
 import hotmath.cm.util.report.StudentSummaryReport;
@@ -23,19 +24,20 @@ import org.apache.log4j.Logger;
 public class GeneratePdfCommand implements ActionHandler<GeneratePdfAction, CmWebResource>{
 
     Logger logger = Logger.getLogger(GeneratePdfCommand.class);
+
     /** 
      * Performs the action: generate a PDF and return absolute file name to created PDF
      */
     @Override
     public CmWebResource execute(Connection conn, GeneratePdfAction action) throws Exception {
         String reportName=null;
-    
+
         Integer adminId = action.getAdminId();
         List<Integer> studentUids = action.getStudentUids();
         PdfType pdfType = action.getPdfType();
-        
+
         String reportId = new CmAdminDao().getPrintableStudentReportId(studentUids);
-        
+
         ByteArrayOutputStream baos = null;
         if (pdfType == PdfType.STUDENT_SUMMARY) {
             StudentSummaryReport ss = new StudentSummaryReport();
@@ -44,7 +46,7 @@ public class GeneratePdfCommand implements ActionHandler<GeneratePdfAction, CmWe
         }
         else if (pdfType == PdfType.STUDENT_DETAIL) {
             StudentDetailReport sd = new StudentDetailReport();
-            baos = sd.makePdf(conn,reportId, adminId);
+            baos = sd.makePdf(conn, reportId, adminId);
             reportName = sd.getReportName();
         }
         else if (pdfType == PdfType.REPORT_CARD) {
@@ -52,28 +54,31 @@ public class GeneratePdfCommand implements ActionHandler<GeneratePdfAction, CmWe
             baos = sr.makePdf(conn, reportId, adminId);
             reportName = sr.getReportName();
         }
+        else if (pdfType == PdfType.GROUP_ASSESSMENT) {
+            GroupAssessmentReport gr = new GroupAssessmentReport();
+            baos = gr.makePdf(conn, reportId, adminId);
+            reportName = gr.getReportName();
+        }        
         else {
             throw new IllegalArgumentException("Unrecognized report type: " + pdfType);
         }
-        
+
         // write PDF ByteArrayOutputStream to a ServletOutputStream
         if (baos != null) {
-            
-            // write out to temporary file to be clean up later
+
+        	// write to temporary file to be cleaned up later
             String outputBase = CmWebResourceManager.getInstance().getFileBase();
-            
+
             // if outputBase/adminId directory doesn't exist, create it
             String outputDir = ensureOutputDir(outputBase, adminId);
             
-
-            
-            File filePath = new File(outputDir,reportName + ".pdf");
+            File filePath = new File(outputDir, reportName + ".pdf");
             logger.info("Writing PDF output: " + filePath);
             FileOutputStream fw = null;
             try {
                 fw = new FileOutputStream(filePath);
                 baos.writeTo(fw);
-                
+
                 return new CmWebResource(filePath.getPath(), CmWebResourceManager.getInstance().getFileBase(), CmWebResourceManager.getInstance().getWebBase());
             }
             finally {
@@ -84,15 +89,15 @@ public class GeneratePdfCommand implements ActionHandler<GeneratePdfAction, CmWe
             throw new Exception("PDF generation failed");
         }
     }
-    
+
     @Override
     public Class<? extends Action<? extends Response>> getActionType() {
         // TODO Auto-generated method stub
         return GeneratePdfAction.class;
     }
-  
+
     private String ensureOutputDir(String outputBase, Integer adminId) {
-        
+
         File file = new File(outputBase,Integer.toString(adminId));
     	if (! file.exists()) {
     		file.mkdirs();
