5378be3917e351e617e28a4861eaca35901a2ae6
Casey Rodgers <grodgers@gmail.com>
2010-02-15 18:37:52 +0000

    add isActive

--- src/main/java/hotmath/testset/ha/CmProgram.java
+++ src/main/java/hotmath/testset/ha/CmProgram.java
@@ -9,28 +9,32 @@ package hotmath.testset.ha;
  * 
  */
 public enum CmProgram {
-    PREALG_PROF_V1(16, "Pre-Alg_v1", "Prof"),
-    PREALG_PROF(34, "Pre-Alg", "Prof"), 
-    PREALG_CHAP(22, "Pre-Alg", "Chap"), 
-    ALG1_PROF(30, "Alg 1", "Prof"),
-    ALG1_PROF_V1(17, "Alg 1_v1", "Prof"), 
-    ALG2_CHAP(31, "Alg 2", "Chap"), 
-    ALG1_CHAP(32, "Alg 1", "Chap"), 
-    ALG2_PROF(29, "Alg 2", "Prof"), 
-    GEOM_PROF(18, "Geom","Prof"), 
-    CAHSEEHM(28, "", "Grad Prep"), 
-    TAKS(33,"","Grad Prep Tx"),
-    GEOM_CHAP(24, "Geom", "Chap"),
-    AUTO_ENROLL(15,"","Auto-Enroll");
+    PREALG_PROF_V1(16, "Pre-Alg_v1", "Prof",false),
+    PREALG_PROF(34, "Pre-Alg", "Prof",true), 
+    PREALG_CHAP(22, "Pre-Alg", "Chap",true), 
+    ALG1_PROF(30, "Alg 1", "Prof",true),
+    ALG1_PROF_V1(17, "Alg 1_v1", "Prof",false), 
+    ALG2_CHAP(31, "Alg 2", "Chap",true), 
+    ALG1_CHAP(32, "Alg 1", "Chap",true), 
+    ALG2_PROF(29, "Alg 2", "Prof",true), 
+    GEOM_PROF(18, "Geom","Prof",true), 
+    CAHSEEHM(28, "", "Grad Prep",true), 
+    TAKS(33,"","Grad Prep Tx",true),
+    GEOM_CHAP(24, "Geom", "Chap",true),
+    AUTO_ENROLL(15,"","Auto-Enroll",true);
+
+
 
     private final int defId;
     private final String subject;
     private final String programId;
+    private final boolean isActive;
 
-    CmProgram(int defId, String subject, String programId) {
+    CmProgram(int defId, String subject, String programId,boolean isActive) {
         this.defId = defId;
         this.subject = subject;
         this.programId = programId;
+        this.isActive = isActive;
     }
 
     public int getDefId() {
@@ -44,4 +48,8 @@ public enum CmProgram {
     public String getProgramId() {
         return programId;
     }
+    
+    public boolean isActive() {
+        return isActive;
+    }    
 }
