02f710f1da124c735a1756589d49244e75344388
Casey Rodgers <grodgers@gmail.com>
2010-03-04 23:41:32 +0000

    added static version number checking

---
+++ src/main/java/hotmath/gwt/shared/client/rpc/result/CatchupMathVersion.java
@@ -0,0 +1,32 @@
+package hotmath.gwt.shared.client.rpc.result;
+
+import hotmath.gwt.shared.client.rpc.Response;
+
+/** Provide information about current build version numbers
+ *  for CM
+ *  
+ * @author casey
+ *
+ */
+public class CatchupMathVersion implements Response{
+    int version;
+    
+    public CatchupMathVersion() {}
+    
+    public CatchupMathVersion(Integer version) {
+        this.version = version;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    @Override
+    public String toString() {
+        return "CatchupMathVersion [version=" + version + "]";
+    }
+}
---
+++ src/main/java/hotmath/gwt/shared/client/util/SystemVersionUpdateChecker.java
@@ -0,0 +1,101 @@
+package hotmath.gwt.shared.client.util;
+
+import hotmath.gwt.cm_tools.client.ui.CmWindow.CmWindow;
+import hotmath.gwt.shared.client.CatchupMathVersionInfo;
+import hotmath.gwt.shared.client.CmShared;
+import hotmath.gwt.shared.client.eventbus.CmEvent;
+import hotmath.gwt.shared.client.eventbus.EventBus;
+import hotmath.gwt.shared.client.eventbus.EventType;
+import hotmath.gwt.shared.client.rpc.RetryAction;
+import hotmath.gwt.shared.client.rpc.action.GetCatchupMathVersionAction;
+import hotmath.gwt.shared.client.rpc.result.CatchupMathVersion;
+
+import com.allen_sauer.gwt.log.client.Log;
+import com.extjs.gxt.ui.client.event.ButtonEvent;
+import com.extjs.gxt.ui.client.event.SelectionListener;
+import com.extjs.gxt.ui.client.widget.Html;
+import com.extjs.gxt.ui.client.widget.button.Button;
+import com.google.gwt.user.client.Timer;
+
+/** Display a message from central server
+ * 
+ * @author casey
+ *
+ */
+public class SystemVersionUpdateChecker extends CmWindow {
+
+    static final int CHECK_FOR_MESSAGES_EVERY = 1000 * 60 * 15;
+    static SystemVersionUpdateChecker _theWindow;
+    
+    private SystemVersionUpdateChecker() {
+        _theWindow = this;
+        EventBus.getInstance().fireEvent(new CmEvent(EventType.EVENT_TYPE_MODAL_WINDOW_OPEN));
+        
+        setSize(300,200);
+        setResizable(true);
+        setClosable(true);
+        setModal(true);
+        setClosable(false);
+        setHeading("Catchup Math Update");
+
+        String html = "We have recently updated Catchup Math.  " + 
+                      "Please Refresh your browser by pressing the F5 key. " +
+                      "Thank you for using Catchup Math!";
+        
+        setStyleName("server-update-window");
+        
+        String msg = "<div style='font-size: 110%;padding: 10px;'>" 
+            + html 
+            + "</div>";
+        add(new Html(msg));
+        addButton(new Button("Close", new SelectionListener<ButtonEvent>() {
+            @Override
+            public void componentSelected(ButtonEvent ce) {
+                _theWindow = null;
+                close();
+            }
+        }));
+        setVisible(true);
+    }
+    
+    /** Monitor messages by calling server every N seconds and looks
+     *  for messages this user has not already seen
+     *  
+     */
+    static public void monitorVersionChanges() {
+        Timer timer = new Timer() {
+            @Override
+            public void run() {
+                checkForUpdate();
+            }
+        };
+        timer.scheduleRepeating(CHECK_FOR_MESSAGES_EVERY);
+        
+        /** check now */
+        checkForUpdate();
+    }
+    
+    
+    /** Call server and look for new messages.
+     * 
+     * Only call server if window is not currently being displayed.
+     */
+    static private void checkForUpdate() {
+        if(_theWindow != null)
+            return;
+        new RetryAction<CatchupMathVersion>() {
+            @Override
+            public void attempt() {
+                GetCatchupMathVersionAction action = new GetCatchupMathVersionAction();
+                setAction(action);
+                CmShared.getCmService().execute(action, this);
+            }
+            @Override
+            public void oncapture(CatchupMathVersion version) {
+                if(version.getVersion() != CatchupMathVersionInfo.getBuildVersion()) {
+                    new SystemVersionUpdateChecker();
+                }
+            }
+        }.attempt();
+    }
+}
