90e037a6831e4878f8acb73de1f1bbaa665ec815
Bob Hall <rfhall3@yahoo.com>
2010-03-10 07:38:12 +0000

    add paging support changes (should really be elsewhere...)

--- src/main/java/hotmath/gwt/cm_tools/client/model/StudentModelExt.java
+++ src/main/java/hotmath/gwt/cm_tools/client/model/StudentModelExt.java
@@ -1,5 +1,6 @@
 package hotmath.gwt.cm_tools.client.model;
 
+
 import java.io.Serializable;
 
 public class StudentModelExt extends BaseModel implements Serializable, StudentModelI {
@@ -14,9 +15,12 @@ public class StudentModelExt extends BaseModel implements Serializable, StudentM
     public static final String SUBJ_ID_KEY = "subjId";
     public static final String CHAPTER_KEY = "chapter";
     public static final String LAST_QUIZ_KEY = "last-quiz";
+    public static final String HAS_LAST_QUIZ_KEY = "has-last-quiz";
     public static final String LAST_LOGIN_KEY = "last-login";
+    public static final String HAS_LAST_LOGIN_KEY = "has-last-login";
     public static final String JSON_KEY = "json";
     public static final String STATUS_KEY = "status";
+    public static final String HAS_STATUS_KEY = "has-status";
     public static final String UID_KEY = "uid";
     public static final String EMAIL_KEY = "email";
     public static final String ADMIN_UID_KEY = "admin_uid";
@@ -26,13 +30,17 @@ public class StudentModelExt extends BaseModel implements Serializable, StudentM
     public static final String TUTORING_AVAIL_KEY = "tutoring-avail";
     public static final String TUTORING_STATE_KEY = "tutoring-state";
     public static final String TUTORING_USE_KEY = "tutoring-use";
+    public static final String HAS_TUTORING_USE_KEY = "has-tutoring-use";
     public static final String NAME_KEY = "name";
     public static final String PASSCODE_KEY = "passcode";
     public static final String BACKGROUND_STYLE = "background_style";
     public static final String DEMO_USER_KEY = "demo_user";
     public static final String PROGRAM_DESCR_KEY = "program";
     public static final String PASSING_COUNT_KEY = "passing-count";
+    public static final String HAS_PASSING_COUNT_KEY = "has-passing-count";
     public static final String NOT_PASSING_COUNT_KEY = "not-passing-count";
+    
+    private Boolean hasExtendedData = false;
 
     public StudentModelExt() {
     }
@@ -49,27 +57,41 @@ public class StudentModelExt extends BaseModel implements Serializable, StudentM
         set(PROG_ID_KEY, student.getProgId());
         set(SUBJ_ID_KEY, student.getSubjId());
         set(CHAPTER_KEY, student.getChapter());
-        set(LAST_QUIZ_KEY, student.getLastQuiz());
-        set(LAST_LOGIN_KEY, student.getLastLogin());
+
+        setLastQuiz(student.getLastQuiz());
+        setLastLogin(student.getLastLogin());
+        setStatus(student.getStatus());
+        setTutoringUse(student.getTutoringUse());
+        setPassingCount(student.getPassingCount());
+        setNotPassingCount(student.getNotPassingCount());        
+
         set(JSON_KEY, student.getJson());
 
-        set(STATUS_KEY, student.getStatus());
         set(UID_KEY, student.getUid());
         set(EMAIL_KEY, student.getEmail());
         set(ADMIN_UID_KEY, student.getAdminUid());
         set(SHOW_WORK_KEY, student.getShowWorkRequired());
-        set(SHOW_WORK_STATE_KEY, student.getShowWorkState());
-        set(TOTAL_USAGE_KEY, student.getTotalUsage());
+        set(SHOW_WORK_STATE_KEY, (student.getShowWorkRequired())?"REQUIRED":"OPTIONAL");
+
+        set(TOTAL_USAGE_KEY, (student.getTotalUsage() != null)?student.getTotalUsage():0);
+
         set(TUTORING_AVAIL_KEY, student.getTutoringAvail());
-        set(TUTORING_STATE_KEY, student.getTutoringState());
+        set(TUTORING_STATE_KEY, (student.getTutoringAvail())?"ON":"OFF");
         set(NAME_KEY, student.getName());
         set(PASSCODE_KEY, student.getPasscode());
         set(BACKGROUND_STYLE, student.getBackgroundStyle());
         set(DEMO_USER_KEY, student.getIsDemoUser());
         set(PROGRAM_DESCR_KEY, student.getProgramDescr());
-        set(TUTORING_USE_KEY, student.getTutoringUse());
-        set(PASSING_COUNT_KEY, student.getPassingCount());
-        set(NOT_PASSING_COUNT_KEY, student.getNotPassingCount());
+    }
+
+    public void assignExtendedData(StudentModelI sm) {
+    	setHasExtendedData(true);
+    	setLastQuiz(sm.getLastQuiz());
+    	setLastLogin(sm.getLastLogin());
+    	setStatus(sm.getStatus());
+    	setTutoringUse(sm.getTutoringUse());
+    	setPassingCount(sm.getPassingCount());
+    	setNotPassingCount(sm.getNotPassingCount());
     }
 
     public Integer getTutoringUse() {
@@ -78,33 +100,40 @@ public class StudentModelExt extends BaseModel implements Serializable, StudentM
 
     @Override
     public void setTutoringUse(Integer x) {
-        set(TUTORING_USE_KEY, x);
+        set(TUTORING_USE_KEY, (x != null) ? x : 0);
     }
 
+    @Override
     public String getName() {
         return get(NAME_KEY);
     }
 
+    @Override
     public void setName(String name) {
         set(NAME_KEY, name);
     }
 
+    @Override
     public String getPasscode() {
         return get(PASSCODE_KEY);
     }
 
+    @Override
     public void setPasscode(String passcode) {
         set(PASSCODE_KEY, passcode);
     }
 
+    @Override
     public String getProgramDescr() {
         return get("program");
     }
 
+    @Override
     public void setProgramDescr(String progDescr) {
         set("program", progDescr);
     }
 
+    @Override
     public String getGroup() {
         return get(GROUP_KEY);
     }
@@ -117,98 +146,122 @@ public class StudentModelExt extends BaseModel implements Serializable, StudentM
         return get(GROUP_ID_KEY);
     }
 
+    @Override
     public void setGroupId(String groupId) {
         set(GROUP_ID_KEY, groupId);
     }
 
+    @Override
     public Integer getUserProgramId() {
         return get(USER_PROGRAM_KEY);
     }
 
+    @Override
     public void setUserProgramId(Integer userProgId) {
         set(USER_PROGRAM_KEY, userProgId);
     }
 
+    @Override
     public void setSectionNum(Integer sectionNum) {
         set(SECTION_NUM_KEY, sectionNum);
     }
 
+    @Override
     public Integer getSectionNum() {
         return get(SECTION_NUM_KEY);
     }
 
+    @Override
     public String getLastLogin() {
         return get(LAST_LOGIN_KEY);
     }
 
+    @Override
     public void setLastLogin(String lastLogin) {
-        set(LAST_LOGIN_KEY, lastLogin);
+        set(LAST_LOGIN_KEY, (lastLogin != null) ? lastLogin : " ");
     }
 
+    @Override
     public void setStatus(String status) {
-        set(STATUS_KEY, status);
+        set(STATUS_KEY, (status != null) ? status : " ");
     }
 
+    @Override
     public String getStatus() {
         return get(STATUS_KEY);
     }
 
+    @Override
     public void setTotalUsage(Integer totalUsage) {
         set(TOTAL_USAGE_KEY, totalUsage);
     }
 
+    @Override
     public Integer getTotalUsage() {
         return get(TOTAL_USAGE_KEY);
     }
 
+    @Override
     public String getPassPercent() {
         return get("pass-percent");
     }
 
+    @Override
     public void setPassPercent(String passPercent) {
         set("pass-percent", passPercent);
     }
 
+    @Override
     public void setTutoringState(String tutoringState) {
         set(TUTORING_STATE_KEY, tutoringState);
     }
 
+    @Override
     public String getTutoringState() {
         return get(TUTORING_STATE_KEY);
     }
 
+    @Override
     public void setShowWorkState(String swState) {
         set(SHOW_WORK_STATE_KEY, swState);
     }
 
+    @Override
     public String getShowWorkState() {
         return get(SHOW_WORK_STATE_KEY);
     }
 
+    @Override
     public void setUid(Integer uid) {
         set(UID_KEY, uid);
     }
 
+    @Override
     public Integer getUid() {
         return get(UID_KEY);
     }
 
+    @Override
     public void setEmail(String emailAddr) {
         set(EMAIL_KEY, emailAddr);
     }
 
+    @Override
     public String getEmail() {
         return get(EMAIL_KEY);
     }
 
+    @Override
     public void setAdminUid(Integer adminUid) {
         set(ADMIN_UID_KEY, adminUid);
     }
 
+    @Override
     public Integer getAdminUid() {
         return get(ADMIN_UID_KEY);
     }
 
+    @Override
     public void setProgramChanged(Boolean changed) {
         set("programChanged", changed);
     }
@@ -221,6 +274,7 @@ public class StudentModelExt extends BaseModel implements Serializable, StudentM
      * 
      * @return
      */
+    @Override
     public Boolean getProgramChanged() {
         Boolean pc = get("programChanged");
         if (pc != null) {
@@ -230,74 +284,92 @@ public class StudentModelExt extends BaseModel implements Serializable, StudentM
         }
     }
 
+    @Override
     public String getProgId() {
         return get(PROG_ID_KEY);
     }
 
+    @Override
     public void setProgId(String progId) {
         set(PROG_ID_KEY, progId);
     }
 
+    @Override
     public String getSubjId() {
         return get(SUBJ_ID_KEY);
     }
 
+    @Override
     public void setSubjId(String progId) {
         set(SUBJ_ID_KEY, progId);
     }
 
+    @Override
     public String getChapter() {
         return get(CHAPTER_KEY);
     }
 
+    @Override
     public void setChapter(String chapter) {
         set(CHAPTER_KEY, chapter);
     }
 
+    @Override
     public String getLastQuiz() {
         return get(LAST_QUIZ_KEY);
     }
 
+    @Override
     public void setLastQuiz(String lastQuiz) {
-        set(LAST_QUIZ_KEY, lastQuiz);
+        set(LAST_QUIZ_KEY, (lastQuiz != null) ? lastQuiz : " ");
     }
 
+    @Override
     public String getJson() {
         return get(JSON_KEY);
     }
 
+    @Override
     public void setJson(String json) {
         set(JSON_KEY, json);
     }
 
+    @Override
     public Boolean getShowWorkRequired() {
         return get(SHOW_WORK_KEY);
     }
 
+    @Override
     public void setShowWorkRequired(Boolean val) {
         set(SHOW_WORK_KEY, val);
     }
 
+    @Override
     public Boolean getTutoringAvail() {
         return get(TUTORING_AVAIL_KEY);
     }
 
+    @Override
     public void setTutoringAvail(Boolean val) {
         set(TUTORING_AVAIL_KEY, val);
     }
 
+    @Override
     public String getBackgroundStyle() {
         return get(BACKGROUND_STYLE);
     }
 
+    @Override
     public void setBackgroundStyle(String style) {
         set(BACKGROUND_STYLE, style);
     }
 
+    @Override
     public void setIsDemoUser(Boolean isDemo) {
         set(DEMO_USER_KEY, isDemo);
     }
 
+    @Override
     public Boolean getIsDemoUser() {
         return get(DEMO_USER_KEY);
     }
@@ -309,7 +381,7 @@ public class StudentModelExt extends BaseModel implements Serializable, StudentM
 
 	@Override
 	public void setNotPassingCount(Integer count) {
-		set(NOT_PASSING_COUNT_KEY, count);
+        set(NOT_PASSING_COUNT_KEY, (count != null) ? count : 0);
 	}
 
 	@Override
@@ -319,6 +391,55 @@ public class StudentModelExt extends BaseModel implements Serializable, StudentM
 
 	@Override
 	public void setPassingCount(Integer count) {
-		set(PASSING_COUNT_KEY, count);
+        set(PASSING_COUNT_KEY, (count != null) ? count : 0);
 	}
+
+	public Boolean getHasLastQuiz() {
+		return get(HAS_LAST_QUIZ_KEY);
+	}
+
+	public void setHasLastQuiz(Boolean val) {
+		set(HAS_LAST_QUIZ_KEY, val);
+	}
+
+	public Boolean getHasLastLogin() {
+		return get(HAS_LAST_LOGIN_KEY);
+	}
+
+	public void setHasLastLogin(Boolean val) {
+		set(HAS_LAST_LOGIN_KEY, val);
+	}
+
+	public Boolean getHasStatus() {
+		return get(HAS_STATUS_KEY);
+	}
+
+	public void setHasStatus(Boolean val) {
+		set(HAS_STATUS_KEY, val);
+	}
+
+	public Boolean getHasTutoringUse() {
+		return get(HAS_TUTORING_USE_KEY);
+	}
+
+	public void setHasTutoringUse(Boolean val) {
+		set(HAS_TUTORING_USE_KEY, val);
+	}
+
+	public Boolean getHasPassingCount() {
+		return get(HAS_PASSING_COUNT_KEY);
+	}
+
+	public void setHasPassingCount(Boolean val) {
+		set(HAS_PASSING_COUNT_KEY, val);
+	}
+
+	public Boolean getHasExtendedData() {
+		return hasExtendedData;
+	}
+
+	public void setHasExtendedData(Boolean val) {
+		this.hasExtendedData = val;
+	}
+
 }
