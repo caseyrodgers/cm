84764dc0a815b0195d8fb762b23e17b4533ff774
Casey Rodgers <grodgers@gmail.com>
2010-04-16 22:26:55 +0000

    *** empty log message ***

---
+++ src/main/java/hotmath/cm/test/HaTestSet.java
@@ -0,0 +1,80 @@
+package hotmath.cm.test;
+
+
+import hotmath.SolutionManager;
+import hotmath.solution.Solution;
+import hotmath.solution.SolutionPostProcess;
+import hotmath.util.sql.SqlUtilities;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Creates HTML for each question identified by PID
+ *  
+ * @author Casey
+ * 
+ */
+public class HaTestSet {
+    List<HaTestSetQuestion> questions = new ArrayList<HaTestSetQuestion>();
+    
+    String textCode;
+    String _server;
+
+    /** Create a HaTestSet that contains the HTML for each question in pidList
+     * 
+     * @param conn
+     * @param pidList
+     * @throws Exception
+     */
+    public HaTestSet(final Connection conn, List<String> pidList) throws Exception {
+        Statement stmt=null;
+        try {
+            SolutionPostProcess postProcessor = new SolutionPostProcess();
+            // get all solutions for this textcode and
+            // extract the problem statement for each
+            if(pidList.size() > 0) {
+                String pidInList = "";
+                for (String p : pidList) {
+                    if (pidInList.length() > 0)
+                        pidInList += ",";
+                    pidInList += "'" + p + "'";
+                }
+                String sql = " select PROBLEMINDEX, cast(problemnumber as unsigned int) as pn" +
+                             " FROM SOLUTIONS " +
+                             " where problemindex in (" +
+                               pidInList +
+                             ")" + " ORDER BY booktitle, pn";
+                
+                ResultSet rs = conn.createStatement().executeQuery(sql);
+                while(rs.next()) {
+                    Solution sol = SolutionManager.getSolution(rs.getString("problemindex"));
+                    String statement = postProcessor.processHTML_SolutionImagesAbsolute(sol.getStatement(), sol.getSolutionImagesURI(), null);
+                    questions.add(new HaTestSetQuestion(sol.getGUID(), statement));
+                }
+            }
+        }
+        finally {
+        SqlUtilities.releaseResources(null,stmt,null);
+        }
+    }
+    
+    /** return array of indexes to the correct answer for each question
+     * 
+     * @return
+     */
+    public List<Integer> getAnswers() {
+        List<Integer> answers = new ArrayList<Integer>();
+        for(HaTestSetQuestion q: questions) {
+            answers.add(q.getCorrectAnswer());
+        }
+        return answers;
+    }
+    
+    public List<HaTestSetQuestion> getQuestions() {
+        return this.questions;
+    }
+}
---
+++ src/main/java/hotmath/cm/test/HaTestSetQuestion.java
@@ -0,0 +1,162 @@
+package hotmath.cm.test;
+
+import hotmath.ProblemID;
+
+import org.htmlparser.Node;
+import org.htmlparser.Parser;
+import org.htmlparser.Tag;
+import org.htmlparser.visitors.NodeVisitor;
+
+import sb.util.SbUtilities;
+
+/** Represents a single Question in a test set.
+ * 
+ * Each question is in the form:
+ * 
+
+<DIV id="question_1" class="question_stepunit">
+    <P> The Question Text </P>
+    <UL class="question_responses">
+        <LI>
+            <INPUT onclick="questionGuessChanged(this)" id="question_guess_3"
+                type="radio" name="question_1" value="false">
+                <LABEL for="question_guess_3">
+                    <P>C. 2</P>
+                </LABEL>
+                <DIV style="display: none">Incorrect</DIV>
+        </LI>
+        <LI>
+            <INPUT onclick="questionGuessChanged(this)" id="question_guess_4"
+                type="radio" name="question_1" value="true">
+                <LABEL for="question_guess_4">
+                    <P>D. 8</P>
+                </LABEL>
+                <DIV style="display: none">Correct</DIV>
+        </LI>
+    </UL>
+</DIV>
+ *
+ *
+ * Notice the answer is included in HTML.  This will strip that out of HTML and record
+ * correct answer.
+ * 
+ * @see GetQuizHtmlCommand setAnswers
+ * 
+ */
+public class HaTestSetQuestion {
+    String problemIndex;
+    String questionHtml;
+    QuestionParser questionParser;
+    
+    public HaTestSetQuestion(String guid, String questionHtml) throws Exception {
+        this.problemIndex = guid;
+        this.questionHtml = questionHtml;
+        this.questionParser = new QuestionParser(questionHtml);
+    }
+
+    public String getProblemIndex() {
+        return problemIndex;
+    }
+
+    public void setProblemIndex(String problemIndex) {
+        this.problemIndex = problemIndex;
+    }
+
+    public String getQuestionHtml() {
+        return questionHtml;
+    }
+
+    public void setQuestionHtml(String questionHtml) {
+        this.questionHtml = questionHtml;
+    }
+    
+    public String getSolutionHref() {
+        String path = new ProblemID(problemIndex).getSolutionPath_Full();
+        return path;
+    }
+    
+    public String getProblemNumber() {
+        return new ProblemID(problemIndex).getProblemNumber();
+    }
+    
+    public String getQuestionHtmlProcessed() throws Exception {
+        return questionParser.getProcessedHtml();
+    }
+    
+    public int getCorrectAnswer() {
+        return questionParser.getCorrectAnswer();
+    }
+    
+    
+    /** Provide HTML parsing for the Question.
+     * 
+     * This will find the correct answers stored in the
+     * nested divs with Correct/Incorrect values.  Then 
+     * we remove the answers from the HTML and make value
+     * answer available via getCorrectAnswer method.
+     * 
+     *
+     */
+    static class QuestionParser {
+        static Parser __parser;
+        Node root;
+        int correctAnswer=-1;
+        String questionHtml;
+        String processedHtml;
+        
+        public QuestionParser(String questionHtml) throws Exception {
+            this.questionHtml = questionHtml;
+            if(__parser == null) {
+                __parser = new Parser();
+                __parser.setFeedback(Parser.STDOUT);
+            }
+            
+            processHtml();
+        }
+        
+        public void processHtml() throws Exception {
+            __parser.setInputHTML(questionHtml);
+            __parser.visitAllNodesWith(new NodeVisitor() {
+                int visited=0;
+                @Override
+                public void visitTag(Tag tag) {
+                    if(root == null)
+                        root = tag;
+                    
+                    if(tag.getTagName().equalsIgnoreCase("DIV")) {
+                        if(tag.getAttribute("id") == null && tag.getAttribute("class") == null) {
+                            /** is the answer div */
+                            String text = tag.toPlainTextString();
+                            if(text.indexOf("Correct") > -1) {
+                                correctAnswer=visited;
+                            }
+                            visited++;
+                        }
+                    }
+                }
+            });
+            
+            /** remove the Correct and Incorrect divs ..
+             *  @TODO: why cannot this be done in the Visitor
+             */
+            processedHtml = SbUtilities.replaceSubString(questionHtml,">Correct<","><");
+            processedHtml = SbUtilities.replaceSubString(processedHtml,">Incorrect<","><");
+        }
+
+        public int getCorrectAnswer() {
+            return correctAnswer;
+        }
+
+        public void setCorrectAnswer(int correctAnswer) {
+            this.correctAnswer = correctAnswer;
+        }
+
+        public String getProcessedHtml() {
+            return processedHtml;
+        }
+
+        public void setProcessedHtml(String processedHtml) {
+            this.processedHtml = processedHtml;
+        }
+    }
+}
