df35212309201bf1782ce0849a8c55fa6fb01992
Casey Rodgers <grodgers@gmail.com>
2010-04-18 18:11:15 +0000

    Recactored base RPC into cm_rpc module for easy reuse of RPC command infrastructure.

---
+++ src/main/java/hotmath/gwt/cm_rpc/client/CmRpc.java
@@ -0,0 +1,10 @@
+package hotmath.gwt.cm_rpc.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+
+public class CmRpc implements EntryPoint {
+    public void onModuleLoad() {
+        System.out.println("Catchup Math Test Startup");
+    }
+}
