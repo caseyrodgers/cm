f77d3be47437d3e1020f5ab1db007ca439221bdc
Casey Rodgers <grodgers@gmail.com>
2010-04-28 00:24:27 +0000

    Incomplete - taskBare Bones Mobile

---
+++ src/main/java/hotmath/gwt/cm_mobile/client/MobileUser.java
@@ -0,0 +1,33 @@
+package hotmath.gwt.cm_mobile.client;
+
+public class MobileUser {
+    int uid,testId,testSegment;
+    public MobileUser() {
+    }
+    
+    public MobileUser(int uid, int testId,int testSegment) {
+        this.uid = uid;
+        this.testId = testId;
+        this.testSegment = testSegment;
+    }
+    public int getTestSegment() {
+        return testSegment;
+    }
+
+    public void setTestSegment(int testSegment) {
+        this.testSegment = testSegment;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+    public int getTestId() {
+        return testId;
+    }
+    public void setTestId(int testId) {
+        this.testId = testId;
+    }
+}
