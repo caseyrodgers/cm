9ff61d9199086cc5b122feb9d966416f0b588f75
Bob Hall <rfhall3@yahoo.com>
2010-05-07 07:37:40 +0000

    add showChildrenOutsideTree (WO 835)

--- src/main/java/hotmath/gwt/cm_tools/client/model/CmTreeModel.java
+++ src/main/java/hotmath/gwt/cm_tools/client/model/CmTreeModel.java
@@ -8,6 +8,8 @@ public class CmTreeModel extends BaseTreeModel implements Serializable {
 
 	private static final long serialVersionUID = -8370354125432687765L;
 	
+	private Boolean showChildrenOutsideTree = false;
+	
 	public CmTreeModel() {}
 	
 	public CmTreeModel(String name) {
@@ -21,5 +23,13 @@ public class CmTreeModel extends BaseTreeModel implements Serializable {
 	public String getName() {
 		return get("name");
 	}
+
+	public Boolean showChildrenOutsideTree() {
+		return showChildrenOutsideTree;
+	}
 	
+	public void setShowChildrenOutsideTree(Boolean showChildrenOutsideTree) {
+		this.showChildrenOutsideTree = showChildrenOutsideTree;
+	}
+
 }
