879f5d24797cd735e76aa9d4bcae2195b2250625
Bob Hall <rfhall3@yahoo.com>
2010-05-10 08:02:59 +0000

    add ProgramSection, get section/segment count from ha_test_def (WO 835)

--- src/main/java/hotmath/gwt/cm_admin/server/model/CmProgramListingDao.java
+++ src/main/java/hotmath/gwt/cm_admin/server/model/CmProgramListingDao.java
@@ -5,6 +5,7 @@ import hotmath.gwt.cm_rpc.client.model.program_listing.ProgramChapter;
 import hotmath.gwt.cm_rpc.client.model.program_listing.ProgramChapterAll;
 import hotmath.gwt.cm_rpc.client.model.program_listing.ProgramLesson;
 import hotmath.gwt.cm_rpc.client.model.program_listing.ProgramListing;
+import hotmath.gwt.cm_rpc.client.model.program_listing.ProgramSection;
 import hotmath.gwt.cm_rpc.client.model.program_listing.ProgramSubject;
 import hotmath.gwt.cm_rpc.client.model.program_listing.ProgramType;
 import hotmath.gwt.cm_rpc.client.rpc.CmArrayList;
@@ -19,6 +20,7 @@ import hotmath.util.sql.SqlUtilities;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -51,19 +53,44 @@ public class CmProgramListingDao {
      * @throws Exception
      */
     private ProgramType createProficiencyProgramType(final Connection conn, String type) throws Exception {
-        CmProgram[] programs = { CmProgram.PREALG_PROF, CmProgram.ALG1_PROF, CmProgram.GEOM_PROF, CmProgram.ALG2_PROF};
+    	
+    	HaTestDefDao dao = new HaTestDefDao();
+    	List<Integer> testDefIds = new ArrayList<Integer>();
+    	testDefIds.add(CmProgram.PREALG_PROF.getDefId());
+    	testDefIds.add(CmProgram.ALG1_PROF.getDefId());
+    	testDefIds.add(CmProgram.GEOM_PROF.getDefId());
+    	testDefIds.add(CmProgram.ALG2_PROF.getDefId());
+    	
+    	List<HaTestDef> testDefs = dao.getTestDefs(conn, testDefIds);
+    	
         ProgramType pt = new ProgramType(type);
+
+        for (HaTestDef testDef : testDefs) {
+            ProgramSubject ps = new ProgramSubject();
+            ps.setName(testDef.getSubjectId());
+            pt.getProgramSubjects().add(ps);
+
+            ProgramChapter chapter = new ProgramChapterAll();
+            ps.getChapters().add(chapter);
+            
+            List<ProgramSection> list = buildSectionListForProficiencyTestDef(testDef);
+            chapter.setSections(list);
+        }
+/*   	
+        CmProgram[] programs = { CmProgram.PREALG_PROF, CmProgram.ALG1_PROF, CmProgram.GEOM_PROF, CmProgram.ALG2_PROF};
         for(CmProgram program: programs) {
             ProgramSubject ps = new ProgramSubject();
             ps.setName(program.getSubject());
+            pt.getProgramSubjects().add(ps);
+
             ProgramChapter chapters = new ProgramChapterAll();
             ps.getChapters().add(chapters);
-            pt.getProgramSubjects().add(ps);
         }
+*/
         return pt;
     }
     
-    /** Return distinct lessons for a given testDefIf modified with testConfig
+    /** Return distinct lessons for a given testDefId modified with testConfig
      * 
      * @param conn
      * @param testDefId
@@ -71,8 +98,9 @@ public class CmProgramListingDao {
      * @return
      * @throws Exception
      */
-    public CmList<ProgramLesson> getLessonsFor(final Connection conn, int testDefId,int segment, HaTestConfig testConfig) throws Exception {
-        Statement stmt=null;
+    public CmList<ProgramLesson> getLessonsFor(final Connection conn, int testDefId, int segment, HaTestConfig testConfig) throws Exception {
+        Statement stmt = null;
+        ResultSet rs = null;
         try {
             CmList<ProgramLesson> lessons = new CmArrayList<ProgramLesson>();
             
@@ -84,53 +112,45 @@ public class CmProgramListingDao {
 
             StudentUserProgramModel userProgram = new StudentUserProgramModel();
             userProgram.setTestDef(testDef);
-            List<String> ids = hda.getTestIdsForSegment(conn,userProgram,2,testDef.getTextCode(),testDef.getChapter(),testConfig,0);
+            List<String> pids = hda.getTestIdsForSegment(conn,userProgram,2,testDef.getTextCode(),testDef.getChapter(),testConfig,0);
             
-            /** now get list of lessons assigned to this pids by looking in HM_PROGRAM_LESSONS which is created
+            /** now get list of lessons assigned to these pids by looking in HM_PROGRAM_LESSONS which is created
              *  by the HA_PRESCRIPTION_LOG Deploylet action.  This is a static table that holds information about
-             *  lessons .. and must be rebuild on data changes.
+             *  lessons .. and must be rebuilt on data changes.
              *  
              */
             /** construct pid in list */
             String pidList="";
-            for(String p:ids) {
+            for(String id : pids) {
                 if(pidList.length()>0) pidList += ",";
-                pidList += "'" + p + "'";
+                pidList += "'" + id + "'";
             }
             Map<String,String> map = new HashMap<String,String>(); 
             map.put("PIDLIST", pidList);
             String sql = CmMultiLinePropertyReader.getInstance().getProperty("GET_PROGRAM_LESSONS", map);
             stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(sql);
+            rs = stmt.executeQuery(sql);
             while(rs.next()) {
                 lessons.add(new ProgramLesson(rs.getString("lesson")));
             }
             return lessons;
         }
         finally {
-            SqlUtilities.releaseResources(null,stmt,null);
+            SqlUtilities.releaseResources(rs,stmt,null);
         }
     }
     
+    private List<ProgramSection> buildSectionListForProficiencyTestDef(HaTestDef testDef) {
+    	List<ProgramSection> list = new ArrayList<ProgramSection>();
+    	
+    	int segmentCount = testDef.getTestConfig().getSegmentCount();
+    	
+    	for (int i=1; i<=segmentCount; i++) {
+    		ProgramSection s = new ProgramSection();
+    		s.setName("Section " + i);
+    		list.add(s);
+    	}
+    	return list;
+    }
     
-
-    private ProgramType createProgramType(Connection conn, String type) throws Exception {
-        ProgramType programType = new ProgramType(type);
-
-        String[] subjects = { "Pre-Algebra", "Algebra 1", "Algebra 2", "Geometry" };
-
-        for (String subj : subjects) {
-            ProgramSubject ps = new ProgramSubject();
-            ps.setName(subj);
-            programType.getProgramSubjects().add(ps);
-            
-            ProgramChapter progChap = new ProgramChapterAll();
-            ps.getChapters().add(progChap);
-            for(int i=0;i<30;i++) {
-                progChap.getLessons().add(new ProgramLesson("Lesson #" + i));
-            }
-            
-        }
-        return programType;
-    }    
 }
