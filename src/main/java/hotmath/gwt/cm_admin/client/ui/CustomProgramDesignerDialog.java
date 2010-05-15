33fbabd4b017b78cb03b0c9ba2bfb46178f8ab39
Casey Rodgers <grodgers@gmail.com>
2010-05-15 14:35:43 +0000

    Rearranged labels

--- src/main/java/hotmath/gwt/cm_admin/client/ui/CustomProgramDesignerDialog.java
+++ src/main/java/hotmath/gwt/cm_admin/client/ui/CustomProgramDesignerDialog.java
@@ -183,8 +183,9 @@ public class CustomProgramDesignerDialog extends CmWindow {
         String ledgend = "<div style='position: absolute; top: 2px; left: 0;width: 260px;'>"
                 + "<div style='margin-right: 3px;float: left;' class='pre-alg'>Pre-Algebra</div>"
                 + "<div style='margin-right: 3px;float: left;' class='alg1'>Algebra 1</div>"
-                + "<div style='margin-right: 3px;float: left;' class='geom'>Geometry</div>" + "</div>"
-                + "<div style='margin-right: 3px;float: left;' class='alg2'>Algebra 2</div>";
+                + "<div style='margin-right: 3px;float: left;' class='geom'>Geometry</div>"
+                + "<div style='margin-right: 3px;float: left;' class='alg2'>Algebra 2</div>"
+                + "</div>";
         
 
         getButtonBar().setStyleAttribute("position", "relative");
