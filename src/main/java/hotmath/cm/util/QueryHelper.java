b86f9d3f8d8e41490b26b90e5293b2706f6df3b9
Bob Hall <rfhall3@yahoo.com>
2010-06-04 07:20:02 +0000

    initial commit - utility to build "var in (1,2,3) or var in (4,5,6)" query strings

---
+++ src/main/java/hotmath/cm/util/QueryHelper.java
@@ -0,0 +1,60 @@
+package hotmath.cm.util;
+
+import java.util.List;
+
+public class QueryHelper {
+	
+	/*
+	 * @author bob
+	 */
+	
+	/**
+	 * @param sqlTemplate
+	 * @param vals
+	 * @param name  the column name
+	 * @param limit number of vals per list chunk
+	 * @return SQL with vals in one or more comma separated lists
+	 * @throws Exception
+	 */
+	static public String createInListSQL(String sqlTemplate, List<Integer> vals, String name, Integer limit) throws Exception {
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("(");
+		
+		int count = vals.size(); 
+		int chunks = count / limit;
+		
+		if ((chunks * limit) < count) chunks += 1;
+		
+		int j = 0;
+		for (int i=0; i<chunks; i++) {
+
+			int k = j + limit;
+			if (k > count) k = count;
+
+			boolean first = true;
+			for (; j < k; j++) {
+				
+				if (first) {
+					first = false;
+					sb.append(name).append(" IN (");
+				}
+				else {
+					sb.append(", ");
+				}
+				sb.append(vals.get(j));
+			}
+			sb.append(")");
+			
+			if (k < count) {
+				sb.append(" OR " );
+			}
+		}
+		sb.append(" )");
+		
+		String sql = String.format(sqlTemplate, sb.toString());
+		
+		return sql;
+	}
+
+}
