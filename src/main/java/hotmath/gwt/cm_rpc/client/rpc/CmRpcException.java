215ac5d9b9d8339043692c5ac668a79ba8c78141
Bob Hall <rfhall3@yahoo.com>
2010-06-12 07:38:28 +0000

    e.printStackTrace() --> logger.error(e)

--- src/main/java/hotmath/gwt/cm_rpc/client/rpc/CmRpcException.java
+++ src/main/java/hotmath/gwt/cm_rpc/client/rpc/CmRpcException.java
@@ -1,20 +1,30 @@
 package hotmath.gwt.cm_rpc.client.rpc;
 
+import org.apache.log4j.Logger;
+
 public class CmRpcException extends Exception implements java.io.Serializable {
 
 	String message;
 
+	private static final Logger logger = Logger.getLogger(CmRpcException.class);
+
 	public CmRpcException(Exception e) {
-	    
-	    
+
 	    // make sure stack trace ends up in log
-	    e.printStackTrace();
-	    
+	    logger.error(e);
+
 		message = e.getMessage();
 
 		if(message == null) {
 		    message = e.getClass().getName();
 		}
+	}
+
+	public CmRpcException(String message, Throwable t) {
+		this.message = message;
+
+		// make sure stack trace ends up in log
+	    logger.error(message, t);		
 	}
 
 	public CmRpcException() {
