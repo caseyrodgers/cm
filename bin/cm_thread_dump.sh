cc4737eb696cba037651b1a32686ecaf54a1e706
Bob Hall <rfhall3@yahoo.com>
2010-06-15 08:33:59 +0000

    add messages

--- bin/cm_thread_dump.sh
+++ bin/cm_thread_dump.sh
@@ -3,8 +3,13 @@ export CATALINA_HOME=~hotmath/tomcat2
 
 . $CATALINA_HOME/bin/setenv.sh
 
+echo taking first thread dump...
 kill -3 `cat $CATALINA_PID`
 
+echo waiting 10 seconds
 sleep 10;
 
+echo taking second thread dump...
 kill -3 `cat $CATALINA_PID`
+
+echo DONE
