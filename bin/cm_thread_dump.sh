ed112bb24469c61e093a0b9e52b30d838c3bef11
Bob Hall <rfhall3@yahoo.com>
2010-06-15 10:05:10 +0000

    fix syntax

--- bin/cm_thread_dump.sh
+++ bin/cm_thread_dump.sh
@@ -5,13 +5,13 @@
 
 tc2_pid=`ps -eaf | grep tomcat2 | grep -v grep | grep Bootstrap | awk '{print $2}'`
 
-echo taking first thread dump... ($tc2_pid)
+echo taking first thread dump... $tc2_pid
 kill -3 $tc2_pid
 
 echo waiting 10 seconds
 sleep 10;
 
-echo taking second thread dump... ($tc2_pid)
+echo taking second thread dump... $tc2_pid
 kill -3 $tc2_pid
 
 echo DONE
