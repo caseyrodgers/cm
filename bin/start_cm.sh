741f5903b99adcf911c358c15222cd1e74df90ed
Bob Hall <rfhall3@yahoo.com>
2010-06-15 10:08:05 +0000

    restore usage of 'run'

--- bin/start_cm.sh
+++ bin/start_cm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export CATALINA_HOME=~hotmath/tomcat2
 while true;do
-  $CATALINA_HOME/bin/catalina.sh start >> $CATALINA_HOME/logs/catalina.out 2>&1
+  $CATALINA_HOME/bin/catalina.sh run >> $CATALINA_HOME/logs/catalina.out 2>&1
   sleep 5;
 done;
