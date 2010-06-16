6e0ff70d39c70f0ffd868e04603a70ac70bc66dd
Bob Hall <rfhall3@yahoo.com>
2010-06-16 06:25:51 +0000

    accept PID on command line, use full path to 'ps', 'grep', and 'awk' commands

--- bin/cm_thread_dump.sh
+++ bin/cm_thread_dump.sh
@@ -3,15 +3,24 @@
 
 #. $CATALINA_HOME/bin/setenv.sh
 
-tc2_pid=`ps -eaf | grep tomcat2 | grep -v grep | grep Bootstrap | awk '{print $2}'`
+tc2_pid=$1;
+
+if [ -z $tc2_pid ]; then
+    tc2_pid=`/usr/bin/ps -eaf | grep tomcat2 | /usr/bin/grep -v grep | /usr/bin/grep Bootstrap | /usr/bin/awk '{print $2}'`
+
+    if [ -z $tc2_pid ]; then
+        echo "can't determine CM's tomcat instance PID, exiting..."
+        exit 1;
+    fi
+fi
 
 echo taking first thread dump... $tc2_pid
-kill -3 $tc2_pid
+/bin/kill -3 $tc2_pid
 
 echo waiting 10 seconds
 sleep 10;
 
 echo taking second thread dump... $tc2_pid
-kill -3 $tc2_pid
+/bin/kill -3 $tc2_pid
 
 echo DONE
