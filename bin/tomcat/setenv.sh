2013c392ff3930bf11d74a8b63d5b1ec9ff087e6
Bob Hall <rfhall3@yahoo.com>
2010-06-19 07:01:15 +0000

    fix JAVA_OPTS

--- bin/tomcat/setenv.sh
+++ bin/tomcat/setenv.sh
@@ -2,4 +2,4 @@
 #
 export CATALINA_OPTS=-Dcm.log.base=~hotmath/tomcat2
 
-JAVA_OPTS=-server -Xms1024m -Xmx1024m -XX:MaxPermSize=128m -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000 -XX:+UseConcMarkSweepGC
+export JAVA_OPTS="-server -Xms1024m -Xmx1024m -XX:MaxPermSize=128m -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000 -XX:+UseConcMarkSweepGC"
