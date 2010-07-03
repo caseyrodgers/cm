8ed12bc6e945305c4c0ef56a0ed0057eb9d320e6
Casey Rodgers <grodgers@gmail.com>
2010-07-03 00:31:24 +0000

    updated to gwt-2.0.4

--- libs/add.sh
+++ libs/add.sh
@@ -5,8 +5,8 @@ mvn install:install-file -DgroupId=smartsprites -DartifactId=smartsprites-with-d
 mvn install:install-file -DgroupId=com.yahoo.platform.yui -DartifactId=yuicompressor -Dversion=2.3.6 -Dpackaging=jar -Dfile=yuicompressor-2.4.2.jar
 
 
-mvn install:install-file -DgroupId=com.google.gwt -DartifactId=gwt-servlet -Dversion=gwt-2.1.0.M1 -Dpackaging=jar -Dfile=gwt-2.1.0.M1/gwt-servlet.jar 
-mvn install:install-file -DgroupId=com.google.gwt -DartifactId=gwt-user -Dversion=gwt-2.1.0.M1 -Dpackaging=jar -Dfile=gwt-2.1.0.M1/gwt-user.jar
+mvn install:install-file -DgroupId=com.google.gwt -DartifactId=gwt-servlet -Dversion=2.0.4 -Dpackaging=jar -Dfile=gwt-2.0.4/gwt-servlet.jar 
+mvn install:install-file -DgroupId=com.google.gwt -DartifactId=gwt-user -Dversion=2.0.4 -Dpackaging=jar -Dfile=gwt-2.0.4/gwt-user.jar
 
 
 mvn install:install-file -DgroupId=htmlparser -DartifactId=htmlparser -Dversion=1.6 -Dpackaging=jar -Dfile=htmlparser.jar -DgeneratePom=true
