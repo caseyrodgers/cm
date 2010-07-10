ababe8406c04b9bbb555b745004bd8e9db4d45a0
Bob Hall <rfhall3@yahoo.com>
2010-07-10 07:51:05 +0000

    add another ShowAlert() override

--- src/main/java/hotmath/gwt/cm_tools/client/CatchupMathTools.java
+++ src/main/java/hotmath/gwt/cm_tools/client/CatchupMathTools.java
@@ -74,10 +74,21 @@ public class CatchupMathTools implements EntryPoint {
         });
     }
 
+    static public void showAlert(String title, String msg, final CmAsyncRequest callback, final String arg) {
+        EventBus.getInstance().fireEvent(new CmEvent(EventType.EVENT_TYPE_MODAL_WINDOW_OPEN));
+        MessageBox.alert(title, msg, new Listener<MessageBoxEvent>() {
+            public void handleEvent(MessageBoxEvent be) {
+                if (callback != null)
+                    callback.requestComplete(arg);
+                
+                EventBus.getInstance().fireEvent(new CmEvent(EventType.EVENT_TYPE_MODAL_WINDOW_CLOSED));
+            }
+        });
+    }
+
     static public void showAlert(String msg, final CmAsyncRequest callback) {
         showAlert("Info", msg, callback);
     }
-
     
     static public String FEEDBACK_MESSAGE="<p>Please use the Help button to send us feedback.</p>";
     
