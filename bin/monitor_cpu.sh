a5c2759b50b65ed779fa3414118c4f7e535315a6
Bob Hall <rfhall3@yahoo.com>
2010-07-19 07:32:35 +0000

    fix typo

--- bin/monitor_cpu.sh
+++ bin/monitor_cpu.sh
@@ -18,26 +18,16 @@ case $1 in
     httpd)
     
         hpids=`ps -eo pid,user,args | grep httpd | grep apache | grep -v grep | awk '{print $1}'`;
-        echo $hpids;
-        
         pidarg=`echo $hpids | sed 's/ /,/g'`;
-        echo $pidarg;
-        
         hcpu=`pidstat 1 1 -p $pidarg | grep Average | grep -v CPU | awk '{print $6}' | sed 's/\.//'`;
-        echo $hcpu;
         
         cpu=0;
         for c in $hcpu; do
-            echo $c;
-            cpu=`expr $p + $c`;
+            cpu=`expr $cpu + $c`;
         done;
-        echo $cpu;
     
         w=`expr $cpu / 100`;
-        echo w: $w
-        
         d=`expr $cpu % 100`;
-        echo d: $d;
         
         if [ $d -ge 10 ]; then
             echo -n $w.$d;
