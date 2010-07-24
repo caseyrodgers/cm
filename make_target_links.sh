34ed5ee755538c4b00f10cc11bd8a4a158c4e073
Casey Rodgers <grodgers@gmail.com>
2010-07-24 14:50:04 +0000

    *** empty log message ***

--- make_target_links.sh
+++ make_target_links.sh
@@ -4,4 +4,7 @@ ln -s ~/work/hotmath2/web/tutor target/cm-1.0-SNAPSHOT
 ln -s ~/work/hotmath2/web/images target/cm-1.0-SNAPSHOT
 ln -s ~/work/hotmath2/web/help target/cm-1.0-SNAPSHOT
 ln -s ~/work/hotmath2/web/collab target/cm-1.0-SNAPSHOT
+ln -s ~/work/hotmath2/web/learning_activities target/cm-1.0-SNAPSHOT
+ln -s ~/work/hotmath2/web/hotmath_help target/cm-1.0-SNAPSHOT
+ln -s ~/work/hotmath2/web/util/input_widgets target/cm-1.0-SNAPSHOT/util/input_widgets
 
