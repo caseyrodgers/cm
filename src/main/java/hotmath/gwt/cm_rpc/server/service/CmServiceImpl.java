5a136ff6401f169e5c78ecc43f14bb19b66722f5
Bob Hall <rfhall3@yahoo.com>
2010-10-04 08:36:00 +0000

    Override doUnexpectedFailure() to log  userId and  userType (CM-143)

--- src/main/java/hotmath/gwt/cm_rpc/server/service/CmServiceImpl.java
+++ src/main/java/hotmath/gwt/cm_rpc/server/service/CmServiceImpl.java
@@ -6,14 +6,18 @@ import hotmath.gwt.cm_rpc.client.rpc.CmService;
 import hotmath.gwt.cm_rpc.client.rpc.Response;
 import hotmath.gwt.cm_rpc.server.rpc.ActionDispatcher;
 
+import hotmath.gwt.cm_rpc.client.ClientInfo;
+import hotmath.cm.util.ClientInfoHolder;
+
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+import org.apache.log4j.Logger;
 
 public class CmServiceImpl extends RemoteServiceServlet implements CmService {
 
-    /**
-     * 
-     */
     private static final long serialVersionUID = -3449687645592040570L;
+    
+    private static final Logger LOGGER = Logger.getLogger(CmServiceImpl.class);
 
     /**
      * Central processing of RPC calls. All RPC calls are one-to-one with a
@@ -24,5 +28,22 @@ public class CmServiceImpl extends RemoteServiceServlet implements CmService {
     @Override
     public <T extends Response> T execute(Action<T> action) throws CmRpcException {
         return ActionDispatcher.getInstance().execute(action);
+    }
+    
+    /**
+     * Override doUnexpectedFailure() so we can log userId and userType
+     * 
+     */
+    @Override
+    public void doUnexpectedFailure(Throwable t) {
+        super.doUnexpectedFailure(t);
+
+        ClientInfo ci = ClientInfoHolder.get();
+        if (ci != null) {
+            LOGGER.error(String.format("*** userId: %d, userType: %s", ci.getUserId(), ci.getUserType()), t);
+        }
+        else {
+            LOGGER.error("*** ClientInfo is NULL ***", t);
+        }
     }
 }
