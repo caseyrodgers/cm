8626765591ae3087006e2403953a8a14c06a5dc2
Casey Rodgers <grodgers@gmail.com>
2010-10-29 19:50:58 +0000

    CM-125

--- src/main/java/hotmath/gwt/cm_mobile_shared/client/CmResourceViewerImplVideo.java
+++ src/main/java/hotmath/gwt/cm_mobile_shared/client/CmResourceViewerImplVideo.java
@@ -23,7 +23,16 @@ public class CmResourceViewerImplVideo extends Composite implements CmMobileReso
         
         // String html = "<object width=\"480\" height=\"320\"><param name=\"movie\" value=\"http://www.youtube.com/v/s80J2dAUUyI?fs=1&amp;hl=en_US\"></param><param name=\"allowFullScreen\" value=\"true\"></param><param name=\"allowscriptaccess\" value=\"always\"></param><embed src=\"http://www.youtube.com/v/s80J2dAUUyI?fs=1&amp;hl=en_US\" type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\"480\" height=\"385\"></embed></object>";
         // String html = "<iframe height='335' width='360' src='/gwt-resources/mobile_view_video.html'></iframe>";
-        String html = "<iframe class='youtube-player' type='text/html' width='320' height='190' src='http://www.youtube.com/v/HcYVjwqm7rA' frameborder='0'></iframe>";
+        // String html = "<iframe class='youtube-player' type='text/html' width='320' height='190' src='http://www.youtube.com/v/HcYVjwqm7rA' frameborder='0'></iframe>";
+        String html = 
+            "  <object width='320' height='190'>" +
+            "    <param name='movie' value='http://www.youtube.com/v/gB7MxBIN6Ok' />" +
+            "    <param name='allowFullScreen' value='true' />" +
+            "" +
+            "    <param name='allowscriptaccess' value='always' />" +
+            "    <embed src='http://www.youtube.com/v/HcYVjwqm7rA' type='application/x-shockwave-flash' " +
+            "            allowscriptaccess='always' allowfullscreen='true' width='320' height='190'></embed>" +
+            "  </object>";
         mainPanel.add(new HTML(html));
     }
     
