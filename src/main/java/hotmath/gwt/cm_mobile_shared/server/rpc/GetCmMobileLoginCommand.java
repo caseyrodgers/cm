1869225670e699c4beb5559ffb166246a2c67d51
Bob Hall <rfhall3@yahoo.com>
2010-10-29 05:55:09 +0000

    getStudentModelBase() takes less time than getStudentModel()

--- src/main/java/hotmath/gwt/cm_mobile_shared/server/rpc/GetCmMobileLoginCommand.java
+++ src/main/java/hotmath/gwt/cm_mobile_shared/server/rpc/GetCmMobileLoginCommand.java
@@ -30,7 +30,7 @@ public class GetCmMobileLoginCommand implements ActionHandler<GetCmMobileLoginAc
         if(basicUser.getUserType() != UserType.STUDENT)
             throw new CmException("Invalid user type: " + basicUser.getUserType());
         
-        StudentModelI sm = new CmStudentDao().getStudentModel(conn, basicUser.getUserKey());
+        StudentModelI sm = new CmStudentDao().getStudentModelBase(conn, basicUser.getUserKey());
         StudentActiveInfo active = new CmStudentDao().loadActiveInfo(conn, sm.getUid());
         CmMobileUser mobileUser = new CmMobileUser(sm.getUid(),active.getActiveTestId(),active.getActiveSegment(),active.getActiveSegmentSlot(),active.getActiveRunId());
         
