b4179ce2c34d1fd4b308cfbf0110c5513eab0ceb
Casey Rodgers <grodgers@gmail.com>
2010-10-31 20:12:57 +0000

    *** empty log message ***

---
+++ src/main/java/hotmath/gwt/cm_mobile2/public/make_links.sh
@@ -0,0 +1,17 @@
+ln -s ../gwt-resources .
+ln -s ../resources .
+
+
+ln -s ../../../../hotmath2/web/js ./
+ln -s ../../../../hotmath2/web/css ./
+ln -s ../../../../hotmath2/web/tutor ./
+ln -s ../../../../hotmath2/web/images ./
+ln -s ../../../../hotmath2/web/help ./
+ln -s ../../../../hotmath2/web/collab ./
+ln -s ../../../../hotmath2/web/learning_activities ./
+ln -s ../../../../hotmath2/web/hotmath_help ./
+
+mkdir util
+cd util
+ln -s ../../../../../hotmath2/web/util/input_widgets ./
+cd ..
