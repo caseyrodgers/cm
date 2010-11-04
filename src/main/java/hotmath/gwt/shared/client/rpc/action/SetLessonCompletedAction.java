c8df636e60ce80423ccd4dbf0b47c0e19a6fdd98
Bob Hall <rfhall3@yahoo.com>
2010-11-04 06:53:45 +0000

    refactor MarkPrescriptionLessonAsViewedAction --> SetLessonCompletedAction

--- src/main/java/hotmath/gwt/shared/client/rpc/action/SetLessonCompletedAction.java
+++ src/main/java/hotmath/gwt/shared/client/rpc/action/SetLessonCompletedAction.java
@@ -3,16 +3,16 @@ package hotmath.gwt.shared.client.rpc.action;
 import hotmath.gwt.cm_rpc.client.rpc.Action;
 import hotmath.gwt.cm_rpc.client.rpc.RpcData;
 
-public class MarkPrescriptionLessonAsViewedAction implements Action<RpcData>{
+public class SetLessonCompletedAction implements Action<RpcData>{
     
     String lesson;
     Integer runId;
     Integer session;
     
 
-    public MarkPrescriptionLessonAsViewedAction() {}
+    public SetLessonCompletedAction() {}
     
-    public MarkPrescriptionLessonAsViewedAction(String lesson, Integer runId, Integer session) {
+    public SetLessonCompletedAction(String lesson, Integer runId, Integer session) {
         this.lesson = lesson;
         this.runId = runId;
         this.session = session;
