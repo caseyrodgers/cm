b9c0fcba1e4636b2392af8f2214ccd9ee80ca95f
Bob Hall <rfhall3@yahoo.com>
2010-11-06 05:44:18 +0000

    temp restore from Attic to avoid out-of-date client RPC errors

---
+++ src/main/java/hotmath/gwt/shared/server/service/command/MarkPrescriptionLessonAsViewedCommand.java
@@ -0,0 +1,33 @@
+package hotmath.gwt.shared.server.service.command;
+
+import hotmath.gwt.cm_rpc.client.rpc.Action;
+import hotmath.gwt.cm_rpc.client.rpc.Response;
+import hotmath.gwt.cm_rpc.client.rpc.RpcData;
+import hotmath.gwt.cm_rpc.server.rpc.ActionHandler;
+import hotmath.gwt.shared.client.rpc.action.MarkPrescriptionLessonAsViewedAction;
+import hotmath.testset.ha.HaTestRunDao;
+
+import java.sql.Connection;
+
+
+/** Mark the given Prescription Lesson as being viewed
+ * 
+ * @author casey
+ *
+ */
+public class MarkPrescriptionLessonAsViewedCommand implements ActionHandler<MarkPrescriptionLessonAsViewedAction, RpcData>{
+
+    @Override
+    public RpcData execute(Connection conn, MarkPrescriptionLessonAsViewedAction action) throws Exception {
+        
+        new HaTestRunDao().markLessonAsCompleted(conn, action.getRunId(),action.getLesson());
+        return new RpcData("status=OK");
+    }
+
+    @Override
+    public Class<? extends Action<? extends Response>> getActionType() {
+        // TODO Auto-generated method stub
+        return MarkPrescriptionLessonAsViewedAction.class;
+    }
+
+}
