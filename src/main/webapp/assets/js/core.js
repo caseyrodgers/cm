f922b1b757d6b2591903a8ad14b2538041a46287
Casey Rodgers <grodgers@gmail.com>
2010-11-17 19:03:22 +0000

    no message

--- src/main/webapp/assets/js/core.js
+++ src/main/webapp/assets/js/core.js
@@ -3,7 +3,6 @@ function $get(x) {
 }
 
 function setupPage() {
-    
     /** setup menu buttons
      * 
      */
@@ -30,4 +29,41 @@ function setupPageLocal() {
     /** empty */
 }
 
-setupPage();
+YUI().use('node',function(Y) {
+    Y.on('domready',function() {
+        setupPage();
+    });
+});
+
+
+
+var _overlay=null;
+
+function showMonaMotivationalVideo() {
+    var closer = '<div style="text-align: center;margin-top: 25px;">' +
+                 '<a class="sexybutton sexysimple" href="#" onclick="closeMonaVideo();return false;">Close</a></div>';
+    var html = '<iframe src="/motivational_video/" width="100%" height="250px" scrolling="no" frameborder="no"></iframe>'
+            + closer;
+    
+    YUI().use('anim','overlay',
+                    function(Y) {
+                        _overlay = new Y.Overlay(
+                                {   
+                                    id:"mona-video",
+                                    width : "350px",
+                                    height : "350px",
+                                    centered : true,
+                                    headerContent : "Catchup Math Motivational Video",
+                                    bodyContent : html,
+                                    zIndex : 2,
+                                    visible:true
+                                });
+                        
+                       _overlay.render();
+    });
+}
+
+/** Closed from close button */
+function closeMonaVideo() {
+    _overlay.hide();
+}
---
+++ src/main/webapp/assets/js/teacher-video.js
@@ -0,0 +1,5 @@
+function setupPageLocal() {
+//    var params = {};
+//    params.key = '$a12fd4b15a588479e9e';
+//    flowplayer("player", "/assets/flowplayer/flowplayer.commercial-3.1.5.swf",params);
+}
