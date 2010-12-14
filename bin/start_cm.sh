9755c993fef2e510b07baacd650ba2ec407008fc
Casey Rodgers <grodgers@gmail.com>
2010-12-14 06:09:54 +0000

    *** empty log message ***

--- bin/start_cm.sh
+++ bin/start_cm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export CATALINA_HOME=~hotmath/tomcat
+export CATALINA_HOME=~hotmath/tomcat2
 while true;do
   $CATALINA_HOME/bin/catalina.sh run >> $CATALINA_HOME/logs/catalina.out 2>&1
   sleep 5;
