2f1b52894937275f3eedcefc591791b4c2d591e6
Bob Hall <rfhall3@yahoo.com>
2010-12-16 08:57:40 +0000

    update expired account messages (CM-287)

--- src/main/java/hotmath/cm/login/service/CmExceptionUserExpired.java
+++ src/main/java/hotmath/cm/login/service/CmExceptionUserExpired.java
@@ -1,10 +1,10 @@
 package hotmath.cm.login.service;
 
-import hotmath.gwt.cm_tools.client.data.HaBasicUser;
-import hotmath.gwt.shared.client.util.CmException;
-
 import java.text.Format;
 import java.text.SimpleDateFormat;
+
+import hotmath.gwt.cm_tools.client.data.HaBasicUser;
+import hotmath.gwt.shared.client.util.CmException;
 
 /** Thrown when a user is currently expired
  * 
@@ -13,19 +13,30 @@ import java.text.SimpleDateFormat;
  */
 public class CmExceptionUserExpired extends CmException {
 
-	String message;
-	Format _format = new SimpleDateFormat("dd MMM yyyy");
-	public CmExceptionUserExpired(HaBasicUser user) {
-		if (user.getExpireDate() != null)
-			message = "The entered account expired " + _format.format(user.getExpireDate()) + ".<br/>";
-		else
-			message = "The entered account is not valid.";
+	private static final long serialVersionUID = 4668807820551018668L;
 
-		if (user.getAccountType().equals("ST")) {
-			message += "Please tell the appropriate faculty or administrator.";
-		}
-		if (user.getAccountType().equals("PS")) {
-			message += "We hope you decide to renew!";
+	String message = "";
+
+	Format _format = new SimpleDateFormat("MMM dd yyyy");
+
+	public CmExceptionUserExpired(HaBasicUser user) {
+
+		switch (user.getUserType()) {
+		case ADMIN:
+			message = String.format("Your school account expired on %s.  Please contact your Catchup Math Account Manager to renew.",
+					_format.format(user.getExpireDate()));
+			break;
+		case STUDENT:
+			if (user.getAccountType().equals("ST")) {
+    			message = String.format("Your school account expired on %s.  Please contact the teacher.",
+					_format.format(user.getExpireDate()));
+			}
+			else if (user.getAccountType().equals("PS")) {
+    			message = String.format("Your personal account expired on %s.  We hope you will renew!",
+    					_format.format(user.getExpireDate()));				
+			}
+			break;
+			
 		}
 	}
 	
