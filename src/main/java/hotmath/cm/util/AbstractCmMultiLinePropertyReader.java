2d2b8008b2b2a2f7d65f97f49838ffcbba699d78
Bob Hall <rfhall3@yahoo.com>
2010-12-20 10:07:56 +0000

    initial commit - extracted from CmMultiLinePropertyReader

---
+++ src/main/java/hotmath/cm/util/AbstractCmMultiLinePropertyReader.java
@@ -0,0 +1,99 @@
+package hotmath.cm.util;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
+import sb.util.SbUtilities;
+
+/**
+ * Extracted from CmMultiLinePropertyReader
+ * 
+ * Provides a means of reading in mulit-line property files without having to
+ * provide line continuation escapes.
+ * 
+ * Each entry is marked as follows:
+ * 
+ * .key=THE_KEY_NAME
+ * 
+ * ## comment lines
+ * 
+ * ANY TEXT WITH MULTI LINES MORE TXT dfdf ...
+ * 
+ * 
+ * .key=ANOTHER_KEY
+ * 
+ * The other text until EOF or new .key
+ * 
+ * EOF
+ * 
+ * You can cause a re-read of the propertes by flushing the server via
+ * loading resources/util/cm_system_flush.jsp.
+ * 
+ */
+
+public abstract class AbstractCmMultiLinePropertyReader extends Properties {
+	
+	private static final long serialVersionUID = 6706889922304528657L;
+
+	abstract protected Logger getLogger();
+
+    protected void loadProps(String propFile) {
+
+        try {
+            /**
+             * Read in the multi-line property file
+             * 
+             */
+            InputStream is = getClass().getResourceAsStream(propFile);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line = null;
+
+            String value = "";
+            String name = null;
+    
+            while ((line = br.readLine()) != null) {
+    
+                if (line == null || line.length() == 0 || line.startsWith("#"))
+                    continue;
+    
+                if (line.startsWith(".key=")) {
+                    if (value.length() > 0) {
+                        
+                        /** Make sure this entry does not already exist
+                         * 
+                         */
+                        if(containsKey(name)) {
+                            getLogger().warn("property already exists: " + name);
+                        }
+                        
+                        put(name, value);
+                        value = "";
+                    }
+                    name = line.substring(5);
+                } else {
+                    value += (line + "\n");
+                }
+            }
+    
+            /**
+             * put last value
+             * 
+             */
+            if (value.length() > 0)
+                put(name, value);
+        }
+        catch(Exception e) {
+            getLogger().error("Error reading multi line property file: " + propFile, e);
+        }
+    }
+    
+    public String getProperty(String key, Map<String,String> replacements) {
+        String val = getProperty(key);
+        return SbUtilities.replaceTokens(val, replacements);
+    }
+}
