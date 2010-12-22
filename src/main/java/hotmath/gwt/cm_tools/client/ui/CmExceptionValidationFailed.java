ee6c54e16a2cc8d63d72f4c25c1b59e49fdc6d60
Casey Rodgers <grodgers@gmail.com>
2010-12-22 01:17:06 +0000

    no message

---
+++ src/main/java/hotmath/gwt/cm_tools/client/ui/CmExceptionValidationFailed.java
@@ -0,0 +1,9 @@
+package hotmath.gwt.cm_tools.client.ui;
+
+import hotmath.gwt.shared.client.util.CmException;
+
+class CmExceptionValidationFailed extends CmException {
+    public CmExceptionValidationFailed() {}
+}
+
+
