1e5f2c968b102ff4404b9c7e5ead6ec5adf87ce6
Casey Rodgers <grodgers@gmail.com>
2010-12-24 21:15:53 +0000

    CM-280

--- src/main/java/hotmath/cm/util/service/SolutionDef.java
+++ src/main/java/hotmath/cm/util/service/SolutionDef.java
@@ -4,6 +4,11 @@ import hotmath.HotMathProperties;
 import hotmath.ProblemID;
 import hotmath.cm.util.CatchupMathProperties;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import sb.util.SbUtilities;
+
 public class SolutionDef extends ProblemID {
     public SolutionDef(String pid) {
         super(pid);
@@ -37,4 +42,30 @@ public class SolutionDef extends ProblemID {
         return path;
                
     }
+    
+    public String getCreateNewXml(String createdBy) {
+        Map<String,String> map = new HashMap<String,String>();
+        map.put("DATE", SbUtilities.getDateStamp());
+        map.put("CREATEDBY", createdBy);
+        map.put("BOOK", getBook());
+        map.put("CHAPTER",getChapter());
+        map.put("SECTION",getSection());
+        map.put("SET",getProblemSet());
+        map.put("PROBLEM",getProblemNumber());
+        map.put("PAGE",Integer.toString(getPage()));
+        map.put("PAGE",Integer.toString(getPage()));
+        
+        String result = SbUtilities.replaceTokens(newSolutionXml, map);
+        return result;
+    }
+    
+    String newSolutionXml = 
+        "<?xml version =\"1.0\" encoding=\"UTF-8\"?>" +
+        "  <hmsl version=\"2.0\" date=\"$$DATE$$\">" +
+        "     <problem createdby=\"$$CREATEDBY$$\">" +
+        "        <identification book=\"$$BOOK$$\" chapter=\"$$CHAPTER$$\" section=\"$$SECTION$$\" set=\"$$SET$$\" problemnumber=\"$$PROBLEM$$\" page=\"$$PAGE$$\" />" +
+        "           <statement>The Problem Statement</statement>" +
+        "     </problem>" +
+        "  </hmsl>";
+    
 }
