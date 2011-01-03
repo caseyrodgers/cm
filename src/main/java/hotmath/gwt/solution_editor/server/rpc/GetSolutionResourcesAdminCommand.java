af8b011c91cc4450fd3fb2789415be9f218a8773
Casey Rodgers <grodgers@gmail.com>
2011-01-03 06:11:27 +0000

    *** empty log message ***

--- src/main/java/hotmath/gwt/solution_editor/server/rpc/GetSolutionResourcesAdminCommand.java
+++ src/main/java/hotmath/gwt/solution_editor/server/rpc/GetSolutionResourcesAdminCommand.java
@@ -8,6 +8,7 @@ import hotmath.gwt.cm_rpc.client.rpc.CmList;
 import hotmath.gwt.cm_rpc.client.rpc.Response;
 import hotmath.gwt.cm_rpc.server.rpc.ActionHandler;
 import hotmath.gwt.solution_editor.client.rpc.GetSolutionResourcesAdminAction;
+import hotmath.gwt.solution_editor.client.rpc.GetSolutionResourcesAdminAction.ResourceType;
 import hotmath.gwt.solution_editor.client.rpc.SolutionResource;
 
 import java.io.File;
@@ -23,34 +24,59 @@ public class GetSolutionResourcesAdminCommand implements ActionHandler<GetSoluti
     SolutionDef solutionDef;
     @Override
     public CmList<SolutionResource> execute(Connection conn, GetSolutionResourcesAdminAction action) throws Exception {
-        
+        CmList<SolutionResource> resources = new CmArrayList<SolutionResource>();        
         File solutionPath=null;
-        switch(action.getType()) {
-            case LOCAL:
-                solutionDef = new SolutionDef(action.getPid());
-                solutionPath = new File(solutionDef.getResourcesPath());
-                break;
-                
-            case GLOBAL:
-                solutionPath = new File(CatchupMathProperties.getInstance().getSolutionBase(), "/help/solutions/resources"); 
-                break;
+        if(action.getType() == ResourceType.WIDGET) {
+            getAllRegisteredWidgets(resources);
+        }
+        else {
+            switch(action.getType()) {
+                case LOCAL:
+                    solutionDef = new SolutionDef(action.getPid());
+                    solutionPath = new File(solutionDef.getSolutionPathOnDisk());
+                    break;
+                    
+                case GLOBAL:
+                    solutionPath = new File(CatchupMathProperties.getInstance().getSolutionBase(), "/help/solutions/resources"); 
+                    break;
+            }
+            getSolutionResources(solutionPath, resources);
         }
         
-        CmList<SolutionResource> resources = new CmArrayList<SolutionResource>();
-        getSolutionResources(solutionPath, resources);
         return resources;
     }
 
+    private void getAllRegisteredWidgets(CmList<SolutionResource> resources) throws Exception {
+        SolutionResource widget = new SolutionResource();
+        widget.setFile("The Widget Name");
+        widget.setDisplay("<h1>The Widget Markup</h1><p>This is the example of this widget</p>");
+        widget.setContents("THIS IS THE WIDGET MARKUP");
+        
+        resources.add(widget);
+        
+        widget = new SolutionResource();
+        widget.setFile("The Widget Name 2");
+        widget.setDisplay("<h1>The Widget Markup 2</h1><p>This is the example of this widget 2</p>");
+        widget.setContents("THIS IS THE WIDGET 2 MARKUP");
+        
+        resources.add(widget);
+    }
+    
+    int depth=0;
     private void getSolutionResources(File dir, CmList<SolutionResource> resources) throws Exception {
         if(!dir.exists())
             return;
-        
         for(File kid: dir.listFiles()) {
             if(kid.isDirectory()) {
+                ++depth;
                 getSolutionResources(kid,resources);
             }
             else {
-                if(kid.getName().endsWith(".mathml")) {
+                if(kid.getName().endsWith(".mathml")
+                        || kid.getName().endsWith(".html")
+                        || kid.getName().endsWith(".xml")
+                        || kid.getName().endsWith(".json") 
+                        || kid.getName().endsWith(".js")) {
                     continue;
                 }
                 String urlPath = null;
@@ -60,7 +86,13 @@ public class GetSolutionResourcesAdminCommand implements ActionHandler<GetSoluti
                 else {
                     urlPath = "/help/solutions";  /** global resources */
                 }
-                urlPath += "/resources/" + kid.getName();
+                
+                if(depth > 0) {
+                    urlPath += "/resources/" + kid.getName();
+                }
+                else {
+                    urlPath += "/" + kid.getName();
+                }
                 resources.add(new SolutionResource(kid.getName(), urlPath));
             }
         }
