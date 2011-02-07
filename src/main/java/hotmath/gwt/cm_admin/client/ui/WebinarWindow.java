5cd91fa9362c46605b89bf84989e71811fb63257
Casey Rodgers <grodgers@gmail.com>
2011-02-07 22:32:12 +0000

    CM-397

--- src/main/java/hotmath/gwt/cm_admin/client/ui/WebinarWindow.java
+++ src/main/java/hotmath/gwt/cm_admin/client/ui/WebinarWindow.java
@@ -16,11 +16,12 @@ public class WebinarWindow extends CmWindow {
     String videoPlayerId = "flowPlayer_" + System.currentTimeMillis();
     
     public WebinarWindow() {
-        String swf = "/resources/webinar/CM Webinar draft_controller.swf";
+        String swf = "/assets/webinar/cm_training_video_jan_2011/CM Training video Jan 2011_controller.swf";
 
         setHeading("Teaching with Catchup Math");
         setSize(800,498);
         setResizable(true);
+        setMaximizable(true);
         setModal(false);
         
         SWFSettings s = new SWFSettings();
@@ -30,15 +31,16 @@ public class WebinarWindow extends CmWindow {
          *  if in cache it only plays once.
          *  
          */
-        String flashVars = "autostart=false&thumb=/resources/webinar/FirstFrame.png&thumbscale=45&color=0x1A1A1A,0x1A1A1";
+        String flashVars = "autostart=false&thumb=/assets/webinar/cm_training_video_jan_2011/FirstFrame.png&thumbscale=45&color=0x1A1A1A,0x1A1A1";
         SWFWidget swfWidget = new SWFWidget(swf, "100%", "100%", s);
+
         swfWidget.addFlashVar("config", flashVars);
-        swfWidget.addParam("scale", "scale");
+        swfWidget.addParam("scale", "showall");
         swfWidget.addParam("allowfullscreen", "true");
         swfWidget.addParam("allowscriptaccess", "always");
-        swfWidget.addParam("quality", "high");
+        swfWidget.addParam("quality", "best");
         swfWidget.addParam("cachebusting", "true");
-        swfWidget.addParam("bgcolor", "000000");
+        swfWidget.addParam("bgcolor", "#1a1a1a");
         add(swfWidget);
         
         addButton(new Button("Close", new SelectionListener<ButtonEvent>() {
