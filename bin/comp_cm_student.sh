c7c83d9abe42253cd64350ebb566017d4faaa359
Casey Rodgers <grodgers@gmail.com>
2011-02-18 21:47:55 +0000

    *** empty log message ***

---
+++ bin/comp_cm_student.sh
@@ -0,0 +1,3 @@
+cvs -q update -d -P
+mvn gwt:compile -Dgwt.module=hotmath.gwt.cm.CatchupMath -o
+cp -r src/main/webapp/cm_student target/cm-1.0-SNAPSHOT
---
+++ bin/comp_server.sh
@@ -0,0 +1,10 @@
+cvs -q update -d -P
+mvn compile -o
+cp -r target/classes target/cm-1.0-SNAPSHOT/WEB-INF
+
+
+
+
+
+
+
