7336a1b85ce1f4e0504fab2f155f1ccdc4a700c7
Casey Rodgers <grodgers@gmail.com>
2011-02-18 21:41:55 +0000

    CM-87

---
+++ src/main/java/hotmath/cm/util/FindBug_SkippedQuizzes.java
@@ -0,0 +1,230 @@
+package hotmath.cm.util;
+
+import hotmath.util.HMConnectionPool;
+import hotmath.util.sql.SqlUtilities;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import sb.util.SbUtilities;
+
+/** Identify skipped quizzes by looking a meta data
+ * 
+ * @author casey
+ *
+ */
+public class FindBug_SkippedQuizzes {
+    
+    
+
+    final static Logger __logger = Logger.getLogger(FindBug_SkippedQuizzes.class); 
+    
+    public FindBug_SkippedQuizzes() throws Exception {
+    }
+    
+    public void findSkippedQuizzes(final Connection conn, int adminId) throws Exception {
+        
+        String sqlAids = "select aid from HA_ADMIN";
+        if(adminId > 0) {
+            sqlAids += " WHERE aid = " + adminId;
+        }
+        sqlAids += " order by aid desc";
+        ResultSet rsAids = conn.createStatement().executeQuery(sqlAids);
+        while(rsAids.next()) {
+            int aid = rsAids.getInt("aid");
+
+            testAdminId(conn, aid);
+        }
+    }
+
+    private void testAdminId(final Connection conn, int adminId) throws Exception {
+        __logger.info("Testing admin_id: " + adminId);
+        
+        PreparedStatement ps=null;
+        try {
+            String sql = " select u.uid, p.id as program_id,t.test_id,t.test_segment,r.run_id " +
+                      " from  HA_USER u " +
+                      " join HA_TEST t on t.user_id = u.uid " +
+                      " join CM_USER_PROGRAM p on p.id = t.user_prog_id " +
+                      " left join HA_TEST_RUN r on r.test_id = t.test_id " +
+                      " where u.admin_id = ? " +
+                      " order by u.uid, p.id,t.test_id,t.test_segment ";
+            
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, adminId);
+
+            List<QuizInfo> quizInfos = new ArrayList<QuizInfo>();
+            
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                int userId = rs.getInt("uid");
+                int progId = rs.getInt("program_id");
+                int testId = rs.getInt("test_id");
+                int testSegment = rs.getInt("test_segment");
+                int runId = rs.getInt("run_id");
+            
+                quizInfos.add(new QuizInfo(userId, progId, testId, testSegment, runId));
+            }
+            
+            
+            performChecks(quizInfos);
+            
+        }
+        finally {
+            SqlUtilities.releaseResources(null,ps,null);
+        }
+    }
+    
+    private void performChecks(List<QuizInfo> qs) throws Exception {
+        try {
+            for(int qi=0;qi<qs.size()-1;qi++) {
+                QuizInfo q1 = qs.get(qi);
+                
+                /** only check quizzes that have been taken */
+                if(q1.getRunId() > 0) {
+                    
+                    /** skip resets
+                     * 
+                     */
+                    if(q1.getTestSegment() < 2) {
+                        continue;
+                    }
+                    
+                    /** if we find any of the following this record 
+                     *  make sense:
+                     *  
+                     * 0. a unrun test
+                     * 1. a different user, program
+                     * 2. an equal or greater test segment
+                     */
+                    
+                    for(int qi2=qi+1;qi2<qs.size();qi2++) {
+                        QuizInfo q2 = qs.get(qi2);
+                        if(q2.getRunId() == 0) {
+                            /** unrun test ...
+                             * 
+                             */
+                        }
+                        else if(q2.getUserId() != q1.getUserId()) {
+                            /** different user
+                             * 
+                             */
+                            break;
+                        }
+                        else if(q2.getProgId() != q1.getProgId()) {
+                            /** different program
+                             * 
+                             */
+                            break;
+                        }
+                        else {
+                            /** segment must be either equal or one more than q1
+                             * 
+                             */
+                            int distance = (q2.getTestSegment() - q1.getTestSegment());
+                            if(distance > 1) {
+                                __logger.warn("Found Skipped Quiz: " + q1);
+                            }
+                            else {
+                               // __logger.warn("record OK: " + q1);
+                                break;  // no error
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        catch(Exception e) {
+            __logger.error("Error performing tests", e);
+            throw e;
+        }
+    }
+    
+    class QuizInfo {
+        int userId;
+        int progId;
+        int testId;
+        int testSegment;
+        int runId;
+        
+        public QuizInfo(int userId, int progId, int testId, int testSegment, int runId) {
+            this.userId = userId;
+            this.progId = progId;
+            this.testId = testId;
+            this.testSegment = testSegment;
+            this.runId = runId;
+        }
+
+        public int getUserId() {
+            return userId;
+        }
+
+        public void setUserId(int userId) {
+            this.userId = userId;
+        }
+
+        public int getProgId() {
+            return progId;
+        }
+
+        public void setProgId(int progId) {
+            this.progId = progId;
+        }
+
+        public int getTestId() {
+            return testId;
+        }
+
+        public void setTestId(int testId) {
+            this.testId = testId;
+        }
+
+        public int getTestSegment() {
+            return testSegment;
+        }
+
+        public void setTestSegment(int testSegment) {
+            this.testSegment = testSegment;
+        }
+
+        public int getRunId() {
+            return runId;
+        }
+
+        public void setRunId(int runId) {
+            this.runId = runId;
+        }
+
+        @Override
+        public String toString() {
+            return "QuizInfo [userId=" + userId + ", progId=" + progId + ", testId=" + testId + ", testSegment="
+                    + testSegment + ", runId=" + runId + "]";
+        }
+        
+    }
+    
+    static public void main(String as[]) {
+        
+        SbUtilities.addOptions(as);
+        int adminId = SbUtilities.getInt(SbUtilities.getOption(null, "aid"));
+        Connection conn=null;
+        try {
+            conn = HMConnectionPool.getConnection();
+            new FindBug_SkippedQuizzes().findSkippedQuizzes(conn, adminId);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            SqlUtilities.releaseResources(null,null,conn);
+            
+            System.exit(0);
+        }
+    }
+
+}
