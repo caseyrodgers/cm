6c4065599182861b708a25d96ca9190617a8a809
Casey Rodgers <grodgers@gmail.com>
2011-02-25 21:40:18 +0000

    'CM-444'

--- src/main/java/hotmath/gwt/cm/client/ui/context/SampleDemoMessageWindow.java
+++ src/main/java/hotmath/gwt/cm/client/ui/context/SampleDemoMessageWindow.java
@@ -1,5 +1,11 @@
 package hotmath.gwt.cm.client.ui.context;
 
+import hotmath.gwt.shared.client.eventbus.CmEvent;
+import hotmath.gwt.shared.client.eventbus.EventBus;
+import hotmath.gwt.shared.client.eventbus.EventType;
+
+import com.extjs.gxt.ui.client.event.WindowEvent;
+import com.extjs.gxt.ui.client.event.WindowListener;
 import com.extjs.gxt.ui.client.widget.Html;
 import com.extjs.gxt.ui.client.widget.Window;
 
@@ -14,8 +20,7 @@ public class SampleDemoMessageWindow extends Window {
 
     public SampleDemoMessageWindow() {
         
-        
-
+        EventBus.getInstance().fireEvent(new CmEvent(EventType.EVENT_TYPE_MODAL_WINDOW_OPEN, this));
         
         String msg = "<p>Thank you for trying a Catchup Math Pre-Algebra Session.</p>  "
                 + "<p>We offer complete programs for Pre-Algebra, Algebra 1, Geometry, " +
@@ -24,7 +29,7 @@ public class SampleDemoMessageWindow extends Window {
                 + "<a href='http://catchupmath.com/colleges.html'>Colleges</a>, "
                 + "or <a href='http://catchupmath.com/students.html'>Students</a> pages "
                 + "for more information.</p>";
-        setClosable(false);
+        setClosable(false);   // end of the road
         setStyleName("demo-complete-window");
         setHeight(140);
         setWidth(400);
@@ -32,5 +37,12 @@ public class SampleDemoMessageWindow extends Window {
         setModal(true);
         add(new Html(msg));
         setVisible(true);
+        
+        addWindowListener(new WindowListener() {
+            @Override
+            public void windowHide(WindowEvent we) {
+                EventBus.getInstance().fireEvent(new CmEvent(EventType.EVENT_TYPE_MODAL_WINDOW_CLOSED, this));
+            }
+        });
     }
 }
