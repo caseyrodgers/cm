0909044c1282cdb4c244699c5374474e524c03a2
Bob Hall <rfhall3@yahoo.com>
2011-03-04 07:19:14 +0000

    adjust indentation

--- src/main/java/hotmath/cm/login/service/LoginService.java
+++ src/main/java/hotmath/cm/login/service/LoginService.java
@@ -43,208 +43,208 @@ import sb.util.SbUtilities;
  */
 public class LoginService extends HttpServlet {
 
-        private static final long serialVersionUID = 8258701625476557059L;
+	private static final long serialVersionUID = 8258701625476557059L;
 
-        static final Logger LOGGER = Logger.getLogger(LoginService.class.getName());
-    static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	static final Logger LOGGER = Logger.getLogger(LoginService.class.getName());
+	static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-        protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String user = req.getParameter("user");
-        String pwd = req.getParameter("pwd");
-        String action = req.getParameter("action");
-        String key = req.getParameter("key");
-        boolean isDebug=false;
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String user = req.getParameter("user");
+		String pwd = req.getParameter("pwd");
+		String action = req.getParameter("action");
+		String key = req.getParameter("key");
+		boolean isDebug=false;
 
-        try {
+		try {
 
-            if(action == null)
-                action = "";
-            else if(action.equals("sample")) {
-                user = "catchup_demo";
-                action = "login";
-            }
-            else if(action.startsWith("auto_test")) {
-                user="catchup_demo";
-            }
-            else if(action.equals("LCOM")) {
-                /** if LCOM action, then handle separately
-                 *
-                 */
-                handleLcomLogin(req, resp);
-                return;
-            }
+			if(action == null)
+				action = "";
+			else if(action.equals("sample")) {
+				user = "catchup_demo";
+				action = "login";
+			}
+			else if(action.startsWith("auto_test")) {
+				user="catchup_demo";
+			}
+			else if(action.equals("LCOM")) {
+				/** if LCOM action, then handle separately
+				 *
+				 */
+				handleLcomLogin(req, resp);
+				return;
+			}
 
-            isDebug = req.getParameter("debug") != null;
-                int uid=SbUtilities.getInt(req.getParameter("uid"));
-                String type=req.getParameter("type");
+			isDebug = req.getParameter("debug") != null;
+			int uid=SbUtilities.getInt(req.getParameter("uid"));
+			String type=req.getParameter("type");
 
-                LoginAction loginAction = new LoginAction();
-                loginAction.setDebug(isDebug);
-                loginAction.setKey(key);
-                loginAction.setPassword(pwd);
-                loginAction.setType(type);
-                loginAction.setUid(uid);
-                loginAction.setUserName(user);
+			LoginAction loginAction = new LoginAction();
+			loginAction.setDebug(isDebug);
+			loginAction.setKey(key);
+			loginAction.setPassword(pwd);
+			loginAction.setType(type);
+			loginAction.setUid(uid);
+			loginAction.setUserName(user);
 
-                HaBasicUser cmUser = ActionDispatcher.getInstance().execute(loginAction);
+			HaBasicUser cmUser = ActionDispatcher.getInstance().execute(loginAction);
 
-                if (cmUser == null) {
-                req.getRequestDispatcher("/gwt-resources/login_error.jsp").forward(req, resp);
-                LOGGER.warn(String.format("*** Login failed for user: %s, pwd: %s", user, pwd));
-                return;
-                }
+			if (cmUser == null) {
+				req.getRequestDispatcher("/gwt-resources/login_error.jsp").forward(req, resp);
+				LOGGER.warn(String.format("*** Login failed for user: %s, pwd: %s", user, pwd));
+				return;
+			}
 
-            HaLoginInfo loginInfo = new HaLoginInfo(cmUser);
+			HaLoginInfo loginInfo = new HaLoginInfo(cmUser);
 
-            /** either redirect this user to CM using current information
-             * or return JSON describing login info.
-             */
-            if(action.equals("login")) {
-                throw new Exception("Not Supported");
-            }
-            else if(action.equals("auto_test")) {
-                /** redirect and connect to the CM student,passing auto_test parameter
-                 *
-                 */
-                String params = "type=auto_test&debug=true&uid=" + loginInfo.getUserId();
-                if(SbUtilities.getBoolean(req.getParameter("test_rpp_only"))) {
-                    params += "&test_rpp_only=true";
-                }
+			/** either redirect this user to CM using current information
+			 * or return JSON describing login info.
+			 */
+			if(action.equals("login")) {
+				throw new Exception("Not Supported");
+			}
+			else if(action.equals("auto_test")) {
+				/** redirect and connect to the CM student,passing auto_test parameter
+				 *
+				 */
+				String params = "type=auto_test&debug=true&uid=" + loginInfo.getUserId();
+				if(SbUtilities.getBoolean(req.getParameter("test_rpp_only"))) {
+					params += "&test_rpp_only=true";
+				}
 
-                String urlWithSessionID = "http://" + getCmServer(req) + "/loginService?" + params;
-                resp.sendRedirect(urlWithSessionID);
-            }
-            else if(action.equals("auto_test_net")) {
-                /** redirect and connect to the CM student,passing auto_test parameter
-                 *
-                 */
-                String urlWithSessionID = "http://" + getCmServer(req) + "/loginService?type=auto_test_net&debug=true&uid=" + loginInfo.getUserId();
-                resp.sendRedirect(urlWithSessionID);
-            }
-            else {
+				String urlWithSessionID = "http://" + getCmServer(req) + "/loginService?" + params;
+				resp.sendRedirect(urlWithSessionID);
+			}
+			else if(action.equals("auto_test_net")) {
+				/** redirect and connect to the CM student,passing auto_test parameter
+				 *
+				 */
+				String urlWithSessionID = "http://" + getCmServer(req) + "/loginService?type=auto_test_net&debug=true&uid=" + loginInfo.getUserId();
+				resp.sendRedirect(urlWithSessionID);
+			}
+			else {
 
-                if (cmUser.getExpireDate() == null) {
-                        /**
-                         * this should never happen...
-                         */
+				if (cmUser.getExpireDate() == null) {
+					/**
+					 * this should never happen...
+					 */
 
-                        throw new CmInvalidAccountException();
-                }
+					throw new CmInvalidAccountException();
+				}
 
-                /** Check for expired account.  But, allow debug users to bypass
-                 *
-                 */
-                if(!isDebug && cmUser.getExpireDate().getTime() < System.currentTimeMillis()) {
-                        throw new CmExceptionUserExpired(cmUser);
-                }
+				/** Check for expired account.  But, allow debug users to bypass
+				 *
+				 */
+				if(!isDebug && cmUser.getExpireDate().getTime() < System.currentTimeMillis()) {
+					throw new CmExceptionUserExpired(cmUser);
+				}
 
-                /** add shared user info into session and forward to specific
-                 *  launcher JSP pages that will encapsulate the login/init data
-                 *  in the returned HTML as JSON JS vars.
-                 *
-                 *  Each type (cm_student, cm_admin) will have different data
-                 *  required to initialize, which is defined in each launch.jsp file.
-                 *
-                 */
-                        StringBuilder sb = new StringBuilder();
-                        sb.append("{status:'").append((cmUser.isExpired())?"Expired":"OK");
-                        sb.append("', key:'").append(loginInfo.getKey());
-                        sb.append("', type:'").append(loginInfo.getType());
-                        sb.append("', accountType: '").append(cmUser.getAccountType());
-                        sb.append("', userId:").append(loginInfo.getUserId());
-                        String dateStr = (cmUser.getExpireDate() != null) ? dateFormat.format((cmUser.getExpireDate())) : "n/a";
-                        sb.append(", expireDate: '").append(dateStr);
-                        sb.append("', loginMsg: '").append((cmUser.getLoginMessage() != null)?cmUser.getLoginMessage():"NONE");
-                        sb.append("',  loginName: '").append((cmUser.getLoginName() != null)?cmUser.getLoginName():"");
-                        sb.append("',  email: '").append((cmUser.getEmail() != null)?cmUser.getEmail():"");
-                        sb.append("',  partner: '").append(cmUser.getPartner() != null?cmUser.getPartner():"");
-                        sb.append("' }");
-                        req.getSession().setAttribute("jsonizedLoginInfo", sb.toString());
+				/** add shared user info into session and forward to specific
+				 *  launcher JSP pages that will encapsulate the login/init data
+				 *  in the returned HTML as JSON JS vars.
+				 *
+				 *  Each type (cm_student, cm_admin) will have different data
+				 *  required to initialize, which is defined in each launch.jsp file.
+				 *
+				 */
+				StringBuilder sb = new StringBuilder();
+				sb.append("{status:'").append((cmUser.isExpired())?"Expired":"OK");
+				sb.append("', key:'").append(loginInfo.getKey());
+				sb.append("', type:'").append(loginInfo.getType());
+				sb.append("', accountType: '").append(cmUser.getAccountType());
+				sb.append("', userId:").append(loginInfo.getUserId());
+				String dateStr = (cmUser.getExpireDate() != null) ? dateFormat.format((cmUser.getExpireDate())) : "n/a";
+				sb.append(", expireDate: '").append(dateStr);
+				sb.append("', loginMsg: '").append((cmUser.getLoginMessage() != null)?cmUser.getLoginMessage():"NONE");
+				sb.append("',  loginName: '").append((cmUser.getLoginName() != null)?cmUser.getLoginName():"");
+				sb.append("',  email: '").append((cmUser.getEmail() != null)?cmUser.getEmail():"");
+				sb.append("',  partner: '").append(cmUser.getPartner() != null?cmUser.getPartner():"");
+				sb.append("' }");
+				req.getSession().setAttribute("jsonizedLoginInfo", sb.toString());
 
-                        req.getSession().setAttribute("securityKey", loginInfo.getKey());
+				req.getSession().setAttribute("securityKey", loginInfo.getKey());
 
-                        ClientInfo clientInfo = new ClientInfo();
-                        clientInfo.setUserId(loginInfo.getUserId());
+				ClientInfo clientInfo = new ClientInfo();
+				clientInfo.setUserId(loginInfo.getUserId());
 
-                if(cmUser instanceof HaAdmin) {
-                        clientInfo.setUserType(UserType.ADMIN);
+				if(cmUser instanceof HaAdmin) {
+					clientInfo.setUserType(UserType.ADMIN);
 
-                        req.getSession().setAttribute("loginInfo", loginInfo);
-                        req.getRequestDispatcher("/cm_admin/launch.jsp").forward(req, resp);
-                }
-                else {
-                        clientInfo.setUserType(UserType.STUDENT);
-                        UserInfo userInfo = ActionDispatcher.getInstance().execute(new GetUserInfoAction(loginInfo.getUserId(),loginInfo.getLoginName()));
-                        String jsonizedUserInfo = Jsonizer.toJson(userInfo);
-                        req.getSession().setAttribute("jsonizedUserInfo", jsonizedUserInfo);
+					req.getSession().setAttribute("loginInfo", loginInfo);
+					req.getRequestDispatcher("/cm_admin/launch.jsp").forward(req, resp);
+				}
+				else {
+					clientInfo.setUserType(UserType.STUDENT);
+					UserInfo userInfo = ActionDispatcher.getInstance().execute(new GetUserInfoAction(loginInfo.getUserId(),loginInfo.getLoginName()));
+					String jsonizedUserInfo = Jsonizer.toJson(userInfo);
+					req.getSession().setAttribute("jsonizedUserInfo", jsonizedUserInfo);
 
-                        req.getRequestDispatcher("/cm_student/launch.jsp").forward(req, resp);
-                }
+					req.getRequestDispatcher("/cm_student/launch.jsp").forward(req, resp);
+				}
 
-                req.getSession().setAttribute("clientInfo", clientInfo);
-            }
-        }
-        catch(Exception e) {
-                req.getSession().setAttribute("exception", e);
-                req.getRequestDispatcher("/gwt-resources/login_error.jsp").forward(req, resp);
-            LOGGER.error(String.format("*** Login failed for user: %s, pwd: %s", user, pwd), e);
-        }
-    }
+				req.getSession().setAttribute("clientInfo", clientInfo);
+			}
+		}
+		catch(Exception e) {
+			req.getSession().setAttribute("exception", e);
+			req.getRequestDispatcher("/gwt-resources/login_error.jsp").forward(req, resp);
+			LOGGER.error(String.format("*** Login failed for user: %s, pwd: %s", user, pwd), e);
+		}
+	}
 
-        /** return the server name to use for the CM Student app.
-         * This is complicated due to different deployment configurations.
-         *
-         * @param req
-         * @return
-         */
-        private String getCmServer(HttpServletRequest req) {
-            String sn = req.getServerName();
-            int port = req.getServerPort();
+	/** return the server name to use for the CM Student app.
+	 * This is complicated due to different deployment configurations.
+	 *
+	 * @param req
+	 * @return
+	 */
+	private String getCmServer(HttpServletRequest req) {
+		String sn = req.getServerName();
+		int port = req.getServerPort();
 
-            //if(sn.indexOf("kattare") > -1)
-            //    port = 80;
+		//if(sn.indexOf("kattare") > -1)
+		//    port = 80;
 
-            return sn + (port != 80?":" + port:"");
-        }
+		return sn + (port != 80?":" + port:"");
+	}
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doPost(req, resp);
-    }
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req, resp);
+	}
 
-    /** perform login for LCOM integration
-     *
-     * @param req
-     * @param resp
-     * @throws ServletException
-     * @throws IOException
-     *
-     * http://catchupmath.com/?lcomtype=student&sp1=Catchup%2520Math%2520%20Teacher&sp2=a38f84d9-29c3-4080-a5e7-5996e7b1fdb8&sp3=e1f83d2e-4e64-4342-b94f-bd7095c44c00&sp4=a38f84d9-29c3-4080-a5e7-5996e7b1fdb8
-     */
-    private void handleLcomLogin(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-        try {
-                String type = req.getParameter("lcomtype");
-                String urlToRedirectTo=null;
-                if(type.equals("student")) {
-                        String firstLast = URLDecoder.decode(req.getParameter("sp1"),"UTF-8");
-                        String userId = req.getParameter("sp2");
-                        String courseId = URLDecoder.decode(req.getParameter("sp3"),"UTF-8");
-                        String teacherId = req.getParameter("sp4");
-                        urlToRedirectTo = LcomManager.loginStudent(new LcomStudentSignup(firstLast, userId, courseId, teacherId,0));
-                }
-                else if(type.equals("teacher")) {
-                        String firstLast = URLDecoder.decode(req.getParameter("tp1"),"UTF-8");
-                        String teacherId = req.getParameter("tp2");
-                        String email = req.getParameter("tp3"); // null for now
-                        String district = URLDecoder.decode(req.getParameter("tp4"),"UTF-8");
-                        String zip = req.getParameter("tp5"); // null for now
-                        urlToRedirectTo = LcomManager.loginTeacher(new LcomTeacherSignup(firstLast, teacherId, email, district, zip));
-                }
-                resp.sendRedirect(urlToRedirectTo);
-        }
-        catch(Exception e) {
-                req.getSession().setAttribute("exception", e);
-                req.getRequestDispatcher("/lcom/login_error.jsp").forward(req, resp);
-        }
+	/** perform login for LCOM integration
+	 *
+	 * @param req
+	 * @param resp
+	 * @throws ServletException
+	 * @throws IOException
+	 *
+	 * http://catchupmath.com/?lcomtype=student&sp1=Catchup%2520Math%2520%20Teacher&sp2=a38f84d9-29c3-4080-a5e7-5996e7b1fdb8&sp3=e1f83d2e-4e64-4342-b94f-bd7095c44c00&sp4=a38f84d9-29c3-4080-a5e7-5996e7b1fdb8
+	 */
+	private void handleLcomLogin(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		try {
+			String type = req.getParameter("lcomtype");
+			String urlToRedirectTo=null;
+			if(type.equals("student")) {
+				String firstLast = URLDecoder.decode(req.getParameter("sp1"),"UTF-8");
+				String userId = req.getParameter("sp2");
+				String courseId = URLDecoder.decode(req.getParameter("sp3"),"UTF-8");
+				String teacherId = req.getParameter("sp4");
+				urlToRedirectTo = LcomManager.loginStudent(new LcomStudentSignup(firstLast, userId, courseId, teacherId,0));
+			}
+			else if(type.equals("teacher")) {
+				String firstLast = URLDecoder.decode(req.getParameter("tp1"),"UTF-8");
+				String teacherId = req.getParameter("tp2");
+				String email = req.getParameter("tp3"); // null for now
+				String district = URLDecoder.decode(req.getParameter("tp4"),"UTF-8");
+				String zip = req.getParameter("tp5"); // null for now
+				urlToRedirectTo = LcomManager.loginTeacher(new LcomTeacherSignup(firstLast, teacherId, email, district, zip));
+			}
+			resp.sendRedirect(urlToRedirectTo);
+		}
+		catch(Exception e) {
+			req.getSession().setAttribute("exception", e);
+			req.getRequestDispatcher("/lcom/login_error.jsp").forward(req, resp);
+		}
 
-    }
+	}
 }
