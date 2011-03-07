b1e03dd3c00020d8583b5a8a3ef7b13a5e5d84ae
Bob Hall <rfhall3@yahoo.com>
2011-03-07 09:42:01 +0000

    move from hotmath.testset.ha

--- src/main/java/hotmath/gwt/shared/client/util/CmExceptionLoginAlreadyConsumed.java
+++ src/main/java/hotmath/gwt/shared/client/util/CmExceptionLoginAlreadyConsumed.java
@@ -1,11 +1,12 @@
-package hotmath.testset.ha;
+package hotmath.gwt.shared.client.util;
+
 
 /** Thrown when a login is attempted with an already verified login key
  *  
  * @author casey
  *
  */
-public class CmExceptionLoginAlreadyConsumed extends Exception {
+public class CmExceptionLoginAlreadyConsumed extends CmUserException {
     
     public CmExceptionLoginAlreadyConsumed(String msg) {
         super(msg);
