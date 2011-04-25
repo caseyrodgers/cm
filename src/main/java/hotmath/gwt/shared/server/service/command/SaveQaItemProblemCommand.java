426ca771dadb14a3124c6915c737177146ba9561
Casey Rodgers <grodgers@gmail.com>
2011-04-25 18:00:54 +0000

    new QA problems now send an email

--- src/main/java/hotmath/gwt/shared/server/service/command/SaveQaItemProblemCommand.java
+++ src/main/java/hotmath/gwt/shared/server/service/command/SaveQaItemProblemCommand.java
@@ -9,11 +9,27 @@ import hotmath.gwt.cm_rpc.server.rpc.ActionHandler;
 
 import java.sql.Connection;
 
+import sb.mail.SbMailManager;
+
 public class SaveQaItemProblemCommand implements ActionHandler<SaveQaItemProblemAction, RpcData>{
 
     @Override
     public RpcData execute(Connection conn, SaveQaItemProblemAction action) throws Exception {
         boolean saved = new CmQaDao().saveQaItemProblem(conn, action.getUserName(), action.getItem(), action.getProblem());
+        
+        
+        /** send email message with contents */
+        String subject="CM QA Program: " + action.getItem() + " (" + action.getUserName() + ")";
+        String message = action.getProblem();
+        
+        try {
+            String asTo[] = {"casey@hotmath.com","qa@hotmath.com"};
+            SbMailManager.getInstance().sendMessage(subject, message,asTo,"admin@hotmath.com");
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        
         return new RpcData("status=" + (saved?"OK":"Not Saved"));
     }
 
