e923f440ea36e6d97a2520f6da9a864f3b8b0ad0
Casey Rodgers <grodgers@gmail.com>
2011-05-07 15:40:36 +0000

    *** empty log message ***

--- bin/comp_cm_qa.sh
+++ bin/comp_cm_qa.sh
@@ -1,2 +1,4 @@
 mvn gwt:compile -Dgwt.module=hotmath.gwt.cm_qa.CmQa -o
+cp -r src/main/webapp/cm_qa target/cm-1.0-SNAPSHOT
+
 
--- bin/gcompile.sh
+++ bin/gcompile.sh
@@ -1,4 +1,4 @@
-cvs -q update -d -P
+#cvs -q update -d -P
 mvn compile gwt:compile -o jar:jar
 cp -r src/main/webapp/cm_*  target/cm-1.0-SNAPSHOT
 
