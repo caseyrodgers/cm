bb6c1d13fd53b90e9c4099e26848db50ad24c3e0
Casey Rodgers <grodgers@gmail.com>
2011-05-13 18:01:50 +0000

    Prevent multiple logins

--- src/main/webapp/assets/js/login.js
+++ src/main/webapp/assets/js/login.js
@@ -1,18 +1,55 @@
-/** validate login attemp.  If both user/pass are
+/** validate login attempt.  If both user/pass are
  *  set send to server, otherwise show error and
  *  do not send to server.
  *  
  * @returns {Boolean}
  */
+var _isLoggingIn=false;
 function doLogin() {
+    if(_isLoggingIn)
+        return false;
+
     var user = document.getElementById("fld_user").value;
     var pass = document.getElementById("fld_pwd").value;
     if (!user || !pass) {
         showLoginInvalid();
         return false;
     } else {
+        // disable the login button to not allow repeats
+        _isLoggingIn = true;
+        document.getElementById('login_submit').disabled = 'disabled';
+        document.getElementById('login_busy').innerHTML = 'Logging you in, please wait...';
         return true;
     }
+}
+
+var _overlay;
+
+function showLoggingInDialog(msg, title) {
+    if (_overlay) {
+        _overlay.hide();
+    }
+    var closer = '<div style="text-align: center;padding-bottom: 10px;">'
+            + '<a class="sexybutton sexysimple" href="#" onclick="closeGeneralDialog();return false;">Close</a></div>';
+
+    closer = '';
+
+    var html = '<div style="padding: 10px;">' + msg + '</div>' + closer;
+    var head = title;
+    YUI().use('overlay', function(Y) {
+        _overlay = new Y.Overlay({
+            modal : true,
+            width : "320px",
+            height: "200px",
+            centered : true,
+            headerContent : head,
+            bodyContent : html,
+            zIndex : 2,
+            visible : true
+        });
+
+        _overlay.render();
+    });
 }
 
 function showLoginInvalid() {
