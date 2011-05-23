5954cf6a6e74731b2099815c7dadde92c27f9cf8
Bob Hall <rfhall3@yahoo.com>
2011-05-23 07:36:10 +0000

    initial commit

---
+++ src/test/hotmath/testset/ha/HaUserExtendedDao_Test.java
@@ -0,0 +1,31 @@
+package hotmath.testset.ha;
+
+import hotmath.gwt.cm.server.CmDbTestCase;
+
+import hotmath.spring.SpringManager;
+import hotmath.testset.ha.HaUserExtendedDao;
+
+public class HaUserExtendedDao_Test extends CmDbTestCase {
+
+    HaUserExtendedDao dao;
+    SpringManager manager;
+    
+    public HaUserExtendedDao_Test(String name) {
+        super(name);
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        manager = SpringManager.getInstance();
+        dao = (HaUserExtendedDao)manager.getBeanFactory().getBean("hotmath.testset.ha.HaUserExtendedDao");
+    }
+    
+    public void testResyncUserExtendedLessonStatusForUid() throws Exception {
+        int uid = setupDemoAccount();
+        
+        dao.resyncUserExtendedLessonStatusForUid(conn, uid);
+        assertTrue(true);
+    }    
+    
+
+}
