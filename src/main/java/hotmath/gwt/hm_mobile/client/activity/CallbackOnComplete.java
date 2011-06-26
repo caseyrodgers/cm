8972a8b94c756608e0d5fa2993d5d1c32ada3537
Casey Rodgers <grodgers@gmail.com>
2011-06-26 20:51:04 +0000

    *** empty log message ***

---
+++ src/main/java/hotmath/gwt/hm_mobile/client/activity/CallbackOnComplete.java
@@ -0,0 +1,11 @@
+package hotmath.gwt.hm_mobile.client.activity;
+
+/** Interface used to provide callback when 
+ *  asynchronous operations complete.
+ *  
+ * @author casey
+ *
+ */
+public interface CallbackOnComplete {
+	void isComplete();
+}
