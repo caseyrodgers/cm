cc544e51889a56be867d20972724a3ad24641383
Casey Rodgers <grodgers@gmail.com>
2011-07-12 04:03:00 +0000

    *** empty log message ***

--- src/main/java/hotmath/gwt/hm_mobile/client/AboutDialog.java
+++ src/main/java/hotmath/gwt/hm_mobile/client/AboutDialog.java
@@ -17,7 +17,7 @@ public class AboutDialog extends DialogBox  {
 	
 	public AboutDialog() {
 		super(true);
-		setSize("300px", "400px");
+		setSize("300px", "250px");
 		setText("About Hotmath Mobile");		
 		setGlassEnabled(true);
 		setWidget(uiBinder.createAndBindUi(this));
