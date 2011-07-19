29b437ea38deaf154e9b9aeb3d86b85d669c5378
Casey Rodgers <grodgers@gmail.com>
2011-07-19 04:17:16 +0000

    HM-89

---
+++ src/main/java/hotmath/gwt/hm_mobile/client/event/ResetListSelections.java
@@ -0,0 +1,26 @@
+package hotmath.gwt.hm_mobile.client.event;
+
+import hotmath.gwt.cm_mobile_shared.client.event.ResetListSelectionsEventHandler;
+import hotmath.gwt.cm_mobile_shared.client.util.GenericTextTag;
+
+import com.google.gwt.event.shared.GwtEvent;
+
+public class ResetListSelections extends GwtEvent<ResetListSelectionsEventHandler> {
+	
+	GenericTextTag<String> tag;
+	public static Type<ResetListSelectionsEventHandler> TYPE = new Type<ResetListSelectionsEventHandler>();
+
+	public ResetListSelections(GenericTextTag<String> tag) {
+		this.tag = tag;
+	}
+
+	@Override
+	public Type<ResetListSelectionsEventHandler> getAssociatedType() {
+		return TYPE;
+	}
+
+	@Override
+	protected void dispatch(ResetListSelectionsEventHandler handler) {
+		handler.resetSelections(tag);
+	}
+}
