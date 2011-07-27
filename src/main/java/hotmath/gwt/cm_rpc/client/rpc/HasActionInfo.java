c1a62a688019fec33c38bd17ed43902397ec325d
Casey Rodgers <grodgers@gmail.com>
2011-07-27 23:03:23 +0000

    IN PROGRESS - issue HM-95: Trifcoization of Mobile UI http://admin.catchupmath.com:8081/browse/HM-95

---
+++ src/main/java/hotmath/gwt/cm_rpc/client/rpc/HasActionInfo.java
@@ -0,0 +1,10 @@
+package hotmath.gwt.cm_rpc.client.rpc;
+
+/** Marks an Action as having meta data describing the action
+ * 
+ * @author casey
+ *
+ */
+public interface HasActionInfo {
+	ActionInfo getActionInfo();
+}
