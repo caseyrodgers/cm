c08f002234e95bf66ad1b4b189791aaf0072b06f
Casey Rodgers <grodgers@gmail.com>
2011-07-28 00:10:57 +0000

    ''

--- src/main/java/hotmath/gwt/cm_rpc/client/rpc/ActionBase.java
+++ src/main/java/hotmath/gwt/cm_rpc/client/rpc/ActionBase.java
@@ -1,19 +1,19 @@
 package hotmath.gwt.cm_rpc.client.rpc;
 
 public class ActionBase implements HasActionInfo {
-	
-	ActionInfo _actionInfo = new ActionInfo();
 
-	@Override
-	public ActionInfo getActionInfo() {
-		return _actionInfo;
-	}
+        ActionInfo _actionInfo = new ActionInfo();
 
-	public void setActionInfo(ActionInfo actionInfo) {
-		this._actionInfo = actionInfo;
-	}
-	
-	public void setActionInfo(ActionType type) {
-		this._actionInfo = new ActionInfo(type);
-	}
+        @Override
+        public ActionInfo getActionInfo() {
+                return _actionInfo;
+        }
+
+        public void setActionInfo(ActionInfo actionInfo) {
+                this._actionInfo = actionInfo;
+        }
+
+        public void setActionInfo(ActionType type) {
+                this._actionInfo = new ActionInfo(type);
+        }
 }
