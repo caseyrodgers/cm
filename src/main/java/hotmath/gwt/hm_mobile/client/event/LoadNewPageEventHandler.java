a150c1bea75a0c82460e1903633ea5d100117b9a
Casey Rodgers <grodgers@gmail.com>
2011-08-15 23:34:31 +0000

    *** empty log message ***

--- src/main/java/hotmath/gwt/hm_mobile/client/event/LoadNewPageEventHandler.java
+++
@@ -1,9 +0,0 @@
-package hotmath.gwt.hm_mobile.client.event;
-
-import hotmath.gwt.cm_mobile_shared.client.page.IPage;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface LoadNewPageEventHandler extends EventHandler {
-	void loadPage(IPage page);
-}
--- src/main/java/hotmath/gwt/hm_mobile/client/event/SystemIsBusyEventHandler.java
+++
@@ -1,8 +0,0 @@
-package hotmath.gwt.hm_mobile.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-
-public interface SystemIsBusyEventHandler extends EventHandler {
-	void showIsBusy(boolean trueFalse);
-}
