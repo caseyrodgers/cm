4e936e9294d16b5ce31ed65a4ad90859ae8b798c
Casey Rodgers <grodgers@gmail.com>
2011-08-17 23:51:23 +0000

    *** empty log message ***

---
+++ src/main/java/hotmath/gwt/cm_rpc/client/rpc/SaveWhiteboardDataAction.java
@@ -0,0 +1,79 @@
+package hotmath.gwt.cm_rpc.client.rpc;
+
+
+/** Save a single Whiteboard command for this user
+ * 
+ * @author casey
+ *
+ */
+public class SaveWhiteboardDataAction implements Action<RpcData> {
+
+    Integer uid;
+    Integer rid;
+    String pid;
+    String commandData;
+    
+    static public enum CommandType{DRAW,CLEAR};
+    CommandType commandType;
+    
+    public SaveWhiteboardDataAction() {}
+     
+    
+    public SaveWhiteboardDataAction(int uid, int runId, String pid, CommandType commandType, String commandData) {
+        this.uid = uid;
+        this.rid = runId;
+        this.pid = pid;
+        this.commandType = commandType;
+        this.commandData = commandData;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public Integer getRid() {
+        return rid;
+    }
+
+    public void setRid(Integer rid) {
+        this.rid = rid;
+    }
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+
+    public CommandType getCommandType() {
+        return commandType;
+    }
+
+    
+    public void setCommandType(CommandType commandType) {
+        this.commandType = commandType;
+    }
+
+
+    public String getCommandData() {
+        return commandData;
+    }
+
+    public void setCommandData(String commandData) {
+        this.commandData = commandData;
+    }
+    
+
+    @Override
+    public String toString() {
+        return "SaveWhiteboardDataAction [commandDataLen=" + (commandData!=null?commandData.length():-1) + ", commandType=" + commandType + ", pid=" + pid
+                + ", rid=" + rid + ", uid=" + uid + "]";
+    }
+}
