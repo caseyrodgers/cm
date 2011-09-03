696e53c2c1ae0bcc96314d1c9ae2a474f6a4cc6f
Casey Rodgers <grodgers@gmail.com>
2011-09-03 03:31:22 +0000

    *** empty log message ***

--- src/main/java/hotmath/gwt/cm_mobile3/client/InitialMessage.java
+++ src/main/java/hotmath/gwt/cm_mobile3/client/InitialMessage.java
@@ -1,4 +1,4 @@
-package hotmath.gwt.cm_mobile3.client;
+gpackage hotmath.gwt.cm_mobile3.client;
 
 import com.google.code.gwt.storage.client.Storage;
 import com.google.gwt.core.client.GWT;
@@ -12,55 +12,55 @@ import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class InitialMessage extends DialogBox {
-	
+
     private static InitialMessageUiBinder uiBinder = GWT.create(InitialMessageUiBinder.class);
 
     interface InitialMessageUiBinder extends UiBinder<Widget, InitialMessage> {
     }
 
-	
-	public InitialMessage() {
-		super(true);
-		setSize("300px", "200px");
-		setText("Welcome to Hotmath Mobile!");		
-		setGlassEnabled(true);
-		setWidget(uiBinder.createAndBindUi(this));
-		
-		setAnimationEnabled(true);
-		setAutoHideEnabled(true);
-		
-		setVisible(true);
-		
-		addCloseHandler(new CloseHandler<PopupPanel>() {
-			@Override
-			public void onClose(CloseEvent<PopupPanel> event) {
-				setHasSeen();
-			}
-		});
-	}
-	
-	public void showCentered() {
-		center();
-	}
-	
-	@UiHandler("closeButton")
-	protected void onCloseButton(ClickEvent ce) {
-		this.hide();
-	}
-	
-	/** return true if this user has seen the message
-	 * 
-	 * @return
-	 */
-	static public boolean hasBeenSeen() {
-		Storage storage = Storage.getLocalStorage();
-		String im = storage.getItem("initial_message");
-		return (im != null) ;
-	}
-	
-	public void setHasSeen() {
-		if(Storage.isSupported()) {
-			Storage.getLocalStorage().setItem("initial_message", "true");
-		}
-	}
+
+        public InitialMessage() {
+                super(true);
+                setSize("300px", "200px");
+                setText("Welcome to Hotmath Mobile!");
+                setGlassEnabled(true);
+                setWidget(uiBinder.createAndBindUi(this));
+
+                setAnimationEnabled(true);
+                setAutoHideEnabled(true);
+
+                setVisible(true);
+
+                addCloseHandler(new CloseHandler<PopupPanel>() {
+                        @Override
+                        public void onClose(CloseEvent<PopupPanel> event) {
+                                setHasSeen();
+                        }
+                });
+        }
+
+        public void showCentered() {
+                center();
+        }
+
+        @UiHandler("closeButton")
+        protected void onCloseButton(ClickEvent ce) {
+                this.hide();
+        }
+
+        /** return true if this user has seen the message
+         *
+         * @return
+         */
+        static public boolean hasBeenSeen() {
+                Storage storage = Storage.getLocalStorage();
+                String im = storage.getItem("initial_message");
+                return (im != null) ;
+        }
+
+        public void setHasSeen() {
+                if(Storage.isSupported()) {
+                        Storage.getLocalStorage().setItem("initial_message", "true");
+                }
+        }
 }
