579ead0b9cad06db415db30c15981b2a429c7572
Casey Rodgers <grodgers@gmail.com>
2011-10-18 15:56:01 +0000

    added config to view tutor to add json

--- src/main/java/hotmath/gwt/solution_editor/client/SolutionViewerFrame.java
+++ src/main/java/hotmath/gwt/solution_editor/client/SolutionViewerFrame.java
@@ -6,8 +6,11 @@ import hotmath.gwt.cm_rpc.client.rpc.RpcData;
 import hotmath.gwt.solution_editor.client.rpc.ServerFlusherAction;
 
 import com.extjs.gxt.ui.client.event.ButtonEvent;
+import com.extjs.gxt.ui.client.event.Listener;
+import com.extjs.gxt.ui.client.event.MessageBoxEvent;
 import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.widget.Html;
+import com.extjs.gxt.ui.client.widget.MessageBox;
 import com.extjs.gxt.ui.client.widget.Window;
 import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
@@ -23,11 +26,24 @@ import com.google.gwt.user.client.ui.Frame;
  */
 public class SolutionViewerFrame extends Window {
     String pid;
+    static String _config;
     public SolutionViewerFrame(String pid) {
         this.pid = pid;
         setSize(600,600);
         setHeading("Solution Viewer: " + pid);
         
+        getHeader().addTool(new Button("Configure",new SelectionListener<ButtonEvent>() {
+            @Override
+            public void componentSelected(ButtonEvent ce) {
+                MessageBox mb = MessageBox.prompt("Solution Config", "Enter solution config JSON (" + _config + ")");
+                mb.addCallback(new Listener<MessageBoxEvent>() {
+                    @Override
+                    public void handleEvent(MessageBoxEvent be) {
+                        _config = be.getValue();
+                    }
+                });
+            }
+        }));
 
         setLayout(new FitLayout());
         addButton(new Button("Close", new SelectionListener<ButtonEvent>() {
@@ -58,7 +74,11 @@ public class SolutionViewerFrame extends Window {
     }
     
     private void showFrame() {
-        Frame frame = new Frame("/gwt-resources/solution_editor/tutor_view.html?pid=" + pid);
+        String url = "/gwt-resources/solution_editor/tutor_view.html?pid=" + pid;
+        if(_config != null && _config.length() > 0) {
+            url += "&config=" + _config;
+        }
+        Frame frame = new Frame(url);
         frame.setSize("100%", "99%");
         DOM.setElementProperty(frame.getElement(), "frameBorder", "no"); // disable
         DOM.setElementPropertyInt(frame.getElement(), "border", 0); // disable
