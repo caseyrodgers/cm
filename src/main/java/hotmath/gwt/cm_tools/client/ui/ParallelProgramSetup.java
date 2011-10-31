95e4ddb2ebcc91f3f2147d9d2b215a2a6627aacf
Bob Hall <rfhall3@yahoo.com>
2011-10-31 10:32:54 +0000

    CM-611

--- src/main/java/hotmath/gwt/cm_tools/client/ui/ParallelProgramSetup.java
+++ src/main/java/hotmath/gwt/cm_tools/client/ui/ParallelProgramSetup.java
@@ -3,13 +3,12 @@ package hotmath.gwt.cm_tools.client.ui;
 import hotmath.gwt.cm_rpc.client.rpc.RpcData;
 import hotmath.gwt.cm_tools.client.CatchupMathTools;
 import hotmath.gwt.cm_tools.client.model.CmAdminModel;
+import hotmath.gwt.cm_tools.client.model.ParallelProgramModel;
 import hotmath.gwt.cm_tools.client.model.StudentModel;
+import hotmath.gwt.cm_tools.client.model.StudentModelI;
 import hotmath.gwt.cm_rpc.client.rpc.CmServiceAsync;
 import hotmath.gwt.shared.client.CmShared;
 import hotmath.gwt.shared.client.data.CmAsyncRequest;
-import hotmath.gwt.shared.client.eventbus.CmEvent;
-import hotmath.gwt.shared.client.eventbus.EventBus;
-import hotmath.gwt.shared.client.eventbus.EventType;
 import hotmath.gwt.shared.client.rpc.action.SaveParallelProgramAction;
 import hotmath.gwt.shared.client.util.CmException;
 
@@ -30,69 +29,96 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
  */
 public class ParallelProgramSetup extends RegisterStudent {
 	
-    TextField<String> _nameTag;
-    TextField<String> _passwordTag;
+    TextField<String> nameFld;
+    TextField<String> passwordFld;
+    
+    boolean isSaveOK;
+    Integer parellelProgId;
+    ParallelProgramModel parallelProg;
     
 	private CmAsyncRequest requestCallback;
     
-	public ParallelProgramSetup(CmAsyncRequest callback, CmAdminModel cm) {
-	    super(null, cm);
+	public ParallelProgramSetup(CmAsyncRequest callback, CmAdminModel adminModel) {
+	    super(null, adminModel);
+
+	    isSaveOK = true;
 	    
-	    requestCallback = callback;
-	    
+	    doIt(callback);
+	}	
+
+	public ParallelProgramSetup(CmAsyncRequest callback,
+			CmAdminModel adminModel, ParallelProgramModel ppModel,
+			StudentModelI sm) {
+		super(sm, adminModel);
+		
+		parellelProgId = ppModel.getId();
+		isSaveOK = (ppModel.getStudentCount() < 1);
+		
+		doIt(callback);
+	}
+
+	private void doIt(CmAsyncRequest callback) {
+		requestCallback = callback;
+		
 	    _window.setHeading("Parallel Program Setup");
 	    _fsProfile.removeAll();
+	    _fsProfile.setHeading("");
 
 	    _window.setHeight(350);
         
-        _nameTag = new TextField<String>();  
-        _nameTag.setFieldLabel("Name");
-        _nameTag.setAllowBlank(false);
-        _nameTag.setId("nameTag");
-        _nameTag.setEmptyText("-- Enter Parallel Program name --");
-        _fsProfile.add(_nameTag);
+        nameFld = new TextField<String>();  
+        nameFld.setFieldLabel("Name");
+        nameFld.setAllowBlank(false);
+        nameFld.setId("nameTag");
+        nameFld.setEmptyText("-- Enter Parallel Program name --");
+
+        if (parallelProg != null)
+            nameFld.setValue((String)parallelProg.getName());
+
+        _fsProfile.add(nameFld);
         
         _fsProfile.add(new Html("<p>Student will Log In with your school Login Name, and use this name as a password.</p>"));
 
         
-        _fsProgram.setHeading("Assign Program for This Parallel Program");
+        _fsProgram.setHeading("Assign Program");
 	    _formPanel.layout();
 
 	    // Don't allow Advanced Options (for now?)
 	    hideAdvancedOptionsButton();
-	    
+
 	    showWindow();
 	}
-	
 
 	protected List<Button> getActionButtons() {
 	    List<Button> list = new ArrayList<Button>();
         
-        Button autoCreate = new Button("Save");
-        autoCreate  .addSelectionListener(new SelectionListener<ButtonEvent>() {
-            
-            @Override
-            public void componentSelected(ButtonEvent ce) {
-                try {
-                    doSubmitAction(_fsProgram, _formPanel, new AfterValidation() {
-                        
-                        @Override
-                        public void afterValidation(StudentModel student) {
-                            student.setName(_nameTag.getValue());
-                            student.setGroup(_nameTag.getValue());
-                            student.setPasscode(_nameTag.getValue());
-                            
-                            saveParallelProgramSetup(student);
-                        }
-                    });
-                }
-                catch(CmException cm) {
-                    // CatchupMathTools.showAlert("First, make sure all values on form are valid");
-                }
-            }
-        });
-        
-        list.add(autoCreate);
+	    if (isSaveOK == true) {
+	    	Button save = new Button("Save");
+	    	save.addSelectionListener(new SelectionListener<ButtonEvent>() {
+
+	    		@Override
+	    		public void componentSelected(ButtonEvent ce) {
+	    			try {
+	    				doSubmitAction(_fsProgram, _formPanel, new AfterValidation() {
+
+	    					@Override
+	    					public void afterValidation(StudentModel student) {
+	    						student.setName(nameFld.getValue());
+	    						student.setGroup(nameFld.getValue());
+	    						student.setPasscode(nameFld.getValue());
+
+	    						saveParallelProgramSetup(student);
+	    					}
+	    				});
+	    			}
+	    			catch(CmException cm) {
+	    				// CatchupMathTools.showAlert("First, make sure all values on form are valid");
+	    			}
+	    		}
+	    	});
+
+	    	list.add(save);
+	    }
         
         
         Button close = new Button("Close");
@@ -108,15 +134,14 @@ public class ParallelProgramSetup extends RegisterStudent {
         
         return list;
 	}
-	
-	
+
 	private void saveParallelProgramSetup(StudentModel student) {
 	    CmServiceAsync s = CmShared.getCmService();
-        s.execute(new SaveParallelProgramAction(student.getAdminUid(), student), new AsyncCallback<RpcData>() {
+        s.execute(new SaveParallelProgramAction(student.getAdminUid(), student, parellelProgId), new AsyncCallback<RpcData>() {
             @Override
             public void onSuccess(RpcData result) {
-                    _window.hide();
-                    EventBus.getInstance().fireEvent(new CmEvent(EventType.EVENT_TYPE_REFRESH_STUDENT_DATA));
+                requestCallback.requestComplete("OK");
+                _window.hide();
             }
 
             @Override
