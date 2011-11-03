a1d31d5035a5b805b259cdba03ac2b8643033021
Casey Rodgers <grodgers@gmail.com>
2011-11-03 23:13:51 +0000

    IN PROGRESS - issue CM-620: Cm Mobile UI http://admin.catchupmath.com:8081/browse/CM-620

---
+++ src/main/java/hotmath/gwt/cm_rpc/client/rpc/CmRpcExceptionClientDoesNotSupportFlash.java
@@ -0,0 +1,6 @@
+package hotmath.gwt.cm_rpc.client.rpc;
+
+public class CmRpcExceptionClientDoesNotSupportFlash extends CmRpcException implements java.io.Serializable {
+    public CmRpcExceptionClientDoesNotSupportFlash() {
+    }
+}
