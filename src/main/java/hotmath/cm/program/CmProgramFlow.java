13bbba06cad893d4e64f6042ba68c148b645f896
Bob Hall <rfhall3@yahoo.com>
2011-11-09 16:28:24 +0000

    revert to 1.14, parallel program check done in GetUserInfoCommand (CM-611)

--- src/main/java/hotmath/cm/program/CmProgramFlow.java
+++ src/main/java/hotmath/cm/program/CmProgramFlow.java
@@ -3,7 +3,6 @@ package hotmath.cm.program;
 import hotmath.assessment.AssessmentPrescriptionManager;
 import hotmath.cm.server.model.CmUserProgramDao;
 import hotmath.gwt.cm_admin.server.model.CmStudentDao;
-import hotmath.gwt.cm_admin.server.model.ParallelProgramDao;
 import hotmath.gwt.cm_rpc.client.model.StudentActiveInfo;
 import hotmath.gwt.cm_rpc.client.rpc.CmPlace;
 import hotmath.gwt.cm_rpc.client.rpc.CmProgramFlowAction;
@@ -97,10 +96,8 @@ public class CmProgramFlow {
      */
     public CmProgramFlowAction getActiveFlowAction(final Connection conn) throws Exception {
 
-    	ParallelProgramDao ppDao = ParallelProgramDao.getInstance();
         if (userProgram.isComplete()) {
-            if (student.getSettings().getStopAtProgramEnd() || userProgram.isCustom() ||
-            	ppDao.isStudentInParallelProgram(student.getUid())) {
+            if (student.getSettings().getStopAtProgramEnd() || userProgram.isCustom()) {
                 return new CmProgramFlowAction(CmPlace.END_OF_PROGRAM);
             } else
                 return new CmProgramFlowAction(CmPlace.AUTO_ADVANCED_PROGRAM);
@@ -177,11 +174,8 @@ public class CmProgramFlow {
                  * 
                  */
                 markProgramAsCompleted(conn, true);
-                
-                ParallelProgramDao ppDao = ParallelProgramDao.getInstance();
 
-                if (student.getSettings().getStopAtProgramEnd() ||
-                	ppDao.isStudentInParallelProgram(student.getUid())) {
+                if (student.getSettings().getStopAtProgramEnd()) {
                     action = getActiveFlowAction(conn);
                     assert (action.getPlace() == CmPlace.END_OF_PROGRAM);
                 } else {
