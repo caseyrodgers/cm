6d59f10d6350c55a2fc7c8247f1c34a697d6478f
Casey Rodgers <grodgers@gmail.com>
2011-11-14 21:53:01 +0000

    *** empty log message ***

--- src/main/java/hotmath/gwt/cm_tools/client/ui/viewer/SolutionSetCompleteDialog.java
+++ src/main/java/hotmath/gwt/cm_tools/client/ui/viewer/SolutionSetCompleteDialog.java
@@ -1,7 +1,11 @@
 package hotmath.gwt.cm_tools.client.ui.viewer;
 
 import hotmath.gwt.cm_tools.client.ui.CmWindow.CmWindow;
+import hotmath.gwt.shared.client.eventbus.CmEvent;
+import hotmath.gwt.shared.client.eventbus.EventBus;
+import hotmath.gwt.shared.client.eventbus.EventType;
 
+import com.extjs.gxt.ui.client.event.WindowListener;
 import com.extjs.gxt.ui.client.widget.Html;
 
 public class SolutionSetCompleteDialog extends CmWindow {
@@ -9,6 +13,8 @@ public class SolutionSetCompleteDialog extends CmWindow {
     public SolutionSetCompleteDialog(int numCorrect, int limit) {
         
         setHeading("Problem Set Results");
+        
+        EventBus.getInstance().fireEvent(new CmEvent(EventType.EVENT_TYPE_MODAL_WINDOW_OPEN, this));
         
         setModal(true);
         setSize(350, 150);
@@ -19,6 +25,13 @@ public class SolutionSetCompleteDialog extends CmWindow {
        html = "<p style='text-align: center;margin-top: 10px;' class='solution_set_results'>" + html + "</p>";
        
        add(new Html(html));
+       
+       addWindowListener(new WindowListener() {
+           public void windowHide(com.extjs.gxt.ui.client.event.WindowEvent we) {
+               EventBus.getInstance().fireEvent(new CmEvent(EventType.EVENT_TYPE_MODAL_WINDOW_CLOSED, this));
+           }
+       });
+              
        
        addCloseButton();
        setVisible(true);
