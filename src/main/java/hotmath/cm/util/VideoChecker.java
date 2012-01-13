4b47cc9a7569bb5da1f5b0bbbf6116a69b603d5e
Casey Rodgers <grodgers@gmail.com>
2012-01-13 20:24:46 +0000

    *** empty log message ***

--- src/main/java/hotmath/cm/util/VideoChecker.java
+++ src/main/java/hotmath/cm/util/VideoChecker.java
@@ -11,25 +11,25 @@ import sb.client.SbTesterFrameGeneric;
 import sb.util.SbException;
 import sb.util.SbTestImpl;
 
-/** checks for existence of mobile videos for 
+/** checks for existence of mobile videos for
  *  all prescriptions.
- *  
+ *
  * @author casey
  *
  */
 public class VideoChecker implements SbTestImpl {
-    
+
     public VideoChecker() {}
-    
+
     public void runTests() {
-        
+
         String sql = "select i.file,l.lesson,link_key, count(*) " +
                      " from inmh_link i " +
                      " JOIN HA_PROGRAM_LESSONS_static l on l.file = i.file " +
                      " where link_type = 'video' " +
                      " and subject > '' " +
                      " group by subject, i.file,l.lesson,link_key ";
-        
+
         Connection conn=null;
         try {
             conn = HMConnectionPool.getConnection();
@@ -47,63 +47,63 @@ public class VideoChecker implements SbTestImpl {
             SqlUtilities.releaseResources(null, null,conn);
         }
     }
-    
+
     private boolean doesExistAsMobileVersion(String videoKey) throws Exception {
-        
+
         String base = CatchupMathProperties.getInstance().getSolutionBase();
-        
+
         String videoPath = "";
-        String flvPath="";
-        String mp4Path="";
-        String ogvPath="";
-        
+        File flvPath=null;
+        File mp4Path=null;
+        File ogvPath=null;
+
         if(videoKey.indexOf("youtube") > -1 || videoKey.indexOf("mathtv") > -1) {
             videoPath = videoKey.substring(0, videoKey.indexOf(".flv"));
 
             String p[] =videoPath.split("/");
 
-            flvPath = videoPath + ".flv";
-            mp4Path = "/help/flvs/tw/" + p[p.length-1]  + ".mp4";
-            ogvPath = "/help/flvs/tw/" + p[p.length-1]  + ".ogv";
+            flvPath = new File(base, videoPath + ".flv");
+            mp4Path = new File(base,"/help/flvs/tw/" + p[p.length-1]  + ".mp4");
+            ogvPath = new File(base, "/help/flvs/tw/" + p[p.length-1]  + ".ogv");
         }
         else {
             videoPath = "/help/flvs/tw/" + videoKey;
-            
-            flvPath = videoPath + ".flv";
-            
-            mp4Path = "/help/flvs/tw/" + videoKey + ".mp4";
-            ogvPath = "/help/flvs/tw/" + videoKey + ".ogv";
-        }        
+
+            flvPath = new File(videoPath + ".flv");
+
+            mp4Path = new File("/help/flvs/tw/" + videoKey + ".mp4");
+            ogvPath = new File("/help/flvs/tw/" + videoKey + ".ogv");
+        }
         videoPath = base + videoPath;
-        
+
         /** should be a .ogg and a .mp4 and a .flv
-         * 
+         *
          */
-        
-        if(!new File(base, flvPath).exists()) {
-            System.out.println(videoKey + ": " + ".flv does not exist");
+
+        if(!flvPath.exists()) {
+            System.out.println(flvPath + ": " + ".flv does not exist");
             return false;
         }
-        if(!new File(base, mp4Path).exists()) {
-            System.out.println(videoKey + ": " + ".mp4 does not exist");
+        if(!mp4Path.exists()) {
+            System.out.println(mp4Path + ": " + ".mp4 does not exist");
             return false;
         }
-        if(!new File(base, ogvPath).exists()) {
-            System.out.println(videoKey + ": " + ".ogv does not exist");
+        if(!ogvPath.exists()) {
+            System.out.println(ogvPath + ": " + ".ogv does not exist");
             return false;
         }
-        
+
         return true;
-        
+
     }
 
     @Override
     public void doTest(Object objLogger, String sFromGUI) throws SbException {
         runTests();
     }
-    
-    
-    
+
+
+
     public static void main(String as[]) {
         try {
             System.out.println("Video Checker: look for missing videos requried for mobile");
@@ -112,5 +112,7 @@ public class VideoChecker implements SbTestImpl {
         catch(Exception e) {
             e.printStackTrace();
         }
+
+        System.exit(0);
     }
 }
