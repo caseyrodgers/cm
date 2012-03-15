636f73f91f3b56bb9d6d717455fa2716596786e7
Casey Rodgers <grodgers@gmail.com>
2012-03-15 16:20:22 +0000

    IN PROGRESS - issue CM-280: Solution Manager http://admin.catchupmath.com:8081/browse/CM-280

--- src/main/java/hotmath/gwt/cm_rpc/client/rpc/GetQuizResultsHtmlAction.java
+++ src/main/java/hotmath/gwt/cm_rpc/client/rpc/GetQuizResultsHtmlAction.java
@@ -1,5 +1,11 @@
 package hotmath.gwt.cm_rpc.client.rpc;
 
+import hotmath.gwt.cm_rpc.client.UserInfo;
+import hotmath.gwt.cm_tools.client.ui.CmLogger;
+import hotmath.gwt.shared.client.CmShared;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
 
 public class GetQuizResultsHtmlAction implements Action<RpcData>{
     
@@ -9,9 +15,30 @@ public class GetQuizResultsHtmlAction implements Action<RpcData>{
     
     public GetQuizResultsHtmlAction(int runId) {
         this.runId = runId; 
+       
+        getRunId();
     }
 
     public int getRunId() {
+        
+        if(runId == 0) {
+            SaveFeedbackAction action = new SaveFeedbackAction();
+            action.setStateInfo("GetQuizResultsHtmlAction: bug with runId=0");
+            action.setComments(UserInfo.getInstance().toString());
+            CmShared.getCmService().execute(action,new AsyncCallback<RpcData>() {
+    
+                @Override
+                public void onFailure(Throwable caught) {
+                    CmLogger.info(caught.getMessage());
+                }
+    
+                @Override
+                public void onSuccess(RpcData result) {
+                    CmLogger.info("Feedback saved!");
+                }
+            });
+        }
+        
         return runId;
     }
 
