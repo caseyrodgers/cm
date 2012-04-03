7c8b01c97de539b0b6adcda891650c86c9a6b4db
Casey Rodgers <grodgers@gmail.com>
2012-04-03 10:38:33 +0000

    log from info to debug.

--- src/main/java/hotmath/gwt/cm_admin/server/model/CmHighlightsDao.java
+++ src/main/java/hotmath/gwt/cm_admin/server/model/CmHighlightsDao.java
@@ -367,7 +367,7 @@ public class CmHighlightsDao extends SimpleJdbcDaoSupport{
             ps.setString(5, vals[0]);
             ps.setString(6, vals[1]);            
 
-            __logger.info("report sql: " + ps);
+            __logger.debug("report sql: " + ps);
 
             Map<ActivityTypeEnum, ActivityTime> map = StudentActivityDao.getInstance().getActivityTimeMap();
             ResultSet rs = ps.executeQuery();
