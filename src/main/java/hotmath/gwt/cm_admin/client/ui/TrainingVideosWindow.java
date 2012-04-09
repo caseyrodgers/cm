e6b65c0f2cd6bc302c5cb8f48222f2b24b6fd674
Bob Hall <rfhall3@yahoo.com>
2012-04-09 08:24:29 +0000

    CM-659

--- src/main/java/hotmath/gwt/cm_admin/client/ui/TrainingVideosWindow.java
+++ src/main/java/hotmath/gwt/cm_admin/client/ui/TrainingVideosWindow.java
@@ -38,46 +38,47 @@ public class TrainingVideosWindow extends CmWindow {
 				 "Description of Overview of Teacher Reesources Video...",
 				 "Overview of Teacher Resources/Overview of Teacher Resources_controller.swf",
 				 "Watch the Teacher Resources Video",
-				 lc);
+				 lc, true);
 
 		addVideo("Sample Student Session",
 				 "Description of Sample Student Session Video...",
 				 "Sample Student Session/Sample Student Session_controller.swf",
 				 "Watch the Sample Student Session Video",
-				 lc);
+				 lc, true);
 		
 		addVideo("Available Content",
 				 "Description of Available Content Video...",
 				 "Available Content/Available Content_controller.swf",
 				 "Watch the Available Content Video",
-				 lc);
+				 lc, true);
 		
 		addVideo("Registering Students",
 				 "Description of Registering Students Video...",
 				 "Registering Students/Registering Students_controller.swf",
 				 "Watch the Registering Students Video",
-				 lc);
+				 lc, true);
 		
 		addVideo("Managing Students",
 				 "Description of Managing Students Video...",
 				 "Managing Students/Managing Students_controller.swf",
 				 "Watch the Managing Students Video",
-				 lc);
+				 lc, true);
 		
 		addVideo("Managing Groups",
 				 "Description of Managing Groups Video...",
 				 "Managing Groups/Managing Groups_controller.swf",
 				 "Watch the Managing Groups Video",
-				 lc);
+				 lc, false);
 				
 		add(lc);        
 
 		setVisible(true);
     }
 
-	private void addVideo(final String heading, String description, final String video, String toolTip, LayoutContainer lc) {
+	private void addVideo(final String heading, String description, final String video, String toolTip, LayoutContainer lc,
+			boolean addSeparator) {
 		StringBuilder sb = new StringBuilder();
-		sb.append("<div>");
+		sb.append("<div class='training-videos-window'>");
         sb.append("<h2>").append(heading).append("</h2>");
         sb.append("<p>").append(description).append("</p>");
 		sb.append("</div>");
@@ -95,6 +96,12 @@ public class TrainingVideosWindow extends CmWindow {
             }
         });
         lc.add(button);
+        
+        if (addSeparator) {
+        	html = new HTML();
+        	html.setHTML("<hr>");
+        	lc.add(html);
+        }
 	}
 
     public void onClick(ClickEvent event) {
