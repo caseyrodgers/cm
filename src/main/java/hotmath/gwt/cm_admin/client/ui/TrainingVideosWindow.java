c47c4afe894f423905f2bdcd98a52164783651bb
Bob Hall <rfhall3@yahoo.com>
2012-04-10 08:05:42 +0000

    CM-659

--- src/main/java/hotmath/gwt/cm_admin/client/ui/TrainingVideosWindow.java
+++ src/main/java/hotmath/gwt/cm_admin/client/ui/TrainingVideosWindow.java
@@ -33,39 +33,54 @@ public class TrainingVideosWindow extends CmWindow {
 		lc.setAutoWidth(true);
 		lc.setBorders(false);
         lc.setStyleName("training-videos-window");
-	
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("<div class='training-videos-window'>");
+        sb.append("<h2>").append("Videos and FAQ").append("</h2>");
+        sb.append("<p>").append("The videos below let you explore the richness of Catchup Math, topic ");
+        sb.append("by topic. For specific questions, please click for our ");
+        sb.append("<a href='/teacher-faq.html' target='_blank'>Teacher FAQ</a> or contact your ");
+        sb.append("<a href='/contact.html' target='_blank'>Account Manager</a>. ");
+        sb.append("We hope you and your colleagues will return here often!");
+        sb.append("</p>");
+		sb.append("</div>");
+		HTML html = new HTML();
+		html.setHTML(sb.toString());
+		lc.add(html);
+		
+
 		addVideo("Overview of Teacher Resources",
-				 "Description of Overview of Teacher Reesources Video...",
+				 "Brief introduction to the features and resources available to teachers and administrators.  Length: 15 minutes.",
 				 "Overview of Teacher Resources/Overview of Teacher Resources_controller.swf",
 				 "Watch the Teacher Resources Video",
 				 lc, true);
 
 		addVideo("Sample Student Session",
-				 "Description of Sample Student Session Video...",
+				 "Demonstrates what students experience and the options available to them.  Length:  12 minutes.",
 				 "Sample Student Session/Sample Student Session_controller.swf",
 				 "Watch the Sample Student Session Video",
 				 lc, true);
 		
 		addVideo("Available Content",
-				 "Description of Available Content Video...",
+				 "Explains the various programs available, and the content included within each program.  Length: 9 minutes.",
 				 "Available Content/Available Content_controller.swf",
 				 "Watch the Available Content Video",
 				 lc, true);
 		
 		addVideo("Registering Students",
-				 "Description of Registering Students Video...",
+				 "Demonstrates the various options for registering students into Catchup Math.  Length: 17 minutes.",
 				 "Registering Students/Registering Students_controller.swf",
 				 "Watch the Registering Students Video",
 				 lc, true);
 		
 		addVideo("Managing Students",
-				 "Description of Managing Students Video...",
+				 "Explores the various resources for managing the placement of students, managing their assignments, reviewing their progress, and applying controls.  Length: 25 minutes.",
 				 "Managing Students/Managing Students_controller.swf",
 				 "Watch the Managing Students Video",
 				 lc, true);
 		
 		addVideo("Managing Groups",
-				 "Description of Managing Groups Video...",
+				 "Explains how to set up and assign students to groups (classes), assign or reassign programs/content to the groups, set controls, and analyze group data.  Length: 30 minutes.",
 				 "Managing Groups/Managing Groups_controller.swf",
 				 "Watch the Managing Groups Video",
 				 lc, false);
