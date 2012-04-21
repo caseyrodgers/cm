680f9657e13ba17472bfec4fcd19d32c3676d7cb
Casey Rodgers <grodgers@gmail.com>
2012-04-21 18:10:52 +0000

    added small close to bottom left

--- src/main/webapp/assets/js/core.js
+++ src/main/webapp/assets/js/core.js
@@ -1,13 +1,13 @@
 /** add trim, if not available */
 if(typeof String.prototype.trim !== 'function') {
     String.prototype.trim = function() {
-      return this.replace(/^\s+|\s+$/g, ''); 
+      return this.replace(/^\s+|\s+$/g, '');
     }
   }
 
 
 /** Initialize system
- *  Might be modified in 
+ *  Might be modified in
  *  individual pages */
 var CmPage = {
         menuItem:-1,
@@ -21,24 +21,24 @@ function $get(x) {
 
 function setupPage() {
     /** setup menu buttons
-     * 
+     *
      */
     YUI().use('node', function(Y) {
-        
+
         Y.on('domready',function() {
             if(CmPage.menuItem > -1) {
                 /** Set the selected menu button */
                 var menuButtons = Y.one('#menubar2').all('a');
                 _mainMenuItemObject = menuButtons.item(CmPage.menuItem);
                 _mainMenuItemObject.removeClass('notselected');
-                _mainMenuItemObject.removeClass('sexy_cm_gray');				
+                _mainMenuItemObject.removeClass('sexy_cm_gray');
                 _mainMenuItemObject.addClass('sexy_cm_green');
                 _mainMenuItemObject.addClass('sexybutton');
             }
-                
+
             /** Setup the sticky nodes area with two random
              *  quotes from QuoteManager.
-             *  
+             *
              */
             var postIt = Y.one('#postit');
             if(postIt) {
@@ -48,7 +48,7 @@ function setupPage() {
                     postIt.set('innerHTML',html);
                 }
             }
-            
+
            setupPageLocal();
      });
     });
@@ -70,19 +70,19 @@ var _overlay=null;
 
 function showMonaMotivationalVideo() {
     var closeFoot = '';
-    
-    
+
+
     var html = '<iframe src="/motivational_video/" width="320" height="265px" scrolling="no" frameborder="no"></iframe>' +
               closeFoot;
-    
-    var head = '<a href="#" onclick="closeMonaVideo();return false" class="close"><span>close</span> X</a>' + "Catchup Math Motivational Video";
-    
 
-    
+    var head = '<a href="#" onclick="closeMonaVideo();return false" class="close"><span>close</span> X</a>' + "Catchup Math Motivational Video";
+
+
+
     YUI().use('anim','overlay',
                     function(Y) {
                         _overlay = new Y.Overlay(
-                                {   
+                                {
                                     id:"mona-video",
                                     width : "328px",
                                     centered : true,
@@ -91,7 +91,7 @@ function showMonaMotivationalVideo() {
                                     zIndex : 2,
                                     visible:true
                                 });
-                        
+
                        _overlay.render();
     });
 }
@@ -116,13 +116,13 @@ function showWebinar() {
     var closeFoot = '';
     var html = '<iframe src="/teacher-training-video/embedded.html" width="800" height="498px" scrolling="no" frameborder="no"></iframe>' +
               closeFoot;
-    
+
     var head = '<a href="#" onclick="closeWebinar();return false" class="close"><span>close</span> X</a>' + "Teaching With Catchup Math";
-    
+
     YUI().use('anim','overlay',
                     function(Y) {
                         _overlay = new Y.Overlay(
-                                {   
+                                {
                                     id:"webinar-video",
                                     width : "810px",
                                     height: "498px",
@@ -132,7 +132,7 @@ function showWebinar() {
                                     zIndex : 2,
                                     visible:true
                                 });
-                        
+
                        _overlay.render();
     });
 }
@@ -146,63 +146,66 @@ function closeTeacherVideo() {
 }
 
 function showTeacherVideo(name) {
-	
-	if (_videoOverlay != null) {
-		showDialog('Sorry, only one video may be viewed at a time', 'Training Video Message', 4);
-		return;
-	}
 
-	var html = '';
-	var closeFoot = '';
-	var title = 'Catchup Math Training Video';
-    
-	if (name =='overview') {
-	     html = '<iframe src="/training-videos/embedded-overview.html" width="630" height="525px" scrolling="no" frameborder="no"></iframe>' +
+        if (_videoOverlay != null) {
+                showDialog('Sorry, only one video may be viewed at a time', 'Training Video Message', 4);
+                return;
+        }
+
+        var html = '';
+        var closeFoot = '';
+        var title = 'Catchup Math Training Video';
+
+        if (name =='overview') {
+             html = '<iframe src="/training-videos/embedded-overview.html" width="630" height="525px" scrolling="no" frameborder="no"></iframe>' +
          closeFoot;
-	     title = 'Overview of Teacher Resources';
-	}
-	else if (name == 'sample-session') {
-	     html = '<iframe src="/training-videos/embedded-sample-session.html" width="630" height="525px" scrolling="no" frameborder="no"></iframe>' +
+             title = 'Overview of Teacher Resources';
+        }
+        else if (name == 'sample-session') {
+             html = '<iframe src="/training-videos/embedded-sample-session.html" width="630" height="525px" scrolling="no" frameborder="no"></iframe>' +
          closeFoot;
-	     title = 'Sample Student Session';
-	}
-	else if (name == 'available-content') {
-	     html = '<iframe src="/training-videos/embedded-available-content.html" width="630" height="525px" scrolling="no" frameborder="no"></iframe>' +
+             title = 'Sample Student Session';
+        }
+        else if (name == 'available-content') {
+             html = '<iframe src="/training-videos/embedded-available-content.html" width="630" height="525px" scrolling="no" frameborder="no"></iframe>' +
          closeFoot;
-	     title = 'Available Content';		
-	}
-	else if (name == 'registering-students') {
-	     html = '<iframe src="/training-videos/embedded-registering-students.html" width="630" height="525px" scrolling="no" frameborder="no"></iframe>' +
+             title = 'Available Content';
+        }
+        else if (name == 'registering-students') {
+             html = '<iframe src="/training-videos/embedded-registering-students.html" width="630" height="525px" scrolling="no" frameborder="no"></iframe>' +
          closeFoot;
-	     title = 'Registering Students';		
-	}
-	else if (name == 'managing-students') {
-	     html = '<iframe src="/training-videos/embedded-managing-students.html" width="630" height="525px" scrolling="no" frameborder="no"></iframe>' +
+             title = 'Registering Students';
+        }
+        else if (name == 'managing-students') {
+             html = '<iframe src="/training-videos/embedded-managing-students.html" width="630" height="525px" scrolling="no" frameborder="no"></iframe>' +
          closeFoot;
-	     title = 'Managing Students';
-	}
-	else if (name == 'managing-groups') {
-	     html = '<iframe src="/training-videos/embedded-managing-groups.html" width="630" height="525px" scrolling="no" frameborder="no"></iframe>' +
+             title = 'Managing Students';
+        }
+        else if (name == 'managing-groups') {
+             html = '<iframe src="/training-videos/embedded-managing-groups.html" width="630" height="525px" scrolling="no" frameborder="no"></iframe>' +
          closeFoot;
-	     title = 'Managing Groups';		
-	}
+             title = 'Managing Groups';
+        }
 
     var head = '<a href="#" onclick="closeTeacherVideo();return false" class="close"><span>close</span> X</a>' + title;
     
+    var tail = '<a href="#" onclick="closeTeacherVideo();return false" class="close" style="background-color: white"><span>close</span> X</a>';
+
     YUI().use('anim','overlay',
                     function(Y) {
                         _videoOverlay = new Y.Overlay(
-                                {   
+                                {
                                     id:"webiner-video",
                                     width : "642px",
                                     height: "562px",
                                     centered : true,
+                                    fixedcenter : true,
                                     headerContent : head,
-                                    bodyContent : html,
+                                    bodyContent : html + tail,
                                     zIndex : 2,
                                     visible:true
                                 });
-                        
+
                        _videoOverlay.render();
     });
 }
@@ -235,15 +238,15 @@ function getQueryStringParameter (paramName, url) {
 }
 
 function formatQuote(quote) {
-    
+
     var url = quote.link_type == 'success'?'/success.html':'/research.html';
     var readMore = (quote.link_type == 'success')?'Read more testimonials':'Read more research';
     return '<p>' +
                quote.text +
-               '<div class="credit">' + quote.link_text + '</div><a href="' + url + 
-               '"> ' + readMore + '</a>' + 
+               '<div class="credit">' + quote.link_text + '</div><a href="' + url +
+               '"> ' + readMore + '</a>' +
            '</p>';
-}        
+}
 
 
 
@@ -251,7 +254,7 @@ function formatQuote(quote) {
 var _overlay = null;
 
 function showDialog(msg, title) {
-	showDialog(msg, title, 2);
+        showDialog(msg, title, 2);
 }
 
 function showDialog(msg, title, indexVal) {
@@ -279,8 +282,8 @@ function showDialog(msg, title, indexVal) {
     });
 }
 
-       
-var Quotes = 
+
+var Quotes =
     [
      {
          text:"Really great for students at all achievement levels. It's easy to use, self-explanatory, and encourages students to become independent learners.",
@@ -341,12 +344,12 @@ var Quotes =
          text:'We had incredible results this summer. The teachers are clamoring for us to buy it for the fall!',
          link_text: 'District Curriculum Coordinator, Texas',
          link_type:'success'
-     },     
+     },
      {
          text:'It worked so well over the last 3 weeks that my students are all caught up and doing their regular work now.',
          link_text: 'Geometry Teacher, New York',
          link_type:'success'
-     },     
+     },
      {
          text:'Online mathematics computer-aided instruction products can be a significant component of a school or college review and remediation program.',
          link_text: 'Berniker',
