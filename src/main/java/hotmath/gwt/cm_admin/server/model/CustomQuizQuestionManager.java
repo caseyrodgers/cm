4d093cd59ca893f4e6a76b7d4c6f039b59ef56a7
Casey Rodgers <grodgers@gmail.com>
2012-07-20 05:30:35 +0000

    CM-726

---
+++ src/main/java/hotmath/gwt/cm_admin/server/model/CustomQuizQuestionManager.java
@@ -0,0 +1,110 @@
+package hotmath.gwt.cm_admin.server.model;
+
+import hotmath.cm.util.CatchupMathProperties;
+import hotmath.concordance.ConcordanceEntry;
+import hotmath.flusher.Flushable;
+import hotmath.flusher.HotmathFlusher;
+import hotmath.gwt.cm_rpc.client.rpc.CmArrayList;
+import hotmath.gwt.cm_rpc.client.rpc.CmList;
+import hotmath.gwt.shared.client.model.QuizQuestion;
+
+import java.io.File;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import sb.util.SbFile;
+import sb.util.SbUtilities;
+
+public class CustomQuizQuestionManager {
+    
+    static Logger __logger = Logger.getLogger(CustomQuizQuestionManager.class);
+    static CustomQuizQuestionManager __instance;
+
+    public static CustomQuizQuestionManager getInstance() {
+        if(__instance == null) {
+            try {
+                __instance = new CustomQuizQuestionManager();
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return __instance;
+    }
+    
+    static {
+        HotmathFlusher.getInstance().addFlushable(new Flushable() {
+            @Override
+            public void flush() {
+                __instance = null;
+            }
+        });
+    }
+    
+    
+    Map<String, CustomQuizLessonInfo> data = new HashMap<String,CustomQuizLessonInfo>();
+    
+    protected CustomQuizQuestionManager() throws Exception {
+        readInFile();
+    }
+    
+    
+    private void readInFile() throws Exception{
+        SbFile file = new SbFile(new File(CatchupMathProperties.getInstance().getCatchupRuntime(), "all.custom_quiz"));
+        
+        String contents = file.getFileContents().toString("\n");
+        String lines[] = contents.split("\n");
+        
+        data.clear();
+        for(String line: lines) {
+            String lesson = SbUtilities.getToken(line, 1, ":").trim();
+            String range = SbUtilities.getToken(line, 2, ":").trim();
+            
+            data.put(lesson,new CustomQuizLessonInfo(range)); 
+        }
+    }
+    
+    
+    public CmList<QuizQuestion> getQuestionsFor(final Connection conn, String lessonFile, int gradeLevel) throws Exception {
+        
+        CmList<QuizQuestion> retData = new CmArrayList<QuizQuestion>();
+        CustomQuizLessonInfo lessonInfo = data.get(lessonFile);
+        if(lessonInfo == null) {
+            __logger.warn("Could not find lesson info : " + lessonFile);
+        }
+        else {
+            if(lessonInfo.pids == null) {
+                ConcordanceEntry con = new ConcordanceEntry(conn, lessonInfo.range);
+
+                List<String> pids = new ArrayList<String>();
+                for(String c: con.getGUIDs()) {
+                    pids.add(c);
+                }               
+                lessonInfo.pids = pids;
+            }
+
+            for(String pid: lessonInfo.pids) {
+                QuizQuestion quizQuesion = new QuizQuestion("ques_id", lessonFile,"Prog Name",pid, "quiz HTML", 1);
+                retData.add(quizQuesion);
+            }
+        }
+        return retData;
+    }
+
+}
+
+
+
+class CustomQuizLessonInfo {
+    String range;
+    List<String> pids;
+    
+    public CustomQuizLessonInfo(String range) {
+        this.range = range;
+    }
+}
