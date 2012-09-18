da72248800f94f9af5e56cee1ca1d5ff354a1e21
Bob Hall <rfhall3@yahoo.com>
2012-09-18 06:25:21 +0000

    CM-715: initial commit

---
+++ src/main/java/hotmath/gwt/cm_rpc/client/model/assignment/StudentLessonDto.java
@@ -0,0 +1,51 @@
+package hotmath.gwt.cm_rpc.client.model.assignment;
+
+import hotmath.gwt.cm_rpc.client.rpc.Response;
+
+public class StudentLessonDto implements Response {
+
+    private int uid;
+    private String lessonName;
+    private String status;
+
+    
+    public StudentLessonDto() {
+    }
+
+    public StudentLessonDto(int uid, String lessonName, String status) {
+        this.uid = uid;
+        this.lessonName = lessonName;
+        this.status = status;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getLessonName() {
+        return lessonName;
+    }
+
+    public void setLessonName(String lessonName) {
+        this.lessonName = lessonName;
+    }
+
+
+    
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentLessonDto [uid=" + uid + ", lessonName=" + lessonName + ", status=" + status + "]";
+    }
+}
