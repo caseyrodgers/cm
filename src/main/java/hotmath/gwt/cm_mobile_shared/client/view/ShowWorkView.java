ca1e8c17aaac9fb13f6d01904834efe154e78394
Casey Rodgers <grodgers@gmail.com>
2012-09-18 14:41:24 +0000

    no message

---
+++ src/main/java/hotmath/gwt/cm_mobile_shared/client/view/ShowWorkView.java
@@ -0,0 +1,13 @@
+package hotmath.gwt.cm_mobile_shared.client.view;
+
+import hotmath.gwt.cm_mobile_shared.client.page.IPage;
+
+public interface ShowWorkView extends IPage {
+    void setTitle(String title);
+    void setPresenter(Presenter presenter);
+    static public interface Presenter {
+        void prepareShowWorkView(ShowWorkView view);
+        String getProblemStatementHtml();
+        void goBack();
+    }
+}
