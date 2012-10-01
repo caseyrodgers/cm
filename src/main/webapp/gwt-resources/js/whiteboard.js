17045f4ac2e8f5dcced741f70e850bb84be6a8e6
sathesh <sathesh>
2012-10-01 22:14:44 +0000

    no message

--- src/main/webapp/gwt-resources/js/whiteboard.js
+++ src/main/webapp/gwt-resources/js/whiteboard.js
@@ -249,9 +249,9 @@ console.log("CANVAS_IN_IE: "+canvas+":"+canvas.getContext);
         topcanvas = $get_Element("#tcanvas");
 		try{
 	if (typeof G_vmlCanvasManager != "undefined") {
-		var parent_cont=$get_Element("#canvas_container")
-		console.log("DEBUG_IE: "+parent_cont);
-		console.log("DEBUG_IE: "+parent_cont.removeChild);
+		var parent_cont=$get_Element("#canvas-container")
+		console.log("DEBUG_IE parent_cont: "+parent_cont);
+		console.log("DEBUG_IE: parent_cont.removeChild"+parent_cont.removeChild);
 		parent_cont.removeChild(canvas)
 		parent_cont.removeChild(origcanvas)
 		parent_cont.removeChild(graphcanvas)
