73fed466ba8a4a904e6d7dd1c975ac8d4e7cf0bc
Casey Rodgers <grodgers@gmail.com>
2012-10-05 00:17:15 +0000

    no message

---
+++ src/main/java/hotmath/gwt/cm_tutor/client/view/TutorCallbackDefault.java
@@ -0,0 +1,32 @@
+package hotmath.gwt.cm_tutor.client.view;
+
+import hotmath.gwt.cm_rpc.client.rpc.SaveSolutionContextAction;
+import hotmath.gwt.cm_tutor.client.view.TutorWrapperPanel.TutorCallback;
+
+import com.allen_sauer.gwt.log.client.Log;
+
+public class TutorCallbackDefault implements TutorCallback {
+
+    @Override
+    public void onNewProblem(int problemNumber) {
+        Log.debug("TutorCallbackDefault: on new problem");
+    }
+
+    @Override
+    public void tutorWidgetComplete(String inputValue, boolean correct) {
+        Log.debug("TutorCallbackDefault: tutorWidgetComplete: " + inputValue + ", " + correct);        
+    }
+
+    @Override
+    public void solutionHasBeenViewed(String value) {
+        Log.debug("TutorCallbackDefault: solutionHasBeenViewed: " + value);        
+    }
+
+
+    @Override
+    public SaveSolutionContextAction getSaveSolutionContextAction(String variablesJson, String pid, int problemNumber) {
+        return null;
+    }
+    
+
+}
