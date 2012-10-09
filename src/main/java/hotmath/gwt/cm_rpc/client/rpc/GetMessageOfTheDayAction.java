f93eda15803422c25b84d4d925f8ea67583bf66f
Casey Rodgers <grodgers@gmail.com>
2012-10-09 04:17:31 +0000

    *** empty log message ***

---
+++ src/main/java/hotmath/gwt/cm_rpc/client/rpc/GetMessageOfTheDayAction.java
@@ -0,0 +1,7 @@
+package hotmath.gwt.cm_rpc.client.rpc;
+
+import hotmath.gwt.cm_tools.client.model.StringHolder;
+
+public class GetMessageOfTheDayAction implements Action<StringHolder> {
+    public GetMessageOfTheDayAction(){}
+}
