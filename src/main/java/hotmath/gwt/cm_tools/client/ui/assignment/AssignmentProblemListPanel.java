66e124497d3aa1baf44f419a0a7ebdcf9bd7f03f
Casey Rodgers <grodgers@gmail.com>
2012-10-30 19:37:51 +0000

    *** empty log message ***

--- src/main/java/hotmath/gwt/cm_tools/client/ui/assignment/AssignmentProblemListPanel.java
+++ src/main/java/hotmath/gwt/cm_tools/client/ui/assignment/AssignmentProblemListPanel.java
@@ -34,30 +34,34 @@ import com.sencha.gxt.widget.core.client.container.SimpleContainer;
 import com.sencha.gxt.widget.core.client.grid.ColumnConfig;
 import com.sencha.gxt.widget.core.client.grid.ColumnModel;
 import com.sencha.gxt.widget.core.client.grid.Grid;
+import com.sencha.gxt.widget.core.client.grid.GridViewConfig;
 import com.sencha.gxt.widget.core.client.selection.SelectionChangedEvent;
 import com.sencha.gxt.widget.core.client.selection.SelectionChangedEvent.SelectionChangedHandler;
 
-/** Problem listing for a single Assignment
+/**
+ * Problem listing for a single Assignment
  * 
  * 
  * @author casey
- *
+ * 
  */
 public class AssignmentProblemListPanel extends SimpleContainer {
-    
+
     static AssignmentProblemListPanel __lastInstance;
-    
+
     public interface ProblemListPanelProperties extends PropertyAccess<String> {
         ModelKeyProvider<StudentProblemDto> pid();
+
         ValueProvider<StudentProblemDto, String> pidLabel();
+
         ValueProvider<StudentProblemDto, String> status();
+
         ValueProvider<StudentProblemDto, Boolean> hasShowWorkAdmin();
-      }
-    
-    
+    }
+
     AssignmentProblemListCallback _callBack;
     Grid<StudentProblemDto> _studentProblemGrid;
-    
+
     public AssignmentProblemListPanel(AssignmentProblemListCallback callback) {
         this._callBack = callback;
 
@@ -65,123 +69,139 @@ public class AssignmentProblemListPanel extends SimpleContainer {
 
         List<ColumnConfig<StudentProblemDto, ?>> l = new ArrayList<ColumnConfig<StudentProblemDto, ?>>();
 
-        ColumnConfig<StudentProblemDto, String> labelCol = new ColumnConfig<StudentProblemDto, String>(props.pidLabel(),50, "Problem");
+        ColumnConfig<StudentProblemDto, String> labelCol = new ColumnConfig<StudentProblemDto, String>(
+                props.pidLabel(), 50, "Problem");
         l.add(labelCol);
-        
-        ColumnConfig<StudentProblemDto, String> labelStatus = new ColumnConfig<StudentProblemDto, String>(props.status(),100, "Status");
-        l.add(labelStatus);
-        
-        ColumnConfig<StudentProblemDto, Boolean> labelComment = new ColumnConfig<StudentProblemDto, Boolean>(props.hasShowWorkAdmin(),50, "Teacher Comment");
-        l.add(labelComment);
 
-        ColumnModel<StudentProblemDto> cm = new ColumnModel<StudentProblemDto>(l);        
+        ColumnConfig<StudentProblemDto, String> labelStatus = new ColumnConfig<StudentProblemDto, String>(
+                props.status(), 100, "Status");
+        l.add(labelStatus);
+
+        ColumnModel<StudentProblemDto> cm = new ColumnModel<StudentProblemDto>(l);
 
         ListStore<StudentProblemDto> store = new ListStore<StudentProblemDto>(props.pid());
-                
+
         _studentProblemGrid = new Grid<StudentProblemDto>(store, cm);
-        
+
         _studentProblemGrid.getView().setAutoExpandColumn(labelCol);
         _studentProblemGrid.getView().setStripeRows(true);
         _studentProblemGrid.getView().setColumnLines(true);
         _studentProblemGrid.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-        
 
         _studentProblemGrid.addHandler(new DoubleClickHandler() {
             @Override
             public void onDoubleClick(DoubleClickEvent event) {
-                //loadProblem()
+                // loadProblem()
             }
-        },DoubleClickEvent.getType());
+        }, DoubleClickEvent.getType());
+
+        _studentProblemGrid.getSelectionModel().addSelectionChangedHandler(
+                new SelectionChangedHandler<StudentProblemDto>() {
+                    @Override
+                    public void onSelectionChanged(SelectionChangedEvent<StudentProblemDto> event) {
+                        if (event.getSelection().size() > 0) {
+                            loadProblemStatement(event.getSelection().get(0));
+                        }
+                    }
+                });
         
-        _studentProblemGrid.getSelectionModel().addSelectionChangedHandler(new SelectionChangedHandler<StudentProblemDto>() {
+
+        _studentProblemGrid.getView().setViewConfig(new GridViewConfig<StudentProblemDto>() {
+
             @Override
-            public void onSelectionChanged(SelectionChangedEvent<StudentProblemDto> event) {
-                if(event.getSelection().size() > 0) {
-                    loadProblemStatement(event.getSelection().get(0));
+            public String getRowStyle(StudentProblemDto model, int rowIndex) {
+                if (model != null) {
+                    if (model.isHasShowWorkAdmin()) {
+                        return "assign-showwork-admin";
+                    }
                 }
+                return null;
+            }
+
+            @Override
+            public String getColStyle(StudentProblemDto model,
+                    ValueProvider<? super StudentProblemDto, ?> valueProvider, int rowIndex, int colIndex) {
+                return null;
             }
         });
-
+        
         add(_studentProblemGrid);
-        
-        
+
         __lastInstance = this;
     }
-    
+
     private void loadProblemStatement(StudentProblemDto studentProb) {
-        
-       
+
         _callBack.problemSelected(studentProb.getPidLabel(), studentProb.getProblem());
-        
-        if(studentProb.getStatus().equals("Not Viewed")) {
+
+        if (studentProb.getStatus().equals("Not Viewed")) {
             studentProb.setStatus("Viewed");
             _studentProblemGrid.getStore().update(studentProb);
         }
     }
-    
-    /** The current problem's whiteboard has been updated
-     *  meaning the user has made an entry.  The problem 
-     *  should be marked as pending if is a WHITEBOARD type.
-     *  
-     *  This only applies if the question does not have an input widget. 
+
+    /**
+     * The current problem's whiteboard has been updated meaning the user has
+     * made an entry. The problem should be marked as pending if is a WHITEBOARD
+     * type.
+     * 
+     * This only applies if the question does not have an input widget.
      */
     private void whiteboardUpdated() {
-        
+
         StudentProblemDto prob = _studentProblemGrid.getSelectionModel().getSelectedItem();
-        if(prob != null) {
-            
-            switch(_assignmentProblem.getProblemType()) {
-                case WHITEBOARD:
-                    
-                    if(prob.getStatus().equalsIgnoreCase("Viewed")) {
-                        // update the problem type to current type
-                        prob.getProblem().setProblemType( _assignmentProblem.getProblemType() );
-                        prob.setStatus("Pending");
-                        
-                        _studentProblemGrid.getStore().update(prob);
-                        
-                        
-                        saveAssignmentProblemStatusToServer(prob);
-                        break;
-                    }
-                    
-                case INPUT_WIDGET:
-                case MULTI_CHOICE:
-                    // whiteboard entry does not change status
+        if (prob != null) {
+
+            switch (_assignmentProblem.getProblemType()) {
+            case WHITEBOARD:
+
+                if (prob.getStatus().equalsIgnoreCase("Viewed")) {
+                    // update the problem type to current type
+                    prob.getProblem().setProblemType(_assignmentProblem.getProblemType());
+                    prob.setStatus("Pending");
+
+                    _studentProblemGrid.getStore().update(prob);
+
+                    saveAssignmentProblemStatusToServer(prob);
                     break;
-                    
-                    default:
-                        CmLogger.debug("AssignmentProblemListPanel: unknown problem type: " + _assignmentProblem.getProblemType());
+                }
+
+            case INPUT_WIDGET:
+            case MULTI_CHOICE:
+                // whiteboard entry does not change status
+                break;
+
+            default:
+                CmLogger.debug("AssignmentProblemListPanel: unknown problem type: "
+                        + _assignmentProblem.getProblemType());
             }
-            
-            
-            
-            
+
         }
     }
 
     public void tutorWidgetValueChanged(String value, boolean correct) {
-        
+
         StudentProblemDto prob = _studentProblemGrid.getSelectionModel().getSelectedItem();
-        if(prob != null) {
-            /** Since a widget value has been set, it is either Correct or Incorrect
+        if (prob != null) {
+            /**
+             * Since a widget value has been set, it is either Correct or
+             * Incorrect
              * 
              */
-            prob.setStatus(correct?"Correct":"Incorrect");
+            prob.setStatus(correct ? "Correct" : "Incorrect");
             _studentProblemGrid.getStore().update(prob);
-            
-            
+
             saveAssignmentProblemStatusToServer(prob);
         }
     }
-    
-    
+
     private void saveAssignmentProblemStatusToServer(final StudentProblemDto prob) {
         new RetryAction<RpcData>() {
             @Override
             public void attempt() {
                 String status = prob.getStatus();
-                SaveAssignmentProblemStatusAction action = new SaveAssignmentProblemStatusAction(UserInfoBase.getInstance().getUid(),_assignment.getAssignment().getAssignKey(),prob.getPid(),status);
+                SaveAssignmentProblemStatusAction action = new SaveAssignmentProblemStatusAction(UserInfoBase
+                        .getInstance().getUid(), _assignment.getAssignment().getAssignKey(), prob.getPid(), status);
                 setAction(action);
                 CmShared.getCmService().execute(action, this);
             }
@@ -189,12 +209,12 @@ public class AssignmentProblemListPanel extends SimpleContainer {
             public void oncapture(RpcData data) {
                 CmLogger.debug("Assignment problem status saved: " + data);
             }
-        }.register();   
+        }.register();
     }
 
     private ProblemType extractProblemType(StudentProblemDto prob) {
-        for(StudentProblemDto p: _assignment.getAssigmentStatuses()) {
-            if(p.getProblem().getPid().equals(prob.getPid())) {
+        for (StudentProblemDto p : _assignment.getAssigmentStatuses()) {
+            if (p.getProblem().getPid().equals(prob.getPid())) {
                 return p.getProblem().getProblemType();
             }
         }
@@ -202,13 +222,14 @@ public class AssignmentProblemListPanel extends SimpleContainer {
     }
 
     AssignmentProblem _assignmentProblem;
-    /** Track the currently loaded AssigmentProblem
+
+    /**
+     * Track the currently loaded AssigmentProblem
      * 
-     * variable is used when determining if a problem is
-     * 'complete'.
-     *  
-     *  Called via EventBus.
-     *  
+     * variable is used when determining if a problem is 'complete'.
+     * 
+     * Called via EventBus.
+     * 
      * @param assProb
      */
     private void setAssignmentLoaded(AssignmentProblem assProb) {
@@ -216,19 +237,19 @@ public class AssignmentProblemListPanel extends SimpleContainer {
     }
 
     StudentAssignment _assignment;
+
     public void loadAssignment(StudentAssignment assignment) {
         _assignment = assignment;
         _studentProblemGrid.getSelectionModel().setSelection(new ArrayList<StudentProblemDto>());
         try {
             _studentProblemGrid.getStore().clear();
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         _studentProblemGrid.getStore().addAll(assignment.getAssigmentStatuses());
-        
+
         // select first, if available
-        if(_studentProblemGrid.getStore().size() > 0) {
+        if (_studentProblemGrid.getStore().size() > 0) {
             _studentProblemGrid.getSelectionModel().select(_studentProblemGrid.getStore().get(0), false);
         }
     }
@@ -236,9 +257,7 @@ public class AssignmentProblemListPanel extends SimpleContainer {
     public interface AssignmentProblemListCallback {
         void problemSelected(String title, ProblemDto problem);
     }
-    
-    
-    
+
     static {
         CmRpc.EVENT_BUS.addHandler(ShowWorkModifiedEvent.TYPE, new ShowWorkModifiedHandler() {
             @Override
@@ -247,21 +266,23 @@ public class AssignmentProblemListPanel extends SimpleContainer {
                 __lastInstance.whiteboardUpdated();
             }
         });
-        
+
         CmRpc.EVENT_BUS.addHandler(AssignmentProblemLoadedEvent.TYPE, new AssignmentProblemLoadedHandler() {
             @Override
             public void assignmentProblemLoaded(AssignmentProblem assProb) {
                 __lastInstance.setAssignmentLoaded(assProb);
             }
         });
-        
-//        CmRpc.EVENT_BUS.addHandler(TutorWidgetValueChangedEvent.TYPE, new TutorWidgetValueChangedHandler() {
-//            @Override
-//            public void widgetValueChanged(String value, boolean correct) {
-//                Log.debug("Assignment Tutor widget value changed handled (" + correct + "): " + value);
-//                __lastInstance.tutorWidgetValueChanged(value, correct);
-//            }
-//        });
+
+        // CmRpc.EVENT_BUS.addHandler(TutorWidgetValueChangedEvent.TYPE, new
+        // TutorWidgetValueChangedHandler() {
+        // @Override
+        // public void widgetValueChanged(String value, boolean correct) {
+        // Log.debug("Assignment Tutor widget value changed handled (" + correct
+        // + "): " + value);
+        // __lastInstance.tutorWidgetValueChanged(value, correct);
+        // }
+        // });
 
     }
 
