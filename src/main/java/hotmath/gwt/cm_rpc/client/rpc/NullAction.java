e9154d3368ceec3796cfae01810ef7847dc01bcc
Casey Rodgers <grodgers@gmail.com>
2012-11-20 18:37:12 +0000

    *** empty log message ***

--- src/main/java/hotmath/gwt/cm_rpc/client/rpc/NullAction.java
+++ src/main/java/hotmath/gwt/cm_rpc/client/rpc/NullAction.java
@@ -1,6 +1,5 @@
 package hotmath.gwt.cm_rpc.client.rpc;
 
-import hotmath.gwt.cm_rpc.server.service.ActionHandlerManualConnectionManagement;
 
 
 /** Null do nothing action for testing
@@ -8,7 +7,7 @@ import hotmath.gwt.cm_rpc.server.service.ActionHandlerManualConnectionManagement
  * @author casey
  *
  */
-public class NullAction extends ActionBase implements Action<RpcData>, ActionHandlerManualConnectionManagement {
+public class NullAction extends ActionBase implements Action<RpcData>{
 
     public NullAction() {}
 }
--- src/main/java/hotmath/gwt/shared/server/service/command/NullCommand.java
+++ src/main/java/hotmath/gwt/shared/server/service/command/NullCommand.java
@@ -5,10 +5,11 @@ import hotmath.gwt.cm_rpc.client.rpc.NullAction;
 import hotmath.gwt.cm_rpc.client.rpc.Response;
 import hotmath.gwt.cm_rpc.client.rpc.RpcData;
 import hotmath.gwt.cm_rpc.server.rpc.ActionHandler;
+import hotmath.gwt.cm_rpc.server.service.ActionHandlerManualConnectionManagement;
 
 import java.sql.Connection;
 
-public class NullCommand implements ActionHandler<NullAction, RpcData> {
+public class NullCommand implements ActionHandler<NullAction, RpcData>, ActionHandlerManualConnectionManagement {
 
     @Override
     public Class<? extends Action<? extends Response>> getActionType() {
