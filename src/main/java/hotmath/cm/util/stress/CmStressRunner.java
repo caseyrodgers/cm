b2fd22c03588cd885d08bbee8c7ed868d96288a1
Casey Rodgers <grodgers@gmail.com>
2012-11-23 21:15:24 +0000

    *** empty log message ***

--- src/main/java/hotmath/cm/util/stress/CmStressRunner.java
+++ src/main/java/hotmath/cm/util/stress/CmStressRunner.java
@@ -9,30 +9,29 @@ import java.sql.ResultSet;
 
 import org.apache.log4j.Logger;
 
-
 public class CmStressRunner {
 
     private int count;
     private int delay;
     private String testClassName;
     private int adminId;
-    
+    boolean _externalTable = true;
+
     static Logger __logger = Logger.getLogger(CmStressRunner.class);
 
     public interface CallBack {
         void onEnd();
     }
-    
+
     public CmStressRunner(int adminId, int count, int delay, String testClassName) throws ClassNotFoundException {
         this.adminId = adminId;
         this.count = count;
         this.delay = delay;
-        this.testClassName = testClassName!=null?testClassName:CmStressLoginAndGetActiveFlow.class.getName();
-        
-        
+        this.testClassName = testClassName != null ? testClassName : CmStressLoginAndGetActiveFlow.class.getName();
+
         // error early
         Class.forName(this.testClassName);
-        
+
     }
 
     public void runTests() {
@@ -40,29 +39,41 @@ public class CmStressRunner {
         Connection conn = null;
         PreparedStatement ps = null;
         try {
-            String sql = "select a.user_name, u.user_passcode,u.uid,u.admin_id " + "from HA_ADMIN a "
+            String sql = null;
+            sql = "select a.user_name, u.user_passcode,u.uid,u.admin_id " + "from HA_ADMIN a "
                     + " JOIN SUBSCRIBERS s on s.id = a.subscriber_id " + " JOIN HA_USER u on u.admin_id = a.aid "
-                    + " JOIN CM_USER_PROGRAM p on p.id = u.user_prog_id " 
-                    + " where u.is_active = 1 " 
+                    + " JOIN CM_USER_PROGRAM p on p.id = u.user_prog_id " + " where u.is_active = 1 "
                     + " and u.admin_id != 13 " + " and u.date_created > '2010-11-01' "
                     + " and is_auto_create_template = 0 ";
-            if(adminId > 0) {
+            if (adminId > 0) {
                 sql += " and a.aid = " + adminId;
+            } else {
+                /**
+                 * use external table
+                 * 
+                 */
+                sql += " and u.uid in (select uid from CM_STRESS_RUNNER_UIDS where used = 0)";
             }
-            
-            sql  += " order by rand() " + " limit " + count;
+
+            sql += " order by rand() " + " limit " + count;
 
             conn = HMConnectionPool.getConnection();
             ps = conn.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
+
+            if (!rs.first()) {
+                throw new Exception("No users to test");
+            }
+            do {
                 String uName = rs.getString("user_name");
                 String uPass = rs.getString("user_passcode");
                 int uid = rs.getInt("uid");
                 int aid = rs.getInt("admin_id");
-                
-                new CmStressThread(aid, uid, uName, uPass,delay, this.testClassName).runTest();
-            }
+
+                new CmStressThread(aid, uid, uName, uPass, delay, this.testClassName).runTest();
+
+                conn.createStatement().execute("update CM_STRESS_RUNNER_UIDS set used = 1 where uid = " + uid);
+            } while (rs.next());
 
         } catch (Exception e) {
             __logger.error("Error running stress tests", e);
@@ -73,4 +84,3 @@ public class CmStressRunner {
     }
 
 }
-
