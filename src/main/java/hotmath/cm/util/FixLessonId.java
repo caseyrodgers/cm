fec2d0b92fdbd5b965cbe0d6d9623b79e192fc8d
Bob Hall <rfhall3@yahoo.com>
2012-11-30 18:21:33 +0000

    none

--- src/main/java/hotmath/cm/util/FixLessonId.java
+++ src/main/java/hotmath/cm/util/FixLessonId.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 
 public class FixLessonId {
-    public void doIt() {
+    public void doIt(int start, int limit) {
         Connection conn=null;
         PreparedStatement ps = null;
         PreparedStatement ps2 = null;
@@ -22,11 +22,11 @@ public class FixLessonId {
         try {
             conn = HMConnectionPool.getConnection();
 
-            String sql = "select run_id, id from HA_TEST_RUN_LESSON";
-            ps = conn.prepareStatement(sql);
+            String sql = "select run_id, id from HA_TEST_RUN_LESSON where id >= " + start + " limit " + limit;
 
+            ps = conn.prepareStatement(sql);
             rs = ps.executeQuery();
-                
+
             int oldRunId = 0;
 
             String sql2 = "select id, lid, run_id from HA_TEST_RUN_LESSON_PID_FIX where run_id = ? order by lid";
@@ -34,46 +34,53 @@ public class FixLessonId {
 
             List<Integer> lidList = new ArrayList<Integer>();
             int counter = 1;
+            int lastId;
             while (rs.next()) {
+
             	int runId = rs.getInt("run_id");
-			    if (runId != oldRunId && oldRunId > 0) {
-	            	//if (counter++ == 10) break;
-                	System.out.println("counter: " + counter++);
-	  				System.out.println("lidList: " + lidList.size());
-	     			System.out.println("oldRunId: " + oldRunId);
-    
-	    			ps2 = conn.prepareStatement(sql2);
-	    			ps3 = conn.prepareStatement(sql3);
-	    			ps2.setInt(1, oldRunId);
-	    			rs2 = ps2.executeQuery();
+            	lastId = rs.getInt("id");
 
-	    			int oldLid = 0;
-		    		int newLid = 0;
-	    			int idx = 0;
-	    			while (rs2.next()) {
-		    			int lid = rs2.getInt("lid");
-	    				if (oldLid != lid) {
-		    				if (idx == lidList.size()) {
-		    					// reset
-	    						idx = 0;
-			 		    	}
-						    newLid = lidList.get(idx++);
-					    }
-					    oldLid = lid;
-					    // reassign lid
-					    System.out.println("change lid from: " + lid + " to "
-						  	+ newLid);
-					    ps3.setInt(1, newLid);
-					    ps3.setInt(2, rs2.getInt("id"));
-					    ps3.executeUpdate();
-				    }
-				    lidList = new ArrayList<Integer>();
-			    }
-				oldRunId = runId;
-			    lidList.add(rs.getInt("id"));
-		    }
+            	if (runId != oldRunId && oldRunId > 0) {
+                    if (counter++%1000 == 0) {
+                        System.out.println("counter: " + counter);
+                        System.out.println("lidList: " + lidList.size());
+                        System.out.println("oldRunId: " + oldRunId);
+                    }
 
-    	} catch (Exception e) {
+                    ps2 = conn.prepareStatement(sql2);
+                    ps3 = conn.prepareStatement(sql3);
+                    ps2.setInt(1, oldRunId);
+                    rs2 = ps2.executeQuery();
+
+                    int oldLid = 0;
+                    int newLid = 0;
+                    int idx = 0;
+                    while (rs2.next()) {
+                        int lid = rs2.getInt("lid");
+                        if (oldLid != lid) {
+                            if (idx == lidList.size()) {
+                                // reset
+                                idx = 0;
+                             }
+                            newLid = lidList.get(idx++);
+                            //System.out.println("changing lid from: " + lid + " to "
+                            //          + newLid);
+                        }
+                        oldLid = lid;
+
+                        // reassign lid
+                        ps3.setInt(1, newLid);
+                        ps3.setInt(2, rs2.getInt("id"));
+                        if (ps3.executeUpdate() != 1) {
+                        	System.out.println("COULD NOT UPDATE RECORD: id: " + rs2.getInt("id"));
+                        }
+                    }
+                    lidList = new ArrayList<Integer>();
+                }
+                oldRunId = runId;
+                lidList.add(rs.getInt("id"));
+            }
+        } catch (Exception e) {
 	    	e.printStackTrace();
 	    } finally {
 		    SqlUtilities.releaseResources(null, ps3, null);
@@ -85,8 +92,11 @@ public class FixLessonId {
     
     static public void main(String as[]) {
         try {
+            int start=Integer.parseInt(as[0]);
+            int limit=Integer.parseInt(as[1]);
+
             FixLessonId fli = new FixLessonId();
-            fli.doIt();
+            fli.doIt(start, limit);
         	
         } catch (Exception e) {
             e.printStackTrace();
