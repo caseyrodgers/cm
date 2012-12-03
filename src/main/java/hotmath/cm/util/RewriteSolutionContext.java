70e69843a846adb2d71bb14d9bf053d99d2330ec
Bob Hall <rfhall3@yahoo.com>
2012-12-03 07:19:30 +0000

    do in chunks to reduce memory requirement

--- src/main/java/hotmath/cm/util/RewriteSolutionContext.java
+++ src/main/java/hotmath/cm/util/RewriteSolutionContext.java
@@ -19,86 +19,98 @@ public class RewriteSolutionContext {
 
     final static Logger __logger = Logger.getLogger(RewriteSolutionContext.class);
 
-    public void doIt(int start, int limit) {
-        Connection conn=null;
-        PreparedStatement ps = null;
-        PreparedStatement ps2 = null;
-        ResultSet rs = null;
-        long startTime = System.currentTimeMillis();
+    public void doIt(int start, int limit, int recsToProcess) {
+    	Connection conn=null;
+    	PreparedStatement ps = null;
+    	PreparedStatement ps2 = null;
+    	ResultSet rs = null;
+    	long startTime = System.currentTimeMillis();
+    	int count = 0;
+    	int lastId = 0 ;
 
-        try {
-            conn = HMConnectionPool.getConnection();
+    	String sql = "select id, time_viewed, run_id, pid, problem_number, variables from HA_SOLUTION_CONTEXT where id >= ? limit ?";
 
-            String sql = "select id, time_viewed, run_id, pid, problem_number, variables from HA_SOLUTION_CONTEXT where id >= " + start + " limit " + limit;
+    	try {
+    		conn = HMConnectionPool.getConnection();
+    		ps = conn.prepareStatement(sql);
 
-            ps = conn.prepareStatement(sql);
-            rs = ps.executeQuery();
+    		int compressCount = 0;
+    		int skipCount = 0;
+			int counter = 0;
 
-            String sql2 = "insert into HA_SOLUTION_CONTEXT_NEW (time_viewed, run_id, pid, problem_number, variables) values (?,?,?,?,?)";
-            ps2 = conn.prepareStatement(sql2);
+    		while(count < recsToProcess) {
 
-            int counter = 1;
-            int compressCount = 0;
-            int skipCount = 0;
-            int lastId = 0 ;
-            while (rs.next()) {
+    			ps.setInt(1, start + count);
+    			ps.setInt(2, limit);
 
-            	int id = rs.getInt("id");
-            	lastId = id;
+    			rs = ps.executeQuery();
 
-                if ((counter++)%1000 == 0) {
-                    __logger.debug("counter: " + counter);
-                }
+    			String sql2 = "insert into HA_SOLUTION_CONTEXT_NEW (time_viewed, run_id, pid, problem_number, variables) values (?,?,?,?,?)";
+    			ps2 = conn.prepareStatement(sql2);
 
-                byte[] compressed = rs.getBytes("variables");
-            	if (compressed[0] != "{".getBytes("UTF-8")[0]) {
-            		skipCount++;
-    				ps2.setBytes(5, compressed);
-            	}
-            	else {
-            		compressCount++;
-            		String context = rs.getString("variables");
-        			byte[] inBytes = null;
-        			try {
-        				inBytes = context.getBytes("UTF-8");
+    			while (rs.next()) {
 
-        				byte[] outBytes = CompressHelper.compress(inBytes);
-        				ps2.setBytes(5, outBytes);
+    				count++;
+    				int id = rs.getInt("id");
+    				lastId = id;
 
-        				if (__logger.isDebugEnabled()) __logger.debug("in len: " + inBytes.length +", out len: " + outBytes.length);
+    				if ((counter++)%1000 == 0) {
+    					__logger.debug("counter: " + counter);
+    				}
 
-        			} catch (UnsupportedEncodingException e) {
-        				__logger.error(String.format("*** Error saving solution context for id: %d", id), e);
-        				throw new SQLException(e.getLocalizedMessage());
-        			}
-            	}
-    			ps2.setDate(1, rs.getDate("time_viewed"));
-    			ps2.setInt(2, rs.getInt("run_id"));
-    			ps2.setString(3, rs.getString("pid"));
-    			ps2.setInt(4, rs.getInt("problem_number"));
-				if (ps2.executeUpdate() != 1) {
-                	__logger.error("*** Could NOT INSERT record: id: " + id);
-                }
+    				byte[] compressed = rs.getBytes("variables");
+    				if (compressed[0] != "{".getBytes("UTF-8")[0]) {
+    					skipCount++;
+    					ps2.setBytes(5, compressed);
+    				}
+    				else {
+    					compressCount++;
+    					String context = rs.getString("variables");
+    					byte[] inBytes = null;
+    					try {
+    						inBytes = context.getBytes("UTF-8");
 
-            }
-            __logger.info(String.format("Compressed: %d, skipped: %d", compressCount, skipCount));
-            __logger.info("lastId: " + lastId);
-        } catch (Exception e) { 
-	    	e.printStackTrace();
-	    } finally {
-		    SqlUtilities.releaseResources(null, ps2, null);
-		    SqlUtilities.releaseResources(rs, ps, conn);
-	    }
-	    __logger.info("Completed updating HA_SOLUTION_CONTEXT in: " + (System.currentTimeMillis()-startTime) + " msec");
-	}
+    						byte[] outBytes = CompressHelper.compress(inBytes);
+    						ps2.setBytes(5, outBytes);
+
+    						if (__logger.isDebugEnabled()) __logger.debug("in len: " + inBytes.length +", out len: " + outBytes.length);
+
+    					} catch (UnsupportedEncodingException e) {
+    						__logger.error(String.format("*** Error saving solution context for id: %d", id), e);
+    						throw new SQLException(e.getLocalizedMessage());
+    					}
+    				}
+    				ps2.setDate(1, rs.getDate("time_viewed"));
+    				ps2.setInt(2, rs.getInt("run_id"));
+    				ps2.setString(3, rs.getString("pid"));
+    				ps2.setInt(4, rs.getInt("problem_number"));
+    				if (ps2.executeUpdate() != 1) {
+    					__logger.error("*** Could NOT INSERT record: id: " + id);
+    				}
+
+    			}
+    		}
+    		__logger.info(String.format("Compressed: %d, skipped: %d", compressCount, skipCount));
+    		__logger.info("lastId: " + lastId);
+    	} catch (Exception e) { 
+    		e.printStackTrace();
+    	} finally {
+    		SqlUtilities.releaseResources(null, ps2, null);
+    		SqlUtilities.releaseResources(rs, ps, conn);
+    	}
+    	__logger.info("Completed updating HA_SOLUTION_CONTEXT in: " + (System.currentTimeMillis()-startTime) + " msec");
+    }
     
     static public void main(String as[]) {
         try {
-            int start=Integer.parseInt(as[0]);
-            int limit=Integer.parseInt(as[1]);
+            int start = Integer.parseInt(as[0]);
+            int limit = Integer.parseInt(as[1]);
+            int recsToProcess = Integer.parseInt(as[2]);
+
+            __logger.info(String.format("start: %d,  limit: %d, recsToProcess: %d", start, limit, recsToProcess));
 
             RewriteSolutionContext fli = new RewriteSolutionContext();
-            fli.doIt(start, limit);
+            fli.doIt(start, limit, recsToProcess);
             System.exit(0);
         	
         } catch (Exception e) {
