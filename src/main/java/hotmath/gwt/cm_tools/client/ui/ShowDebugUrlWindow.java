6897101f04901f777d21334d15783ac310cf8789
Casey Rodgers <grodgers@gmail.com>
2012-12-21 21:16:08 +0000

    *** empty log message ***

--- src/main/java/hotmath/gwt/cm_tools/client/ui/ShowDebugUrlWindow.java
+++ src/main/java/hotmath/gwt/cm_tools/client/ui/ShowDebugUrlWindow.java
@@ -4,6 +4,7 @@ import hotmath.gwt.cm_rpc.client.UserInfo;
 import hotmath.gwt.shared.client.CmShared;
 
 import com.google.gwt.user.client.ui.Anchor;
+import com.sencha.gxt.widget.core.client.container.FlowLayoutContainer;
 
 /** Display the CM debug URL for the current user
  * 
@@ -15,10 +16,14 @@ public class ShowDebugUrlWindow extends GWindow {
 	public ShowDebugUrlWindow() {
 		super(true);
 		setHeadingText("Debug URL");
-		setPixelSize(200,50);
+		setPixelSize(400,50);
         String url = CmShared.getServerForCmStudent() + "/loginService?debug=true&uid="  + UserInfo.getInstance().getUid();
         Anchor anchor = new Anchor(url);
-        setWidget(anchor);
+        anchor.setHref(url);
+        
+        FlowLayoutContainer flc = new FlowLayoutContainer();
+        flc.add(anchor);
+        setWidget(flc);
         
         setVisible(true);
 	}
