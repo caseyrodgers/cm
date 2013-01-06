22efa112633660a1f3487724435a3d95f49b32dc
Casey Rodgers <grodgers@gmail.com>
2013-01-06 22:17:31 +0000

    *** empty log message ***

--- src/main/java/hotmath/gwt/cm_tools/client/model/StudentModelExt.java
+++ src/main/java/hotmath/gwt/cm_tools/client/model/StudentModelExt.java
@@ -5,44 +5,6 @@ import java.io.Serializable;
 
 public class StudentModelExt extends BaseModel implements Serializable, StudentModelI {
 
-    public static final String GROUP_KEY = "group";
-    public static final String GROUP_ID_KEY = "groupId";
-    public static final String SECTION_COUNT_KEY = "sectionCount";
-    public static final String SECTION_NUM_KEY = "sectionNum";
-    public static final String USER_PROGRAM_KEY = "userProgramId";
-    public static final String CHAPTER_KEY = "chapter";
-    public static final String LAST_QUIZ_KEY = "last-quiz";
-    public static final String HAS_LAST_QUIZ_KEY = "has-last-quiz";
-    public static final String LAST_LOGIN_KEY = "last-login";
-    public static final String HAS_LAST_LOGIN_KEY = "has-last-login";
-    public static final String JSON_KEY = "json";
-    public static final String STATUS_KEY = "status";
-    public static final String UID_KEY = "uid";
-    public static final String EMAIL_KEY = "email";
-    public static final String ADMIN_UID_KEY = "admin_uid";
-    public static final String SHOW_WORK_STATE_KEY = "show-work-state";
-    public static final String TOTAL_USAGE_KEY = "total-usage";
-    public static final String TUTORING_STATE_KEY = "tutoring-state";
-    public static final String TUTORING_USE_KEY = "tutoring-use";
-    public static final String HAS_TUTORING_USE_KEY = "has-tutoring-use";
-    public static final String NAME_KEY = "name";
-    public static final String PASSCODE_KEY = "passcode";
-    public static final String BACKGROUND_STYLE = "background_style";
-    public static final String DEMO_USER_KEY = "demo_user";
-    public static final String PROGRAM_DESCR_KEY = "program";
-    public static final String PROGRAM_TYPE_KEY  = "program-type";
-    public static final String PASSING_COUNT_KEY = "passing-count";
-    public static final String HAS_PASSING_COUNT_KEY = "has-passing-count";
-    public static final String NOT_PASSING_COUNT_KEY = "not-passing-count";
-
-    public static final String PROGRAM_KEY = "studyProgram";
-    public static final String SETTINGS_KEY = "optionSettings";
-    public static final String PASS_PERCENT_KEY = "pass-percent";
-    public static final String TUTORING_AVAIL_KEY = "tutoring-avail";
-    public static final String LIMIT_GAMES_KEY = "limit-games";
-    public static final String STOP_AT_PROGRAM_END_KEY = "stop-at-program-end";
-    public static final String SECTION_IS_SETTABLE = "section-is-settable";
-
 
     private boolean hasExtendedData = false;
 
--- src/main/java/hotmath/gwt/shared/server/service/command/GetStudentGridPageCommand.java
+++ src/main/java/hotmath/gwt/shared/server/service/command/GetStudentGridPageCommand.java
@@ -8,6 +8,7 @@ import hotmath.gwt.cm_rpc.client.rpc.Action;
 import hotmath.gwt.cm_rpc.client.rpc.Response;
 import hotmath.gwt.cm_rpc.server.rpc.ActionHandler;
 import hotmath.gwt.cm_tools.client.model.GroupInfoModel;
+import hotmath.gwt.cm_tools.client.model.StudentModelExt;
 import hotmath.gwt.cm_tools.client.model.StudentModelI;
 import hotmath.gwt.shared.client.model.CmStudentPagingLoadResult;
 import hotmath.gwt.shared.client.rpc.action.GetStudentGridPageAction;
@@ -358,61 +359,61 @@ class StudentGridComparator implements Comparator<StudentModelI> {
         System.out.println("SORTING NOT ENABLED YET");
         
         
-//    	if (sortField.equals(StudentModelExt.NAME_KEY)) {
-//            return p1.getName().compareToIgnoreCase(p2.getName());
-//        } else if (sortField.equals(StudentModelI.PASSCODE_KEY)) {
-//            return p1.getPasscode().compareToIgnoreCase(p2.getPasscode());
-//        } else if (sortField.equals(StudentModelI.GROUP_KEY)) {
-//            return p1.getGroup().compareToIgnoreCase(p2.getGroup());
-//        } else if (sortField.equals(StudentModelI.PROGRAM_DESCR_KEY)) {
-//            return p1.getProgram().getProgramDescription().compareToIgnoreCase(p2.getProgram().getProgramDescription());
-//        } else if (sortField.equals(StudentModelI.STATUS_KEY)) {
-//            return p1.getStatus().compareToIgnoreCase(p2.getStatus());
-//        } else if (sortField.equals(StudentModelI.LAST_LOGIN_KEY)) {
-//            return nz(p1.getLastLogin()).compareToIgnoreCase(nz(p2.getLastLogin()));
-//        } else if (sortField.equals(StudentModelI.TOTAL_USAGE_KEY)) {
-//            return p1.getTotalUsage() - p2.getTotalUsage();
-//        } else if (sortField.equals(StudentModelI.TUTORING_USE_KEY)) {
-//            return p1.getTutoringUse() - p2.getTutoringUse();
-//        } else if (sortField.equals(StudentModelI.LAST_QUIZ_KEY)) { 
+    	if (sortField.equals(StudentModelExt.NAME_KEY)) {
+            return p1.getName().compareToIgnoreCase(p2.getName());
+        } else if (sortField.equals(StudentModelI.PASSCODE_KEY)) {
+            return p1.getPasscode().compareToIgnoreCase(p2.getPasscode());
+        } else if (sortField.equals(StudentModelI.GROUP_KEY)) {
+            return p1.getGroup().compareToIgnoreCase(p2.getGroup());
+        } else if (sortField.equals(StudentModelI.PROGRAM_DESCR_KEY)) {
+            return p1.getProgram().getProgramDescription().compareToIgnoreCase(p2.getProgram().getProgramDescription());
+        } else if (sortField.equals(StudentModelI.STATUS_KEY)) {
+            return p1.getStatus().compareToIgnoreCase(p2.getStatus());
+        } else if (sortField.equals(StudentModelI.LAST_LOGIN_KEY)) {
+            return nz(p1.getLastLogin()).compareToIgnoreCase(nz(p2.getLastLogin()));
+        } else if (sortField.equals(StudentModelI.TOTAL_USAGE_KEY)) {
+            return p1.getTotalUsage() - p2.getTotalUsage();
+        } else if (sortField.equals(StudentModelI.TUTORING_USE_KEY)) {
+            return p1.getTutoringUse() - p2.getTutoringUse();
+        } else if (sortField.equals(StudentModelI.LAST_QUIZ_KEY)) { 
+        	
+            String lq1 = (p1IsCustom) ? "" : p1.getLastQuiz();
+            String lq2 = (p2IsCustom) ? "" : p2.getLastQuiz();
+            
+            if ((lq1 == null || lq1.trim().length() == 0) && (lq2 == null | lq2.trim().length() == 0)) return 0;
+            
+            return nz(lq1).compareToIgnoreCase(nz(lq2));
+
+        } else if(sortField.equals(StudentModelI.PASSING_COUNT_KEY)) {
+        	
+        	int t1 = (p1IsCustom) ? 0 : p1.getNotPassingCount() + p1.getPassingCount();
+        	int t2 = (p2IsCustom) ? 0 : p2.getNotPassingCount() + p2.getPassingCount();
+
+            // check total # quizzes
+        	if (t1 != t2) return t1 - t2;
+        	
+        	// total same, sort by number passed
+        	int pass1 = (p1IsCustom) ? 0 : p1.getPassingCount();
+        	int pass2 = (p2IsCustom) ? 0 : p2.getPassingCount();
+        	int pDiff = pass1 - pass2;
+        	if (pDiff != 0) return pDiff;
 //        	
-//            String lq1 = (p1IsCustom) ? "" : p1.getLastQuiz();
-//            String lq2 = (p2IsCustom) ? "" : p2.getLastQuiz();
-//            
-//            if ((lq1 == null || lq1.trim().length() == 0) && (lq2 == null | lq2.trim().length() == 0)) return 0;
-//            
-//            return nz(lq1).compareToIgnoreCase(nz(lq2));
-//
-//        } else if(sortField.equals(StudentModelI.PASSING_COUNT_KEY)) {
-//        	
-//        	int t1 = (p1IsCustom) ? 0 : p1.getNotPassingCount() + p1.getPassingCount();
-//        	int t2 = (p2IsCustom) ? 0 : p2.getNotPassingCount() + p2.getPassingCount();
-//
-//            // check total # quizzes
-//        	if (t1 != t2) return t1 - t2;
-//        	
-//        	// total same, sort by number passed
-//        	int pass1 = (p1IsCustom) ? 0 : p1.getPassingCount();
-//        	int pass2 = (p2IsCustom) ? 0 : p2.getPassingCount();
-//        	int pDiff = pass1 - pass2;
-//        	if (pDiff != 0) return pDiff;
-////        	
-////            // following test disabled for now
-////        	if (false && t1 > 0 && t2 > 0) {
-////            	// sort on ratio of passed quizzes
-////            	float f1 = (float) p1.getPassingCount() / (float) t1;
-////            	float f2 = (float) p2.getPassingCount() / (float) t2;
-////            	if (f1 != f2) {
-////        	    	return (f1 < f2) ? -1 : 1;
-////        	    }
-////        	}
-//
-//            /** 
-//             *  passed ratios the same or not available, 
-//             *  so sort on total number of tests taken
-//             */
-//            return t1 - t2;
-//        }
+//            // following test disabled for now
+//        	if (false && t1 > 0 && t2 > 0) {
+//            	// sort on ratio of passed quizzes
+//            	float f1 = (float) p1.getPassingCount() / (float) t1;
+//            	float f2 = (float) p2.getPassingCount() / (float) t2;
+//            	if (f1 != f2) {
+//        	    	return (f1 < f2) ? -1 : 1;
+//        	    }
+//        	}
+
+            /** 
+             *  passed ratios the same or not available, 
+             *  so sort on total number of tests taken
+             */
+            return t1 - t2;
+        }
 
         return 0;
     }
