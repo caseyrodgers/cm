9428109aabee59991b973234e4e311cf9e948d22
Casey Rodgers <grodgers@gmail.com>
2013-01-10 20:33:38 +0000

    *** empty log message ***

--- src/main/java/hotmath/gwt/cm_mobile_shared/client/ui/FastButton.java
+++ src/main/java/hotmath/gwt/cm_mobile_shared/client/ui/FastButton.java
@@ -1,16 +1,14 @@
 package hotmath.gwt.cm_mobile_shared.client.ui;
 
-import hotmath.gwt.fastpress.client.TestFastPressElement;
-import hotmath.gwt.fastpress.client.fast.PressHandler;
 
-public class FastButton extends TestFastPressElement {
-    public FastButton() {
-        super("No label");
-
-    }
-    public FastButton(String text, PressHandler pressHandler) {
-        super(text, pressHandler);
-    }
-
-}
+public class FastButton {} //  extends TestFastPressElement {
+//    public FastButton() {
+//        super("No label");
+//
+//    }
+//    public FastButton(String text, PressHandler pressHandler) {
+//        super(text, pressHandler);
+//    }
+//
+//}
 
