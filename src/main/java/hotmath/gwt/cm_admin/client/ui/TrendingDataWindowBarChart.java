bf373b20aa3c6c01c29c92668116d4bc12c2785e
Bob Hall <rfhall3@yahoo.com>
2013-01-22 07:44:11 +0000

    move lesson name to tool tip

--- src/main/java/hotmath/gwt/cm_admin/client/ui/TrendingDataWindowBarChart.java
+++ src/main/java/hotmath/gwt/cm_admin/client/ui/TrendingDataWindowBarChart.java
@@ -1,12 +1,10 @@
 package hotmath.gwt.cm_admin.client.ui;
 
-import hotmath.gwt.cm_tools.client.CmBusyManager;
-import hotmath.gwt.cm_tools.client.model.StudentModelExt;
-import hotmath.gwt.cm_tools.client.model.StudentModelI;
 import hotmath.gwt.cm_rpc.client.rpc.CmList;
 import hotmath.gwt.cm_rpc.client.rpc.CmServiceAsync;
+import hotmath.gwt.cm_tools.client.CmBusyManager;
+import hotmath.gwt.cm_tools.client.model.StudentModelI;
 import hotmath.gwt.shared.client.CmShared;
-import hotmath.gwt.shared.client.data.CmAsyncRequestImplDefault;
 import hotmath.gwt.shared.client.model.TrendingData;
 import hotmath.gwt.shared.client.rpc.action.GetAdminTrendingDataDetailAction;
 import hotmath.gwt.shared.client.util.CmAsyncCallback;
@@ -16,52 +14,38 @@ import java.util.List;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.editor.client.Editor.Path;
-import com.google.gwt.event.logical.shared.ValueChangeEvent;
-import com.google.gwt.event.logical.shared.ValueChangeHandler;
-import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlUtils;
-import com.google.gwt.text.shared.SimpleSafeHtmlRenderer;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.IsWidget;
-import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
-
-import com.sencha.gxt.cell.core.client.SimpleSafeHtmlCell;
 import com.sencha.gxt.chart.client.chart.Chart;
 import com.sencha.gxt.chart.client.chart.Chart.Position;
+//import com.sencha.gxt.chart.client.chart.Legend;
 import com.sencha.gxt.chart.client.chart.axis.CategoryAxis;
 import com.sencha.gxt.chart.client.chart.axis.NumericAxis;
 import com.sencha.gxt.chart.client.chart.event.SeriesItemOverEvent;
 import com.sencha.gxt.chart.client.chart.event.SeriesItemOverEvent.SeriesItemOverHandler;
 import com.sencha.gxt.chart.client.chart.event.SeriesSelectionEvent;
 import com.sencha.gxt.chart.client.chart.event.SeriesSelectionEvent.SeriesSelectionHandler;
-
 import com.sencha.gxt.chart.client.chart.series.BarSeries;
+import com.sencha.gxt.chart.client.chart.series.Series.LabelPosition;
+import com.sencha.gxt.chart.client.chart.series.SeriesLabelConfig;
 import com.sencha.gxt.chart.client.chart.series.SeriesToolTipConfig;
 import com.sencha.gxt.chart.client.draw.RGB;
-import com.sencha.gxt.chart.client.draw.sprite.TextSprite.TextAnchor;
 import com.sencha.gxt.chart.client.draw.sprite.TextSprite;
 import com.sencha.gxt.core.client.ValueProvider;
-import com.sencha.gxt.data.shared.LabelProvider;
 import com.sencha.gxt.data.shared.ListStore;
 import com.sencha.gxt.data.shared.ModelKeyProvider;
 import com.sencha.gxt.data.shared.PropertyAccess;
 import com.sencha.gxt.fx.client.Draggable;
-import com.sencha.gxt.widget.core.client.ContentPanel;
 import com.sencha.gxt.widget.core.client.FramedPanel;
 import com.sencha.gxt.widget.core.client.Resizable;
 import com.sencha.gxt.widget.core.client.Resizable.Dir;
-import com.sencha.gxt.widget.core.client.button.TextButton;
-import com.sencha.gxt.widget.core.client.button.ToggleButton;
 import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
 import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer.VerticalLayoutData;
 import com.sencha.gxt.widget.core.client.event.CollapseEvent;
 import com.sencha.gxt.widget.core.client.event.CollapseEvent.CollapseHandler;
 import com.sencha.gxt.widget.core.client.event.ExpandEvent;
 import com.sencha.gxt.widget.core.client.event.ExpandEvent.ExpandHandler;
-import com.sencha.gxt.widget.core.client.event.SelectEvent;
-import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
-import com.sencha.gxt.widget.core.client.toolbar.ToolBar;
 
 public class TrendingDataWindowBarChart implements IsWidget {
 
@@ -71,9 +55,13 @@ public class TrendingDataWindowBarChart implements IsWidget {
 
 		ValueProvider<TrendingData, String> lessonName();
 
-		@Path("lessonName")
-		ModelKeyProvider<TrendingData> nameKey();
-	}
+		ValueProvider<TrendingData, Integer> number();
+
+		ValueProvider<TrendingData, String> label();
+
+		@Path("number")
+		ModelKeyProvider<TrendingData> numberKey();
+}
 
 	private static final DataPropertyAccess dataAccess = GWT.create(DataPropertyAccess.class);
 
@@ -84,10 +72,10 @@ public class TrendingDataWindowBarChart implements IsWidget {
 	public TrendingDataWindowBarChart(String title, List<TrendingData> data) {
 		this.trendingData = data;
 		this.title = title;
-		//int idx = 1;
-		//for (TrendingData td : data) {
-		//	td.setLessonName("Lesson " + idx++);
-		//}
+		int idx = 1;
+		for (TrendingData td : data) {
+			td.setNumber(idx++);
+		}
 	}
 
 	private Widget _widget;
@@ -96,7 +84,7 @@ public class TrendingDataWindowBarChart implements IsWidget {
 	public Widget asWidget() {
 		if (_widget != null) return _widget;
 
-		final ListStore<TrendingData> store = new ListStore<TrendingData>(dataAccess.nameKey());
+		final ListStore<TrendingData> store = new ListStore<TrendingData>(dataAccess.numberKey());
 		store.addAll(this.trendingData);
 
 		final Chart<TrendingData> chart = new Chart<TrendingData>();
@@ -115,25 +103,33 @@ public class TrendingDataWindowBarChart implements IsWidget {
 		axis.setWidth(50);
 		chart.addAxis(axis);
 
-		CategoryAxis<TrendingData, String> catAxis = new CategoryAxis<TrendingData, String>();
+		CategoryAxis<TrendingData, Integer> catAxis = new CategoryAxis<TrendingData, Integer>();
 		catAxis.setPosition(Position.BOTTOM);
-		catAxis.setField(dataAccess.lessonName());
-		TextSprite sprite = new TextSprite();
-		sprite.setRotation(45);
-		sprite.setTextAnchor(TextAnchor.START);
-		sprite.setY(catAxis.getY()+5.);
+		catAxis.setField(dataAccess.number());
+	    TextSprite catAxisTitle = new TextSprite("Lesson Rank");
+	    catAxisTitle.setFontSize(18);
+	    catAxis.setTitleConfig(catAxisTitle);
+	    catAxis.setHeight(30);
 
-		catAxis.setLabelPadding(-10);
-		catAxis.setLabelConfig(sprite);
-		catAxis.setLabelTolerance(50);
 		chart.addAxis(catAxis);
-
+/*
+	    Legend<TrendingData> legend = new Legend<TrendingData>();
+	    legend.setPosition(Position.BOTTOM);
+	    legend.setItemHighlighting(true);
+	    legend.setItemHiding(false);
+	    chart.setLegend(legend);
+*/ 	    
 		final BarSeries<TrendingData> column = new BarSeries<TrendingData>();
 		column.setYAxisPosition(Position.LEFT);
 		column.addYField(dataAccess.countAssigned());
 		column.addColor(new RGB(148,174,10));
 		column.setColumn(true);
-		final SeriesToolTipConfig<TrendingData> config = new SeriesToolTipConfig<TrendingData>();
+
+	    SeriesLabelConfig<TrendingData> labelConfig = new SeriesLabelConfig<TrendingData>();
+	    labelConfig.setLabelPosition(LabelPosition.OUTSIDE);
+	    column.setLabelConfig(labelConfig);
+
+	    final SeriesToolTipConfig<TrendingData> config = new SeriesToolTipConfig<TrendingData>();
 		config.setLabelProvider(null);
 		column.setToolTipConfig(config);
 		column.addSeriesItemOverHandler(new SeriesItemOverHandler<TrendingData>() {
@@ -141,7 +137,7 @@ public class TrendingDataWindowBarChart implements IsWidget {
 			public void onSeriesOverItem(SeriesItemOverEvent<TrendingData> event) {
 				if (true) {
 					int index = event.getIndex();
-					String msg = "Click to see list of students [" + trendingData.get(index).getCountAssigned() + "]";
+					String msg = trendingData.get(index).getLessonName() + " (click to see students)";
 					SafeHtmlUtils.htmlEscape(msg);
 					config.setBodyHtml(SafeHtmlUtils.htmlEscape(msg));
 					column.setToolTipConfig(config);
@@ -158,13 +154,12 @@ public class TrendingDataWindowBarChart implements IsWidget {
 			}
 		});
 
-
 		chart.addSeries(column);
 
-		ContentPanel panel = new FramedPanel();
+		FramedPanel panel = new FramedPanel();
+		panel.setHeaderVisible(false);
 		panel.getElement().getStyle().setMargin(10, Unit.PX);
 		panel.setCollapsible(true);
-		panel.setHeadingText(this.title);
 		panel.setPixelSize(500, 500);
 		panel.setBodyBorder(true);
 
