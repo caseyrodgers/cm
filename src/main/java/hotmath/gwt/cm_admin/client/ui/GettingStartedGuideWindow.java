fb39370221a014ff1d54b961bbe101f11966b260
Bob Hall <rfhall3@yahoo.com>
2013-02-02 07:35:24 +0000

    gxt2 --> gxt3

--- src/main/java/hotmath/gwt/cm_admin/client/ui/GettingStartedGuideWindow.java
+++ src/main/java/hotmath/gwt/cm_admin/client/ui/GettingStartedGuideWindow.java
@@ -1,51 +1,46 @@
 package hotmath.gwt.cm_admin.client.ui;
 
-import hotmath.gwt.cm_tools.client.ui.CmWindow.CmWindow;
+import hotmath.gwt.cm_tools.client.ui.GWindow;
 
-import com.extjs.gxt.ui.client.event.ButtonEvent;
-import com.extjs.gxt.ui.client.event.SelectionListener;
-import com.extjs.gxt.ui.client.widget.button.Button;
+import com.sencha.gxt.widget.core.client.button.TextButton;
+import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
+import com.sencha.gxt.widget.core.client.event.SelectEvent;
+import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
+
 import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.Frame;
 
-public class GettingStartedGuideWindow extends CmWindow {
+public class GettingStartedGuideWindow extends GWindow {
 
     public  GettingStartedGuideWindow() {
-        setAutoHeight(true);
-        setSize(640,480);
+    	super(true);
+        //setAutoHeight(true);
+        setPixelSize(640,480);
 
         setModal(true);
         setResizable(false);
-        setStyleName("help-window");
-        setHeading("Catchup Math Administrator Getting Started Guide");
+        //setStyleName("help-window");
+        setHeadingText("Catchup Math Administrator Getting Started Guide");
+
+        VerticalLayoutContainer vlc = new VerticalLayoutContainer();
+        vlc.setAdjustForScroll(true);
 
         Frame frame = new Frame("/gwt-resources/cm-admin-getting-started-guide.html");
-        DOM.setElementProperty(frame.getElement(), "frameBorder", "no"); // disable
-        DOM.setElementPropertyInt(frame.getElement(), "border", 0); // disable
-        DOM.setElementPropertyInt(frame.getElement(), "frameSpacing", 0); // disable
         frame.setSize("100%", "450px");
         
-        
-        Button closeBtn = new Button("Close");
-        closeBtn.addSelectionListener(new SelectionListener<ButtonEvent>() {
-            public void componentSelected(ButtonEvent ce) {
-                GettingStartedGuideWindow.this.close();
-            }
-        });
-        addButton(closeBtn);
-
-        
-        Button webinar = new Button("Teaching with Catchup Math Videos");
-        webinar.setStyleAttribute("margin","10px;");
-        webinar.setToolTip("These videos supplement our Getting Started Guide for teachers and administrators");
-        webinar.addSelectionListener(new SelectionListener<ButtonEvent>() {
-            public void componentSelected(ButtonEvent ce) {
+        TextButton webinar = new TextButton("Teaching with Catchup Math Videos", new SelectHandler() {
+            @Override
+            public void onSelect(SelectEvent event) {
                 showTrainingVideosPage();
             }
         });
-        add(webinar);
-        add(frame);
+        //webinar.setStyleAttribute("margin","10px;");
+        webinar.setToolTip("These videos supplement our Getting Started Guide for teachers and administrators");
+
+        vlc.add(webinar);
+        vlc.add(frame);
+
+        add(vlc);
 
         setVisible(true);
     }
