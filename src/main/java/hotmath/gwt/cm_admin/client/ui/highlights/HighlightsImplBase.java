88ab9f4357353bbdda8180a4fdd63abc2ffd8665
Bob Hall <rfhall3@yahoo.com>
2013-02-12 09:28:05 +0000

    gxt2 --> gxt3

---
+++ src/main/java/hotmath/gwt/cm_admin/client/ui/highlights/HighlightsImplBase.java
@@ -0,0 +1,54 @@
+package hotmath.gwt.cm_admin.client.ui.highlights;
+
+import hotmath.gwt.shared.client.rpc.action.HighlightReportLayout;
+
+import com.google.gwt.user.client.ui.Widget;
+
+abstract class HighlightsImplBase  {
+    String name;
+    String toolTip;
+    Widget _widget;
+    
+    HighlightsImplBase(String name, String toolTip) {
+        this.name = name;
+        this.toolTip = toolTip;
+    }
+    
+    public String getToolTip() {
+        return toolTip;
+    }
+    
+    public String getText() {
+        return this.name;
+    }
+    
+    public Widget getWidget() {
+        if(_widget == null)
+            _widget = prepareWidget();
+        return _widget;
+    }
+    
+    /** one time call to draw the gui
+     * 
+     * @return
+     */
+    abstract Widget prepareWidget();
+    
+    
+    /** return the column labels used
+     *  to print the report.
+     *  
+     * @return
+     */
+    protected HighlightReportLayout getReportLayout() {
+        String cols[] = {"Name:75", "Lessons Viewed:25"};
+        HighlightReportLayout rl = new HighlightReportLayout(cols, getReportValues());
+        rl.setCountLabel("Student Count: ");
+        return rl;
+    }
+    
+    protected String[][] getReportValues() {
+        return new String[0][0];
+    }
+}
+
