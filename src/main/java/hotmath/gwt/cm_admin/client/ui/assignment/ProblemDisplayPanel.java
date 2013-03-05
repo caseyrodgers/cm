5cdeb90cd1a88d850abf63152fd3586c5ddf99d9
Bob Hall <rfhall3@yahoo.com>
2013-03-05 10:14:27 +0000

    gt2 --> gxt3

--- src/main/java/hotmath/gwt/cm_admin/client/ui/assignment/ProblemDisplayPanel.java
+++ src/main/java/hotmath/gwt/cm_admin/client/ui/assignment/ProblemDisplayPanel.java
@@ -1,6 +1,7 @@
 package hotmath.gwt.cm_admin.client.ui.assignment;
 
-import com.extjs.gxt.ui.client.widget.Label;
+import com.google.gwt.user.client.ui.Label;
+
 import com.sencha.gxt.widget.core.client.ContentPanel;
 import com.sencha.gxt.widget.core.client.container.BorderLayoutContainer;
 
@@ -9,17 +10,15 @@ public class ProblemDisplayPanel extends BorderLayoutContainer {
     ContentPanel center;
     public ProblemDisplayPanel() {
         center = new ContentPanel();
-        
+
         center.setHeadingHtml("Assignment Problem");
-        
-        
+
         setCenterWidget(center);
     }
-    
-    
+
     public void setShowProblem(String problemLabel, String pid) {
         center.setWidget(new Label("Problem: "+ pid));
-        
+
         center.setHeadingHtml("Assignment Problem: " + problemLabel);
     }
 
