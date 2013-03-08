5b97623bc071839f7331c565f8ee0417ead0af8a
Bob Hall <rfhall3@yahoo.com>
2013-03-08 18:11:55 +0000

    initial commit

---
+++ src/main/java/hotmath/cm/exception/InformationOnlyException.java
@@ -0,0 +1,11 @@
+package hotmath.cm.exception;
+
+import java.io.Serializable;
+
+public class InformationOnlyException extends Exception {
+
+	public InformationOnlyException(String msg) {
+		super(msg);
+	}
+
+}
