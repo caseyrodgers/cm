021a3317f06dbfd6b573703805bb4b35c01a8672
Casey Rodgers <grodgers@gmail.com>
2013-03-21 19:42:08 +0000

    *** empty log message ***

---
+++ src/main/java/hotmath/gwt/shared/client/event/ForceSystemSyncCheckEvent.java
@@ -0,0 +1,27 @@
+package hotmath.gwt.shared.client.event;
+
+
+import com.google.gwt.event.shared.GwtEvent;
+
+/** Forces a call to SystemSyncChecker.checkForUpdate
+ *  
+ *  This will causes a server action to happen and sync the user
+ *  with the known state of the database.
+ *  
+ * @author casey
+ *
+ */
+public class ForceSystemSyncCheckEvent extends GwtEvent<ForceSystemSyncCheckHandler> {
+
+    public static Type<ForceSystemSyncCheckHandler> TYPE = new Type<ForceSystemSyncCheckHandler>();
+    
+    @Override
+    public Type<ForceSystemSyncCheckHandler> getAssociatedType() {
+        return TYPE;
+    }
+
+    @Override
+    protected void dispatch(ForceSystemSyncCheckHandler handler) {
+        handler.forceSyncCheck();
+    }
+}
---
+++ src/main/java/hotmath/gwt/shared/client/event/ForceSystemSyncCheckHandler.java
@@ -0,0 +1,7 @@
+package hotmath.gwt.shared.client.event;
+
+import com.google.gwt.event.shared.EventHandler;
+
+public interface ForceSystemSyncCheckHandler extends EventHandler {
+    void forceSyncCheck();
+}
