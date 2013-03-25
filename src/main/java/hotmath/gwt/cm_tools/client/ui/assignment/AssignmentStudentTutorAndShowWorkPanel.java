bf5a408667e2893e9a7ef9afd33cdda4c11e90a7
Casey Rodgers <grodgers@gmail.com>
2013-03-25 18:40:41 +0000

    *** empty log message ***

--- src/main/java/hotmath/gwt/cm_tools/client/ui/assignment/AssignmentStudentTutorAndShowWorkPanel.java
+++ src/main/java/hotmath/gwt/cm_tools/client/ui/assignment/AssignmentStudentTutorAndShowWorkPanel.java
@@ -2,6 +2,7 @@ package hotmath.gwt.cm_tools.client.ui.assignment;
 
 import hotmath.gwt.cm_rpc.client.model.assignment.Assignment;
 import hotmath.gwt.cm_rpc.client.model.assignment.ProblemDto;
+import hotmath.gwt.cm_rpc.client.model.assignment.ProblemDto.ProblemType;
 import hotmath.gwt.cm_rpc.client.rpc.Action;
 import hotmath.gwt.cm_rpc.client.rpc.CmList;
 import hotmath.gwt.cm_rpc.client.rpc.GetAssignmentWhiteboardDataAction;
@@ -22,6 +23,7 @@ import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.core.client.util.Margins;
 import com.sencha.gxt.widget.core.client.ContentPanel;
 import com.sencha.gxt.widget.core.client.button.TextButton;
+import com.sencha.gxt.widget.core.client.button.ToggleButton;
 import com.sencha.gxt.widget.core.client.container.BorderLayoutContainer;
 import com.sencha.gxt.widget.core.client.container.BorderLayoutContainer.BorderLayoutData;
 import com.sencha.gxt.widget.core.client.event.SelectEvent;
@@ -47,6 +49,7 @@ public class AssignmentStudentTutorAndShowWorkPanel extends ContentPanel {
         void whiteboardUpdated();
     }
     
+    BorderLayoutContainer _container;
     public AssignmentStudentTutorAndShowWorkPanel(String title, final int uid, Assignment assignment, final ProblemDto problem, AssignmentStudentTutorAndShowWorkPanelCallback callBack) {
         _callBack = callBack;
         _uid = uid;
@@ -69,10 +72,29 @@ public class AssignmentStudentTutorAndShowWorkPanel extends ContentPanel {
             }
         });
         
-        BorderLayoutContainer container = new BorderLayoutContainer();
-        BorderLayoutData bd = new BorderLayoutData(.50);
+        _container = new BorderLayoutContainer();
+        
+        BorderLayoutData bd = new BorderLayoutData();
         bd.setSplit(true);
         bd.setCollapsible(true);
+        bd.setMargins(new Margins(5, 10, 5, 5));
+        _container.setCenterWidget(_tutorPanel, bd);
+        
+        addTool(createShowWhiteboardButton());
+        addTool(createShowLessonButton());
+        
+        if(_whiteboardShown || problem.getProblemType() == ProblemType.WHITEBOARD) {
+            showWhiteboard();
+        }
+        
+        setWidget(_container);
+        
+        loadTutor(title, uid, _assignKey, problem);
+    }
+
+    static boolean _whiteboardShown;
+    ContentPanel _showWorkWrapper;
+    private void showWhiteboard() {
         
         _showWork = new ShowWorkPanel(new ShowWorkPanelCallbackDefault() {
             @Override
@@ -82,29 +104,23 @@ public class AssignmentStudentTutorAndShowWorkPanel extends ContentPanel {
             
             @Override
             public void showWorkIsReady() {
-                loadAssignmentWhiteboardData(uid, _assignKey, problem.getPid());
+                loadAssignmentWhiteboardData(_uid, _assignKey, _problem.getPid());
             }
         });
-        ContentPanel cpWrapper = new ContentPanel();
-        cpWrapper.setWidget(_showWork);
-        container.setEastWidget(cpWrapper,  bd);
-        
-        bd = new BorderLayoutData();
+
+        BorderLayoutData bd = new BorderLayoutData(.50);
         bd.setSplit(true);
         bd.setCollapsible(true);
-        bd.setMargins(new Margins(5, 10, 5, 5));
-        container.setCenterWidget(_tutorPanel, bd);
-        
-        
-        
-        addTool(createShowLessonButton());
-        
-        
-        setWidget(container);
-        
-        loadTutor(title, uid, _assignKey, problem);
-    }
 
+        
+        _showWorkWrapper = new ContentPanel();
+        _showWorkWrapper.setWidget(_showWork);
+        _container.setEastWidget(_showWorkWrapper,  bd);
+        
+        _container.forceLayout();
+        
+        _whiteboardShown=true;
+    }
     
     private Widget createShowLessonButton() {
         TextButton showLesson = new TextButton("Show Lesson");
@@ -116,6 +132,27 @@ public class AssignmentStudentTutorAndShowWorkPanel extends ContentPanel {
         });
         return showLesson;
     }
+    
+    private Widget createShowWhiteboardButton() {
+        final ToggleButton showLesson = new ToggleButton("Show/Hide Whiteboard");
+        showLesson.addSelectHandler(new SelectHandler() {
+            @Override
+            public void onSelect(SelectEvent event) {
+                if(showLesson.getValue()) {
+                    showWhiteboard();
+                    loadAssignmentWhiteboardData(_uid,_assignKey,_problem.getPid());
+                }
+                else {
+                    _container.remove(_showWorkWrapper);
+                    forceLayout();
+                    
+                    _whiteboardShown=false;
+                }
+            }
+        });
+        return showLesson;
+    }
+
 
     private void showAssociatedLesson() {
         Log.debug("Show lesson for: " + _problem.getLesson());
@@ -131,7 +168,9 @@ public class AssignmentStudentTutorAndShowWorkPanel extends ContentPanel {
                 Scheduler.get().scheduleDeferred(new ScheduledCommand() {
                     @Override
                     public void execute() {
-                        _showWork.loadWhiteboard(commands);                    }
+                        if(_showWork != null) {
+                            _showWork.loadWhiteboard(commands);                    }
+                        }
                 });
             }
 
