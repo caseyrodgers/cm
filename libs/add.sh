d091b5a00b1a27d4ccdb540ec031b93817ce973a
Bob Hall <rfhall3@yahoo.com>
2013-04-20 07:38:15 +0000

    upgrade to POI 3.9

--- libs/add.sh
+++ libs/add.sh
@@ -12,13 +12,11 @@ mvn install:install-file -DgroupId=jtidy -DartifactId=jtidy -Dversion=r-938 -Dpa
 
 #mvn install:install-file -DgroupId=com.yahoo.platform.yui -DartifactId=yuicompressor -Dversion=2.3.6 -Dpackaging=jar -Dfile=yuicompressor-2.4.2.jar -DgeneratePom=true
 
-
 mvn install:install-file -DgroupId=com.bramosystems.oss.player -DartifactId=bst-player-api -Dversion=1.2.1 -Dpackaging=jar -Dfile=bst-player-api-1.2.1.jar -DgeneratePom=true
 
-mvn install:install-file -DgroupId=com.google.gwt -DartifactId=gwt-user -Dversion=2.4.0 -Dpackaging=jar -Dfile=gwt-2.4.0/gwt-user.jar -DgeneratePom=true
-mvn install:install-file -DgroupId=com.google.gwt -DartifactId=gwt-dev -Dversion=2.4.0 -Dpackaging=jar -Dfile=gwt-2.4.0/gwt-dev.jar -DgeneratePom=true
-mvn install:install-file -DgroupId=com.google.gwt -DartifactId=gwt-servlet -Dversion=2.4.0 -Dpackaging=jar -Dfile=gwt-2.4.0/gwt-servlet.jar -DgeneratePom=true
-
+mvn install:install-file -DgroupId=com.google.gwt -DartifactId=gwt-user -Dversion=2.5.0 -Dpackaging=jar -Dfile=gwt-2.5.0/gwt-user.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=com.google.gwt -DartifactId=gwt-dev -Dversion=2.5.0 -Dpackaging=jar -Dfile=gwt-2.5.0/gwt-dev.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=com.google.gwt -DartifactId=gwt-servlet -Dversion=2.5.0 -Dpackaging=jar -Dfile=gwt-2.5.0/gwt-servlet.jar -DgeneratePom=true
 
 mvn install:install-file -DgroupId=htmlparser -DartifactId=htmlparser -Dversion=1.6 -Dpackaging=jar -Dfile=htmlparser.jar -DgeneratePom=true
 mvn install:install-file -DgroupId=org.json -DartifactId=json -Dversion=1.0 -Dpackaging=jar -Dfile=json-1.0.jar -DgeneratePom=true
@@ -31,12 +29,6 @@ mvn install:install-file -DgroupId=gwt-debug-panel -DartifactId=gwt-debug-panel 
 mvn install:install-file -DgroupId=xmlrpc -DartifactId=xmlrpc -Dversion=3.1.2 -Dpackaging=jar -Dfile=xmlrpc-client-3.1.2.jar -DgeneratePom=true
 mvn install:install-file -DgroupId=paypal -DartifactId=paypal_api_combined -Dversion=1.0 -Dpackaging=jar -Dfile=paypal_api_combined.jar -DgeneratePom=true
 
-
-mvn install:install-file -DgroupId=org.apache -DartifactId=poi -Dversion=3.6 -Dpackaging=jar -Dfile=poi-3.6.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=org.apache -DartifactId=poi -Dversion=3.9 -Dpackaging=jar -Dfile=poi-3.9.jar -DgeneratePom=true
 
 mvn install:install-file -DgroupId=org.seleniumhq.selenium -DartifactId=selenium -Dversion=2.0rc3 -Dpackaging=jar -Dfile=selenium-java-2.0rc3.jar -DgeneratePom=true
-
-
-
-
-
