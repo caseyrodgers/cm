24e54dd6131d95bb9904806aaf6c8e05d1d6c219
Casey Rodgers <grodgers@gmail.com>
2013-04-24 16:00:24 +0000

    *** empty log message ***

--- src/main/java/hotmath/cm/assignment/AssignmentDao.java
+++ src/main/java/hotmath/cm/assignment/AssignmentDao.java
@@ -529,7 +529,7 @@ public class AssignmentDao extends SimpleJdbcDaoSupport {
          * get assignment problem status list for all users
          */
         final Map<Integer, String> nameMap = new HashMap<Integer, String>();
-        List<StudentProblemDto> problemStatuses = getJdbcTemplate().query(sql, new Object[] { assignKey, assignKey, assignKey },
+        List<StudentProblemDto> problemStatuses = getJdbcTemplate().query(sql, new Object[] { assignKey, assignKey, assignKey, assignKey, assignKey },
                 new RowMapper<StudentProblemDto>() {
                     @Override
                     public StudentProblemDto mapRow(ResultSet rs, int rowNum) throws SQLException {
