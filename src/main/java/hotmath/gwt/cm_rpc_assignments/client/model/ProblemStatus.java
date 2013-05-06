4823a726f8b3eba9689f225535a2a67f649d7b90
Bob Hall <rfhall3@yahoo.com>
2013-05-06 09:06:29 +0000

    use 'for' instead of 'if/else'

--- src/main/java/hotmath/gwt/cm_rpc_assignments/client/model/ProblemStatus.java
+++ src/main/java/hotmath/gwt/cm_rpc_assignments/client/model/ProblemStatus.java
@@ -2,29 +2,23 @@ package hotmath.gwt.cm_rpc_assignments.client.model;
 
 public enum ProblemStatus {
 
-	NOT_VIEWED("Not Viewed"), VIEWED("Viewed"), PENDING("Pending"), CORRECT("Correct"), INCORRECT("Incorrect"), HALF_CREDIT("Half Credit"), SUBMITTED("Submitted"), UNANSWERED("Unanswered");
-	static public ProblemStatus parseString(String object) {
-		if (ProblemStatus.VIEWED.toString().equals(object)) {
-			return ProblemStatus.VIEWED;
-		} else if (ProblemStatus.CORRECT.toString().equals(object)) {
-			return ProblemStatus.CORRECT;
-		} else if (ProblemStatus.NOT_VIEWED.toString().equals(object)) {
-			return ProblemStatus.NOT_VIEWED;
-		} else if (ProblemStatus.PENDING.toString().equals(object)) {
-			return ProblemStatus.PENDING;
-		} else if(ProblemStatus.HALF_CREDIT.toString().equals(object)) {
-		    return ProblemStatus.HALF_CREDIT;
+	CORRECT      ("Correct"),
+	HALF_CREDIT  ("Half Credit"),
+	INCORRECT    ("Incorrect"),
+	NOT_VIEWED   ("Not Viewed"),
+	PENDING      ("Pending"),
+	SUBMITTED    ("Submitted"),
+	UNANSWERED   ("Unanswered"),
+	UNKNOWN      ("Unknown"),
+	VIEWED       ("Viewed");
+
+	static public ProblemStatus parseString(String value) {
+		for (ProblemStatus p : ProblemStatus.values()) {
+			if (value.equals(p.toString())) return p;
 		}
-		else if(ProblemStatus.SUBMITTED.toString().equals(object)) {
-		    return ProblemStatus.SUBMITTED;
-		}
-		else if(ProblemStatus.UNANSWERED.toString().equals(object)) {
-		    return ProblemStatus.UNANSWERED;
-		}
-		else {
-			return ProblemStatus.INCORRECT;
-		}
+		return ProblemStatus.UNKNOWN;
 	}
+
 	private String status;
 
 	ProblemStatus(String status) {
