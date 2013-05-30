348bfbf0ce81a9698028fc9fbcecc56082a55891
Bob Hall <rfhall3@yahoo.com>
2013-05-30 07:58:47 +0000

    CM-808: PDF window w/nav (except chrome) for CCSS site viewing

---
+++ src/main/java/hotmath/gwt/cm_tools/client/ui/PdfWindowWithNav.java
@@ -0,0 +1,43 @@
+package hotmath.gwt.cm_tools.client.ui;
+
+import hotmath.gwt.cm_rpc_core.client.rpc.Action;
+import hotmath.gwt.cm_rpc_core.client.rpc.CmServiceAsync;
+import hotmath.gwt.cm_tools.client.CatchupMathTools;
+import hotmath.gwt.shared.client.CmShared;
+import hotmath.gwt.shared.client.rpc.CmWebResource;
+import hotmath.gwt.shared.client.rpc.RetryAction;
+
+import com.google.gwt.user.client.Window;
+
+public class PdfWindowWithNav {
+
+    Action<CmWebResource> action;
+    Integer aid;
+    public PdfWindowWithNav(Integer aid,  String title, Action<CmWebResource> action) {
+        this.action = action;
+        this.aid = aid;
+        createPdfRpc();
+    }
+
+    private void createPdfRpc() {
+        new RetryAction<CmWebResource>() {
+            @Override
+            public void attempt() {
+                CatchupMathTools.setBusy(true);
+                CmServiceAsync s = CmShared.getCmService();
+                setAction(action);
+                s.execute(action,this);                
+            }
+            @Override
+            public void oncapture(CmWebResource webResource) {
+                CatchupMathTools.setBusy(false);
+                
+                /** Was blanking out screen in IE 
+                 * 
+                 */
+                String features = "resizable=1,scrollbars=1,status=1,toolbar=1,height=900,width=700";
+                Window.open(webResource.getUrl(), "_blank", features);
+            }
+        }.register();
+    }
+}
