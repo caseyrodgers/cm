3cc2399a8b22082e6319696e69afeeb3137339b8
Bob Hall <rfhall3@yahoo.com>
2013-06-28 06:40:45 +0000

    add adminId

--- src/main/java/hotmath/gwt/cm_rpc/client/model/GroupDto.java
+++ src/main/java/hotmath/gwt/cm_rpc/client/model/GroupDto.java
@@ -4,16 +4,20 @@ import hotmath.gwt.cm_rpc_core.client.rpc.Response;
 
 public class GroupDto implements Response {
     
-    private int groupId;
+	private static final long serialVersionUID = 4990841749746834603L;
+
+	private int groupId;
+	private int adminId;
     private String name;
     private String info;
 
     public GroupDto() {}
     
-    public GroupDto(int groupId, String name, String info) {
+    public GroupDto(int groupId, String name, String info, int adminId) {
         this.groupId = groupId;
         this.name = name;
         this.info = info;
+        this.adminId = adminId;
     }
 
     public int getGroupId() {
@@ -24,7 +28,15 @@ public class GroupDto implements Response {
         this.groupId = groupId;
     }
 
-    public String getName() {
+    public int getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+
+	public String getName() {
         return name;
     }
 
