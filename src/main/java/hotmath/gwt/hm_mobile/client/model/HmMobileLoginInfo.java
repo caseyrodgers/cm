7d4710ab4521364c38a2d9bce4a22fc4d2c39646
Casey Rodgers <grodgers@gmail.com>
2013-07-11 15:43:20 +0000

    *** empty log message ***

--- src/main/java/hotmath/gwt/hm_mobile/client/model/HmMobileLoginInfo.java
+++ src/main/java/hotmath/gwt/hm_mobile/client/model/HmMobileLoginInfo.java
@@ -3,5 +3,44 @@ package hotmath.gwt.hm_mobile.client.model;
 import hotmath.gwt.cm_rpc_core.client.rpc.Response;
 
 public class HmMobileLoginInfo implements Response {
+    private String user;
+    private String password;
+    private String accountType;
 
+    public HmMobileLoginInfo() {}
+    
+    public HmMobileLoginInfo(String user, String password, String accountType) {
+        this.user = user;
+        this.password = password;
+        this.accountType = accountType;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    @Override
+    public String toString() {
+        return "HmMobileLoginInfo [user=" + user + ", password=" + password + ", accountType=" + accountType + "]";
+    }
 }
--- src/main/java/hotmath/gwt/hm_mobile/client/rpc/HmMobileLoginAction.java
+++ src/main/java/hotmath/gwt/hm_mobile/client/rpc/HmMobileLoginAction.java
@@ -1,40 +1,38 @@
 package hotmath.gwt.hm_mobile.client.rpc;
 
-import hotmath.gwt.cm_rpc.client.rpc.HmMobileActionBase;
 import hotmath.gwt.cm_rpc_core.client.rpc.Action;
 import hotmath.gwt.hm_mobile.client.model.HmMobileLoginInfo;
 
-public class HmMobileLoginAction extends HmMobileActionBase implements Action<HmMobileLoginInfo> {
+public class HmMobileLoginAction implements Action<HmMobileLoginInfo>{
+    
+    private String user;
+    private String password;
 
-	private static final long serialVersionUID = 6669211783141017189L;
-
-	String userName;
-	String password;
-
-	public HmMobileLoginAction(String userName, String password) {
-		this.userName = userName;
-		this.password = password;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	@Override
-    public String toString() {
-	    return "HmMobileLoginAction [userName=" + userName + ", password=" + password + "]";
+    public HmMobileLoginAction(){}
+    
+    public HmMobileLoginAction(String user, String password) {
+        this.user = user;
+        this.password = password;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "HmMobileLoginAction [user=" + user + ", password=" + password + "]";
+    }
 }
