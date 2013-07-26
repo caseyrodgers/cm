8c3db393067c951c73b37cdc7ae2c609aeb25b28
Casey Rodgers <grodgers@gmail.com>
2013-07-26 19:54:33 +0000

    *** empty log message ***

--- bin/comp_hm_mobile.sh
+++ bin/comp_hm_mobile.sh
@@ -1,5 +1,5 @@
 #cvs -q update -d -P
-mvn gwt:compile -Dgwt.module=hotmath.gwt.hm_mobile.HmMobile -o
+mvn gwt:compile -Dgwt.module=hotmath.gwt.hm_mobile.HmMobile -o -Dgwt.compiler.force=true
 cp -r src/main/webapp/hm_mobile target/cm-1.0-SNAPSHOT
 
 cd target/cm-1.0-SNAPSHOT/hm_mobile
