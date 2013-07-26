a5ff47f13a407beedf483d8df3b70ee5bd657f3e
Bob Hall <rfhall3@yahoo.com>
2013-07-26 08:57:15 +0000

    CM-819: initial commit

---
+++ src/main/java/hotmath/gwt/shared/client/rpc/action/CCSSCoverageChartDataAction.java
@@ -0,0 +1,82 @@
+package hotmath.gwt.shared.client.rpc.action;
+
+import hotmath.gwt.cm_rpc_core.client.rpc.Action;
+import hotmath.gwt.cm_rpc_core.client.rpc.CmList;
+import hotmath.gwt.shared.client.model.CCSSCoverageBar;
+
+import java.util.Date;
+
+/**
+ * 
+ * @author bob
+ *
+ */
+
+public class CCSSCoverageChartDataAction implements Action<CmList<CCSSCoverageBar>> {
+    
+	private static final long serialVersionUID = 934238051392406321L;
+
+	ReportType type;
+    Integer uid;
+    Integer adminId;
+    Date from;
+    Date to;
+    
+    public CCSSCoverageChartDataAction() {
+    }
+
+    public CCSSCoverageChartDataAction(ReportType type, int uid, Date from, Date to) {
+        this.type = type;
+        this.uid = uid;
+        this.from = from;
+        this.to = to;
+    }
+    
+    public ReportType getType() {
+        return type;
+    }
+
+    public void setType(ReportType type) {
+        this.type = type;
+    }
+
+    public Integer getUID() {
+		return uid;
+	}
+
+	public void setUID(Integer uid) {
+		this.uid = uid;
+	}
+
+	public Integer getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(Integer adminId) {
+		this.adminId = adminId;
+	}
+
+	public Date getFrom() {
+        return from;
+    }
+
+    public void setFrom(Date from) {
+        this.from = from;
+    }
+
+    public Date getTo() {
+        return to;
+    }
+
+    public void setTo(Date to) {
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "CCSSCoverageChartDataAction [type=" + type + ", from=" + from + ", to=" + to +"]";
+    }
+
+    public enum ReportType {STUDENT_WEEKLY_CHART, GROUP_WEEKLY_CHART};
+
+}
---
+++ src/main/java/hotmath/gwt/shared/server/service/command/CCSSCoverageChartDataCommand.java
@@ -0,0 +1,74 @@
+package hotmath.gwt.shared.server.service.command;
+
+import hotmath.cm.dao.CCSSReportDao;
+import hotmath.gwt.cm_rpc_core.client.rpc.Action;
+import hotmath.gwt.cm_rpc_core.client.rpc.CmArrayList;
+import hotmath.gwt.cm_rpc_core.client.rpc.CmList;
+import hotmath.gwt.cm_rpc_core.client.rpc.Response;
+import hotmath.gwt.cm_rpc_core.server.rpc.ActionHandler;
+import hotmath.gwt.shared.client.model.CCSSCoverageBar;
+import hotmath.gwt.shared.client.rpc.action.CCSSCoverageChartDataAction;
+
+import java.sql.Connection;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+/**
+ * 
+ * @author bob
+ *
+ */
+
+public class CCSSCoverageChartDataCommand implements ActionHandler<CCSSCoverageChartDataAction, CmList<CCSSCoverageBar>>{
+
+    @Override
+    public CmList<CCSSCoverageBar> execute(Connection conn, CCSSCoverageChartDataAction action) throws Exception {
+        try {
+            CmList<CCSSCoverageBar> list=null;
+
+            Date fromDate = action.getFrom();
+            if(fromDate == null) {
+                fromDate = new GregorianCalendar(2010,0,0).getTime();
+            }
+            Date toDate = action.getTo();
+            if(toDate == null) {
+                toDate = new GregorianCalendar(2050,0,0).getTime();
+            }
+
+            int adminId = action.getAdminId();
+            int groupId = action.getUID();
+
+            CCSSReportDao crDao = CCSSReportDao.getInstance();
+            switch(action.getType()) {
+                case STUDENT_WEEKLY_CHART:
+                    list = toCmList(crDao.getStudentAllByWeekStandardNames(action.getUID(), fromDate, toDate));
+                    break;
+
+                case GROUP_WEEKLY_CHART:
+                    //list = toCmList(crDao.getGroupAllByNameStandardNames(adminId, groupId, fromDate, toDate));
+                    break;
+
+                default:
+                    throw new Exception("Unknown report type: " + action);
+            }
+            return list;
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+	private CmList<CCSSCoverageBar> toCmList(
+			List<CCSSCoverageBar> listIn) {
+		CmList<CCSSCoverageBar> listOut = new CmArrayList<CCSSCoverageBar>();
+		listOut.addAll(listIn);
+		return listOut;
+	}
+
+    @Override
+    public Class<? extends Action<? extends Response>> getActionType() {
+        return  CCSSCoverageChartDataAction.class;
+    }
+}
