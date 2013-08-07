a28636e95731680f0e2b502c640938002775d4ab
Casey Rodgers <grodgers@gmail.com>
2013-08-07 21:16:09 +0000

    *** empty log message ***

--- src/main/webapp/gwt-resources/js/HmMobile.js
+++ src/main/webapp/gwt-resources/js/HmMobile.js
@@ -7,7 +7,7 @@ function setupEducationWorldAdServer() {
 }
 
 var _ZONE_MOBILE=128;
-var _ZONE_DESKTOP=130;
+var _ZONE_DESKTOP=24;
 
 function getEwAdFrame() {
 	return isMobileSmallScreen()? getEwAdFrameMobile(): getEwAdFrameDesktop();
@@ -16,7 +16,7 @@ function getEwAdFrame() {
 function getEwAdFrameMobile() {
     var randN = Math.floor(Math.random() * 99999999999) + '$1';
     var adHtml = null;
-    var adHtml = "<iframe style='margin-top: 10px' id='a3d1175f' name='a3d1175f' src='http://maxmedia.educationworld.com/www/delivery/afr.php?n=a3d1175f&amp;zoneid=" + _ZONE_MOBILE + "&amp;cb="
+    var adHtml = "<iframe id='a3d1175f' name='a3d1175f' src='http://maxmedia.educationworld.com/www/delivery/afr.php?n=a3d1175f&amp;zoneid=" + _ZONE_MOBILE + "&amp;cb="
                     + randN
                     + "' framespacing='0' frameborder='no' scrolling='no' width='100%' height='50'><a href='http://maxmedia.educationworld.com/www/delivery/ck.php?n=afa0b553&amp;cb="
                     + randN
