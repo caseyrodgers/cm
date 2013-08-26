08be30f67d2cdd67d4651defb92333e511500100
Bob Hall <rfhall3@yahoo.com>
2013-08-26 07:19:32 +0000

    CM-819: add showMessage()

--- src/main/java/hotmath/gwt/cm_tools/client/util/CmMessageBox.java
+++ src/main/java/hotmath/gwt/cm_tools/client/util/CmMessageBox.java
@@ -4,6 +4,7 @@ import hotmath.gwt.cm_rpc.client.CallbackOnComplete;
 
 import com.sencha.gxt.widget.core.client.Dialog.PredefinedButton;
 import com.sencha.gxt.widget.core.client.box.AlertMessageBox;
+import com.sencha.gxt.widget.core.client.box.MessageBox;
 import com.sencha.gxt.widget.core.client.box.ConfirmMessageBox;
 import com.sencha.gxt.widget.core.client.event.HideEvent;
 import com.sencha.gxt.widget.core.client.event.HideEvent.HideHandler;
@@ -33,6 +34,27 @@ public class CmMessageBox {
         d.show();
         
         d.toFront();
+    }
+
+    public static void showMessage(String title,String msg) {
+    	showMessage(title, msg, null);
+    }
+
+    public static void showMessage(String title,String msg, final CallbackOnComplete onComplete) {
+        MessageBox msgBox = new MessageBox(title, msg);
+        msgBox.setWidth(300);
+        HideHandler hideHandler = new HideHandler() {
+            @Override
+            public void onHide(HideEvent event) {
+                if (onComplete != null) {
+                    onComplete.isComplete();
+                }
+            }
+        };
+        msgBox.addHideHandler(hideHandler);
+        msgBox.show();
+        
+        msgBox.toFront();
     }
 
     public static void confirm(String title, String msg, final ConfirmCallback onComplete) {
