0e70117c184a53e6112ab2c19e65eceee676d587
sathesh <sathesh>
2013-08-30 02:55:40 +0000

    no message

--- src/main/webapp/gwt-resources/js/whiteboard_v2.js
+++ src/main/webapp/gwt-resources/js/whiteboard_v2.js
@@ -61,8 +61,8 @@ var Whiteboard = function (cont, isStatic) {
     var useMQ = false;
 	var selectionDragged=false;
 	//
-	var cwi=2000;
-	var cht=2620;
+	var cwi=1000;
+	var cht=1000;
 	var canvas_drawing_width=0,canvas_drawing_height=0;
     //
     var toolArr = [{
