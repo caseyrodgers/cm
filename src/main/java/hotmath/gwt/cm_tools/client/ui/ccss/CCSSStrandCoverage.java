af8d43254a2505790670546626bc27413c3de6f0
Casey Rodgers <grodgers@gmail.com>
2013-09-05 21:33:19 +0000

    *** empty log message ***

---
+++ src/main/java/hotmath/gwt/cm_tools/client/ui/ccss/CCSSStrandCoverage.java
@@ -0,0 +1,16 @@
+package hotmath.gwt.cm_tools.client.ui.ccss;
+
+
+public class CCSSStrandCoverage {
+
+    public void setCount(int count) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void setLabel(String levelName) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
