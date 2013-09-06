52f12d1e18189dad3f7c3df024e2b2b911b9004e
Bob Hall <rfhall3@yahoo.com>
2013-09-06 07:22:49 +0000

    add from/to date

--- src/main/java/hotmath/gwt/cm_tools/client/ui/DateRangeWidget.java
+++ src/main/java/hotmath/gwt/cm_tools/client/ui/DateRangeWidget.java
@@ -1,5 +1,7 @@
 package hotmath.gwt.cm_tools.client.ui;
 
+import java.util.Date;
+
 import com.google.gwt.user.client.ui.Label;
 
 import hotmath.gwt.cm_tools.client.ui.DateRangePanel;
@@ -27,6 +29,16 @@ public class DateRangeWidget extends Label {
 	public void refresh() {
 		DateRangePanel dateRangePanel = DateRangePanel.getInstance();
 		setText((dateRangePanel != null) ? "Date range: " + DateRangePanel.getInstance().formatDateRange():"");
+	}
+
+	public Date getFromDate() {
+		DateRangePanel dateRangePanel = DateRangePanel.getInstance();
+		return (dateRangePanel != null) ? dateRangePanel.getFromDate() : null;
+	}
+
+	public Date getToDate() {
+		DateRangePanel dateRangePanel = DateRangePanel.getInstance();
+		return (dateRangePanel != null) ? dateRangePanel.getToDate() : null;
 	}
 
 }
