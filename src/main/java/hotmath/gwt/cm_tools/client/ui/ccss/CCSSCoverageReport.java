30e4cdfee95c029d39c45909449b65ae162586d3
Bob Hall <rfhall3@yahoo.com>
2013-09-06 06:57:00 +0000

    CM-836: CCSS level/strand coverage

--- src/main/java/hotmath/gwt/cm_tools/client/ui/ccss/CCSSCoverageReport.java
+++ src/main/java/hotmath/gwt/cm_tools/client/ui/ccss/CCSSCoverageReport.java
@@ -4,43 +4,56 @@ public class CCSSCoverageReport {
 
 	CCSSCoverageImplBase report;
 	String decorationClass;
+	String label;
 	boolean isGroupReport;
-	
-	    public CCSSCoverageReport(CCSSCoverageImplBase report) {
-	        this.report = report;
-	    }
-	    
-	    /** The report name 
-	     * 
-	     * @return
-	     */
-	    public String getText() {
-	        return report.getText();
-	    }
-	    
-	    public String getToolTip() {
-	    	return report.getToolTip();
-	    }
-	    
-	    public CCSSCoverageImplBase getReport() {
-	        return report;
-	    }
 
-		public String getDecorationClass() {
-			return decorationClass;
-		}
+	public CCSSCoverageReport() {
+		// empty
+	}
 
-		public void setDecorationClass(String decorationClass) {
-			this.decorationClass = decorationClass;
-		}
+	public CCSSCoverageReport(CCSSCoverageImplBase report) {
+		this.report = report;
+	}
 
-		public boolean isGroupReport() {
-			return isGroupReport;
-		}
+	/** The report name 
+	 * 
+	 * @return
+	 */
+	public String getText() {
+		return report.getText();
+	}
 
-		public void setGroupReport(boolean isGroupReport) {
-			this.isGroupReport = isGroupReport;
-		}
+	public void setLabel(String label) {
+	    this.label = label;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public String getToolTip() {
+		return report.getToolTip();
+	}
+
+	public CCSSCoverageImplBase getReport() {
+		return report;
+	}
+
+	public String getDecorationClass() {
+		return decorationClass;
+	}
+
+	public void setDecorationClass(String decorationClass) {
+		this.decorationClass = decorationClass;
+	}
+
+	public boolean isGroupReport() {
+		return isGroupReport;
+	}
+
+	public void setGroupReport(boolean isGroupReport) {
+		this.isGroupReport = isGroupReport;
+	}
 
 
 }
---
+++ src/main/java/hotmath/gwt/shared/server/service/command/CCSSStrandCoverageCommand.java
@@ -0,0 +1,87 @@
+package hotmath.gwt.shared.server.service.command;
+
+import hotmath.cm.dao.CCSSReportDao;
+import hotmath.gwt.cm_rpc_core.client.rpc.Action;
+import hotmath.gwt.cm_rpc_core.client.rpc.CmArrayList;
+import hotmath.gwt.cm_rpc_core.client.rpc.CmList;
+import hotmath.gwt.cm_rpc_core.client.rpc.Response;
+import hotmath.gwt.cm_rpc_core.server.rpc.ActionHandler;
+import hotmath.gwt.cm_tools.client.ui.ccss.CCSSStrandCoverage;
+import hotmath.gwt.shared.client.rpc.action.CCSSStrandCoverageAction;
+
+import java.sql.Connection;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 
+ * @author bob
+ *
+ */
+
+public class CCSSStrandCoverageCommand implements ActionHandler<CCSSStrandCoverageAction, CmList<CCSSStrandCoverage>>{
+
+    @Override
+    public CmList<CCSSStrandCoverage> execute(Connection conn, CCSSStrandCoverageAction action) throws Exception {
+        try {
+            List<CCSSStrandCoverage> sList = null;
+
+            Date fromDate = action.getFrom();
+            if(fromDate == null) {
+                fromDate = new GregorianCalendar(2010,0,0).getTime();
+            }
+            Date toDate = action.getTo();
+            if(toDate == null) {
+                toDate = new GregorianCalendar(2050,0,0).getTime();
+            }
+
+            int adminId = action.getAdminId();
+            int uid = action.getUID();
+
+            CCSSReportDao crDao = CCSSReportDao.getInstance();
+            Map<Integer, List<CCSSStrandCoverage>> map = crDao.getCCSSStrandCoverageForStudent(Arrays.asList(uid), fromDate, toDate);
+
+            sList = crDao.getCCSSStrandCoverage();
+
+            List<CCSSStrandCoverage> list = map.get(uid);
+            
+            calculatePercentComplete(sList, list);
+
+            return toCmList(sList);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+    
+	private void calculatePercentComplete(List<CCSSStrandCoverage> sList,
+			List<CCSSStrandCoverage> list) {
+		for (CCSSStrandCoverage stdCov : sList) {
+			String name = stdCov.getLabel();
+			float stdCount = stdCov.getCount();
+			for (CCSSStrandCoverage cov : list) {
+				if (name.equals(cov.getLabel())) {
+					// calculate % complete
+					int percentComplete = Math.round(((float)cov.getCount()*100f)/stdCount);
+					stdCov.setPercentComplete(String.format("%d%s", percentComplete, "%"));
+					break;
+				}
+			}
+		}
+	}
+
+	private CmList<CCSSStrandCoverage> toCmList(List<CCSSStrandCoverage> list) {
+        CmList<CCSSStrandCoverage> listOut = new CmArrayList<CCSSStrandCoverage>();
+        listOut.addAll(list);
+        return listOut;
+    }
+
+    @Override
+    public Class<? extends Action<? extends Response>> getActionType() {
+        return  CCSSStrandCoverageAction.class;
+    }
+}
