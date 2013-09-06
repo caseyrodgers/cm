302b5323e205810a5a401ee2fdc186f1d42be6a5
Bob Hall <rfhall3@yahoo.com>
2013-09-06 06:15:04 +0000

    CM-836: CCSS level/strand coverage counts

--- src/main/java/hotmath/gwt/cm_tools/client/ui/ccss/CCSSStrandCoverage.java
+++ src/main/java/hotmath/gwt/cm_tools/client/ui/ccss/CCSSStrandCoverage.java
@@ -1,16 +1,30 @@
 package hotmath.gwt.cm_tools.client.ui.ccss;
 
+public class CCSSStrandCoverage extends CCSSCoverageReport {
 
-public class CCSSStrandCoverage {
+	String percentComplete;
+	int count;
 
-    public void setCount(int count) {
-        // TODO Auto-generated method stub
-        
-    }
+	public CCSSStrandCoverage() {
+	}
 
-    public void setLabel(String levelName) {
-        // TODO Auto-generated method stub
-        
-    }
+	public CCSSStrandCoverage(CCSSCoverageImplBase report) {
+		super(report);
+	}
 
+	public String getPercentComplete() {
+		return percentComplete;
+	}
+
+	public void setPercentComplete(String percentComplete) {
+		this.percentComplete = percentComplete;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
 }
