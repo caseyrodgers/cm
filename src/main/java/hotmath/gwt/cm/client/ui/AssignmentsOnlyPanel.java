e9d5ed07b3553512064dbec5cc6b5544f020c852
Casey Rodgers <grodgers@gmail.com>
2013-09-11 01:18:19 +0000

    *** empty log message ***

--- src/main/java/hotmath/gwt/cm/client/ui/AssignmentsOnlyPanel.java
+++ src/main/java/hotmath/gwt/cm/client/ui/AssignmentsOnlyPanel.java
@@ -4,9 +4,13 @@ import hotmath.gwt.cm_rpc.client.UserInfo;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.widget.core.client.ContentPanel;
+import com.sencha.gxt.widget.core.client.button.TextButton;
 import com.sencha.gxt.widget.core.client.container.CenterLayoutContainer;
+import com.sencha.gxt.widget.core.client.event.SelectEvent;
+import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
 
 public class AssignmentsOnlyPanel extends CenterLayoutContainer {
     
@@ -16,10 +20,18 @@ public class AssignmentsOnlyPanel extends CenterLayoutContainer {
     public AssignmentsOnlyPanel() {
         ContentPanel lc = new ContentPanel();
         lc.setHeadingText("Catchup Math Assignments");
+        addStyleName("cm-welcome-panel");
+        lc.addStyleName("welcome-wrapper");
         lc.setWidth(400);
         lc.setHeight(200);
         addStyleName(UserInfo.getInstance().getBackgroundStyle());
-        lc.add(uiBinder.createAndBindUi(this));
+        lc.add(new HTML("<div style='font-size: 1.2em;margin: 20px'>Click the Assignments button at the top left or below to check your assignments.</p>"));
+        lc.addButton(new TextButton("Show Assignments", new SelectHandler() {
+            @Override
+            public void onSelect(SelectEvent event) {
+                StudentAssignmentSelectorDialog.showSharedDialog();
+            }
+        }));
         setWidget(lc);
     }
 }
