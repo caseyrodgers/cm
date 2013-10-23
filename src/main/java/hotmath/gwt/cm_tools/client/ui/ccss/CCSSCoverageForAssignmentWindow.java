e0f3172cf511ec2a5baf7e81b5a7b537f52251fe
Bob Hall <rfhall3@yahoo.com>
2013-10-23 07:50:41 +0000

    CM-857: display CCSS Coverage for an Assignment

--- src/main/java/hotmath/gwt/cm_tools/client/ui/ccss/CCSSCoverageForAssignmentWindow.java
+++ src/main/java/hotmath/gwt/cm_tools/client/ui/ccss/CCSSCoverageForAssignmentWindow.java
@@ -1,29 +1,21 @@
 package hotmath.gwt.cm_tools.client.ui.ccss;
 
-import hotmath.gwt.cm_tools.client.model.StudentModelBase;
 import hotmath.gwt.cm_rpc.client.CallbackOnComplete;
-import hotmath.gwt.cm_rpc.client.model.GroupDto;
 import hotmath.gwt.cm_rpc_assignments.client.model.assignment.Assignment;
-import hotmath.gwt.cm_tools.client.model.StudentModelI;
-import hotmath.gwt.cm_tools.client.ui.DateRangePanel;
-import hotmath.gwt.cm_tools.client.ui.DateRangeWidget;
 import hotmath.gwt.cm_tools.client.ui.GWindow;
 import hotmath.gwt.cm_tools.client.ui.PdfWindowWithNav;
-import hotmath.gwt.shared.client.eventbus.CmEvent;
-import hotmath.gwt.shared.client.eventbus.CmEventListenerImplDefault;
-import hotmath.gwt.shared.client.eventbus.EventBus;
-import hotmath.gwt.shared.client.eventbus.EventType;
 import hotmath.gwt.shared.client.rpc.action.GeneratePdfAction;
 import hotmath.gwt.shared.client.rpc.action.GeneratePdfAction.PdfType;
 
 import java.util.Arrays;
-import java.util.Date;
 
 import com.google.gwt.user.client.ui.HTML;
+import com.sencha.gxt.core.client.dom.ScrollSupport.ScrollMode;
+import com.sencha.gxt.core.client.util.Margins;
 import com.sencha.gxt.widget.core.client.button.TextButton;
 import com.sencha.gxt.widget.core.client.container.BorderLayoutContainer;
 import com.sencha.gxt.widget.core.client.container.BorderLayoutContainer.BorderLayoutData;
-import com.sencha.gxt.widget.core.client.container.CenterLayoutContainer;
+import com.sencha.gxt.widget.core.client.container.FlowLayoutContainer;
 import com.sencha.gxt.widget.core.client.event.SelectEvent;
 import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
 
@@ -38,9 +30,9 @@ public class CCSSCoverageForAssignmentWindow extends GWindow {
 
     BorderLayoutContainer _container;
 
-    private static final String TITLE = "CCSS Coverage for ";
+    private static final String TITLE = "Assignment CCSS Coverage";
 
-    BorderLayoutData _centerData;
+    BorderLayoutData _centerData = new BorderLayoutData();
     CCSSCoverageListPanel _CCSSCoverageListPanel;
     Assignment _assignment;
     int _assignKey;
@@ -54,32 +46,25 @@ public class CCSSCoverageForAssignmentWindow extends GWindow {
         _assignKey = assignment.getAssignKey();
         _adminId = assignment.getAdminId();
 
-        setHeadingText(TITLE + assignment.getAssignmentName());
+        setHeadingText(TITLE);
         setWidth(310);
         setHeight(500);
 
         _container = new BorderLayoutContainer();
         _container.setBorders(true);
 
-        _centerData = new BorderLayoutData();
         _centerData.setSize(300);
 
         final CCSSCoverageImplAssignment impl = new CCSSCoverageImplAssignment(_assignKey, new CallbackOnComplete() {
 			@Override
 			public void isComplete() {
-		        _container.forceLayout();
+		        setWidget(_container);
+		        forceLayout();
 			}
         });
+        displaySummary(assignment.getAssignmentName());
         _container.setCenterWidget(impl.getWidget(), _centerData);
 
-/*
-        getHeader().addTool(new TextButton("Refresh", new SelectHandler() {
-            @Override
-            public void onSelect(SelectEvent event) {
-                reloadData();
-            }
-        }));
-*/
         getHeader().addTool(new TextButton("Print Report", new SelectHandler() {
             @Override
             public void onSelect(SelectEvent ce) {
@@ -89,22 +74,23 @@ public class CCSSCoverageForAssignmentWindow extends GWindow {
 
         super.addCloseButton();
 
-        /**
-         * turn on after data retrieved
-         * 
-         */
-        setWidget(_container);
-
         setVisible(true);
-        
-        forceLayout();
     }
 
-    private void reloadData() {
-        _container.getCenterWidget().removeFromParent();
+    private void displaySummary(String summary) {
 
-        _container.forceLayout();
-    }
+    	FlowLayoutContainer flc = new FlowLayoutContainer();
+    	flc.setScrollMode(ScrollMode.AUTOY);
+    	flc.setBorders(true);
+
+    	BorderLayoutData bld = new BorderLayoutData(50);
+    	bld.setMargins(new Margins(5));
+
+        String html = "<p style='padding: 5px;'>" + summary + "</p>";
+        flc.add(new HTML(html));
+
+        _container.setNorthWidget(flc, bld);
+	}
 
     private void printAssignmentCCSSCoverageReport() {
     	new PdfWindowWithNav(_adminId, "Catchup Math CCSS Report for: " + _assignment.getAssignmentName(),
