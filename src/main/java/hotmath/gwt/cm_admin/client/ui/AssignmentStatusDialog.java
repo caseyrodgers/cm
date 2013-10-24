873b34da7d5017c63b71bd3343ff9b63eeac0b9d
Bob Hall <rfhall3@yahoo.com>
2013-10-24 06:27:50 +0000

    move Assignment summary to dialog body

--- src/main/java/hotmath/gwt/cm_admin/client/ui/AssignmentStatusDialog.java
+++ src/main/java/hotmath/gwt/cm_admin/client/ui/AssignmentStatusDialog.java
@@ -7,24 +7,41 @@ import hotmath.gwt.cm_tools.client.ui.GWindow;
 import hotmath.gwt.shared.client.CmShared;
 import hotmath.gwt.shared.client.model.UserInfoBase;
 
+import com.google.gwt.user.client.ui.HTML;
+import com.sencha.gxt.core.client.dom.ScrollSupport.ScrollMode;
+import com.sencha.gxt.core.client.util.Margins;
 import com.sencha.gxt.widget.core.client.button.TextButton;
+import com.sencha.gxt.widget.core.client.container.BorderLayoutContainer;
+import com.sencha.gxt.widget.core.client.container.FlowLayoutContainer;
+import com.sencha.gxt.widget.core.client.container.BorderLayoutContainer.BorderLayoutData;
 import com.sencha.gxt.widget.core.client.event.SelectEvent;
 import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
 
 public class AssignmentStatusDialog extends GWindow {
 
     GradeBookPanel _gradingPanel;
+    BorderLayoutContainer _container;
+
     public AssignmentStatusDialog(final Assignment asgn) {
         
         super(false);
-        setPixelSize(500,  400);
+        setPixelSize(500, 450);
         setMaximizable(true);
-        setHeadingText("Student Status: Due: " + asgn.getDueDate() + " " + asgn.getComments());
-        
+        setHeadingText("Student Status");
+
+        _container = new BorderLayoutContainer();
+        _container.setBorders(true);
+
+        BorderLayoutData centerData = new BorderLayoutData();
+        centerData.setSize(400);
+
+        displaySummary("Due: " + asgn.getDueDate() + " " + asgn.getComments());
+
         _gradingPanel = new GradeBookPanel();
-        setWidget(_gradingPanel);
+        _container.setCenterWidget(_gradingPanel, centerData);
         _gradingPanel.showGradeBookFor(asgn);
-        
+
+        setWidget(_container);
 
         TextButton addRemove = new TextButton("Add/Remove Students", new SelectHandler() {
             @Override
@@ -55,7 +72,20 @@ public class AssignmentStatusDialog extends GWindow {
 
         setVisible(true);
     }
-    
+
+    private void displaySummary(String summary) {
+    	FlowLayoutContainer flc = new FlowLayoutContainer();
+    	flc.setScrollMode(ScrollMode.AUTO);
+    	flc.setBorders(false);
+
+    	BorderLayoutData bld = new BorderLayoutData(50);
+    	bld.setMargins(new Margins(5));
+
+        flc.add(new HTML("<p style='padding: 5px;'>" + summary + "</p>"));
+
+        _container.setNorthWidget(flc, bld);
+	}
+
     public static void startTest() {
         //new AssignmentManagerDialog2(566,2);
         //return;
