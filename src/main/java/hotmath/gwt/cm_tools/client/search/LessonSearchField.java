070a13742c63a6526ae8861b77ac9fde7278dc50
Casey Rodgers <grodgers@gmail.com>
2013-10-29 18:37:50 +0000

    no message

---
+++ src/main/java/hotmath/gwt/cm_tools/client/search/LessonSearchField.java
@@ -0,0 +1,42 @@
+package hotmath.gwt.cm_tools.client.search;
+
+import hotmath.gwt.cm_rpc.client.model.Topic;
+import hotmath.gwt.cm_rpc_core.client.rpc.CmList;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Widget;
+
+public class LessonSearchField extends Composite {
+    private TextBox _lessonText = new TextBox();
+    protected CmList<Topic> _allLessons;
+
+    interface MyUiBinder extends UiBinder<Widget, LessonSearchField> {
+    }
+
+    private static MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
+
+    public LessonSearchField() {
+        initWidget(uiBinder.createAndBindUi(this));
+    }
+    
+    
+    @UiHandler("doSearch")
+    public void doSearch(ClickEvent ce) {
+        LessonSearchPopup popup = new LessonSearchPopup();
+        //asWidget().add(popup);
+        popup.show();
+    }
+
+    @UiField
+    TextBox searchField;
+    
+    @UiField
+    Button doSearch;
+}
---
+++ src/main/java/hotmath/gwt/cm_tools/client/search/LessonSearchPopup.java
@@ -0,0 +1,27 @@
+package hotmath.gwt.cm_tools.client.search;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.Widget;
+
+public class LessonSearchPopup extends Composite {
+
+    private static final Binder binder = GWT.create(Binder.class);
+
+    interface Binder extends UiBinder<Widget, LessonSearchPopup> {
+    }
+
+    public LessonSearchPopup() {
+        initWidget(binder.createAndBindUi(this));
+    }
+    
+    @UiField 
+    DialogBox theDialog;
+
+    public void show() {
+        theDialog.show();
+    }
+}
