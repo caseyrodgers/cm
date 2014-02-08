3b678bc84086046dfb816432dc37f0ac83bc7d52
Casey Rodgers <grodgers@gmail.com>
2014-02-08 23:28:02 +0000

    *** empty log message ***

--- src/main/java/hotmath/gwt/cm_admin/client/ui/assignment/AssignmentTreeAllLessonsListingPanel.java
+++ src/main/java/hotmath/gwt/cm_admin/client/ui/assignment/AssignmentTreeAllLessonsListingPanel.java
@@ -193,30 +193,30 @@ public class AssignmentTreeAllLessonsListingPanel extends ContentPanel {
             public String getPath() {
                 return "name";
             }
-        });
+        }, new CheckableMinLevelGxtTreeAppearance(0));
         _tree.setLoader(loader);
         _tree.setWidth(300);
         _tree.setCheckable(true);
         _tree.setCheckStyle(CheckCascade.CHILDREN);        
 
-//        SimpleSafeHtmlCell<String> cell = new SimpleSafeHtmlCell<String>(SimpleSafeHtmlRenderer.getInstance(),
-//        		BrowserEvents.CLICK) {
-//            @Override
-//            public void onBrowserEvent(Context context, Element parent, String value, NativeEvent event,
-//                    ValueUpdater<String> valueUpdater) {
-//                super.onBrowserEvent(context, parent, value, event, valueUpdater);
-//                if (BrowserEvents.CLICK.equals(event.getType())) {
-//                    BaseDto base = _tree.getSelectionModel().getSelectedItem();
-//                    if (base instanceof ProblemDto) {
-//                        ProblemDto p = (ProblemDto)base;
-//                        Log.debug("View Question", "Viewing " + p.getLabel());
-//                        
-//                        QuestionViewerPanel.getInstance().viewQuestion(p, false);
-//                    }
-//                }
-//            }
-//        };
-//        _tree.setCell(cell);
+        SimpleSafeHtmlCell<String> cell = new SimpleSafeHtmlCell<String>(SimpleSafeHtmlRenderer.getInstance(),
+        		BrowserEvents.CLICK) {
+            @Override
+            public void onBrowserEvent(Context context, Element parent, String value, NativeEvent event,
+                    ValueUpdater<String> valueUpdater) {
+                super.onBrowserEvent(context, parent, value, event, valueUpdater);
+                if (BrowserEvents.CLICK.equals(event.getType())) {
+                    BaseDto base = _tree.getSelectionModel().getSelectedItem();
+                    if (base instanceof ProblemDto) {
+                        ProblemDto p = (ProblemDto)base;
+                        Log.debug("View Question", "Viewing " + p.getLabel());
+                        
+                        QuestionViewerPanel.getInstance().viewQuestion(p, false);
+                    }
+                }
+            }
+        };
+        _tree.setCell(cell);
 
         _tree.addCheckChangedHandler(new CheckChangedHandler<BaseDto>() {
             @Override
@@ -288,16 +288,7 @@ public class AssignmentTreeAllLessonsListingPanel extends ContentPanel {
         }
         
         
-        FolderDto all = makeFolder("All Lessons");
-        all.setChildren(children);
-        
-        
-        
-        
-        all.setChildren(children);
-        List<BaseDto> dl = new ArrayList<BaseDto>();
-        dl.add(all);
-        _root.setChildren(dl);
+        _root.setChildren(children);
         
         FolderDto root = _root;
         for (BaseDto base : root.getChildren()) {
