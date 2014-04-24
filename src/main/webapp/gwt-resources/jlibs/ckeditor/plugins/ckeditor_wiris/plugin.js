b7a0a6fe1c731134b125a0f4b78270480e9d94d7
Casey Rodgers <grodgers@gmail.com>
2014-04-24 04:26:21 +0000

    *** empty log message ***

--- src/main/webapp/gwt-resources/jlibs/ckeditor/plugins/ckeditor_wiris/plugin.js
+++ src/main/webapp/gwt-resources/jlibs/ckeditor/plugins/ckeditor_wiris/plugin.js
@@ -6,7 +6,7 @@ document.getElementsByTagName('head')[0].appendChild(script);
 
 // Configuration
 var _wrs_conf_editorEnabled = true;		// Specifies if fomula editor is enabled.
-var _wrs_conf_CASEnabled = true;		// Specifies if WIRIS cas is enabled.
+var _wrs_conf_CASEnabled = false;		// Specifies if WIRIS cas is enabled.
 
 var _wrs_conf_imageMathmlAttribute = 'data-mathml';	// Specifies the image tag where we should save the formula editor mathml code.
 var _wrs_conf_CASMathmlAttribute = 'alt';	// Specifies the image tag where we should save the WIRIS cas mathml code.
