3a8532d1e7aed429354f338edf56ebc10f25038d
Casey Rodgers <grodgers@gmail.com>
2014-06-05 18:16:37 +0000

    *** empty log message ***

--- src/main/webapp/gwt-resources/jlibs/ckeditor-4.4.1/config.js
+++ src/main/webapp/gwt-resources/jlibs/ckeditor-4.4.1/config.js
@@ -10,10 +10,11 @@ CKEDITOR.editorConfig = function( config ) {
 	
 	
 	// Add WIRIS to the plugin list
-    config.extraPlugins += (config.extraPlugins.length == 0 ? '' : ',') + 'ckeditor_wiris,font,specialchar';
+        config.extraPlugins += (config.extraPlugins.length == 0 ? '' : ',') + 'ckeditor_wiris,font,specialchar';
  	config.fontSize_sizes = '12/12pt;16/16pt;24/24pt;48/48pt;';
 
- 	
+
+
 	config.toolbar = 'CM';
 	config.toolbar_CM = 
 	    [
@@ -28,5 +29,137 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Make dialogs simpler.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
+
+
+
+        config.specialChars = [
+["&radic;","Square Root"],
+["&fnof;","fnof"],
+["&Alpha;","Alpha"],
+["&Beta;","Beta"],
+["&Gamma;","Gamma"],
+["&Delta;","Delta"],
+["&Epsilon;","Epsilon"],
+["&Zeta;","Zeta"],
+["&Eta;","Eta"],
+["&Theta;","Theta"],
+["&Iota;","Iota"],
+["&Kappa;","Kappa"],
+["&Lambda;","Lambda"],
+["&Mu;","Mu"],
+["&Nu;","Nu"],
+["&Xi;","Xi"],
+["&Omicron;","Omicron"],
+["&Pi;","Pi"],
+["&Rho;","Rho"],
+["&Sigma;","Sigma"],
+["&Tau;","Tau"],
+["&Upsilon;","Upsilon"],
+["&Phi;","Phi"],
+["&Chi;","Chi"],
+["&Psi;","Psi"],
+["&Omega;","Omega"],
+["&alpha;","alpha"],
+["&beta;","beta"],
+["&gamma;","gamma"],
+["&delta;","delta"],
+["&epsilon;","epsilon"],
+["&zeta;","zeta"],
+["&eta;","eta"],
+["&theta;","theta"],
+["&iota;","iota"],
+["&kappa;","kappa"],
+["&lambda;","lambda"],
+["&mu;","mu"],
+["&nu;","nu"],
+["&xi;","xi"],
+["&omicron;","omicron"],
+["&pi;","pi"],
+["&rho;","rho"],
+["&sigmaf;","sigmaf"],
+["&sigma;","sigma"],
+["&tau;","tau"],
+["&upsilon;","upsilon"],
+["&phi;","phi"],
+["&chi;","chi"],
+["&psi;","psi"],
+["&omega;","omega"],
+["&thetasym;","thetasym"],
+["&upsih;","upsih"],
+["&piv;","piv"],
+["&bull;","bull"],
+["&hellip;","hellip"],
+["&prime;","prime"],
+["&Prime;","Prime"],
+["&oline;","oline"],
+["&frasl;","frasl"],
+["&weierp;","weierp"],
+["&image;","image"],
+["&real;","real"],
+["&trade;","trade"],
+["&alefsym;","alefsym"],
+["&larr;","larr"],
+["&rarr;","rarr"],
+["&darr;","darr"],
+["&harr;","harr"],
+["&crarr;","crarr"],
+["&lArr;","lArr"],
+["&uArr;","uArr"],
+["&rArr;","rArr"],
+["&dArr;","dArr"],
+["&hArr;","hArr"],
+["&forall;","forall"],
+["&part;","part"],
+["&exist;","exist"],
+["&empty;","empty"],
+["&nabla;","nabla"],
+["&isin;","isin"],
+["&notin;","notin"],
+["&ni;","ni"],
+["&prod;","prod"],
+["&sum;","sum"],
+["&minus;","minus"],
+["&lowast;","lowast"],
+["&prop;","prop"],
+["&infin;","infin"],
+["&ang;","ang"],
+["&and;","and"],
+["&or;","or"],
+["&cap;","cap"],
+["&cup;","cup"],
+["&int;","int"],
+["&there4;","there4"],
+["&sim;","sim"],
+["&cong;","cong"],
+["&asymp;","asymp"],
+["&ne;","ne"],
+["&equiv;","equiv"],
+["&le;","le"],
+["&ge;","ge"],
+["&sub;","sub"],
+["&sup;","sup"],
+["&nsub;","nsub"],
+["&sube;","sube"],
+["&supe;","supe"],
+["&oplus;","oplus"],
+["&otimes;","otimes"],
+["&perp;","perp"],
+["&sdot;","sdot"],
+["&lceil;","lceil"],
+["&rceil;","rceil"],
+["&lfloor;","lfloor"],
+["&rfloor;","rfloor"],
+["&lang;","lang"],
+["&rang;","rang"],
+["&loz;","loz"],
+["&spades;","spades"],
+["&clubs;","clubs"],
+["&hearts;","hearts"],
+["&diams;","diams"]
+];
+
+
+
+
 };
 
