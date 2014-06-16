ab33a0813662f900d90faa206b6bfaec0183a349
Bob Hall <rfhall3@yahoo.com>
2014-06-16 08:00:31 +0000

    add HTML "template" and prob ids

--- src/main/java/hotmath/gwt/shared/server/service/command/helper/GetAssignmentHTMLHelper.java
+++ src/main/java/hotmath/gwt/shared/server/service/command/helper/GetAssignmentHTMLHelper.java
@@ -21,9 +21,20 @@ public class GetAssignmentHTMLHelper {
 
 	private static final Logger LOGGER = Logger.getLogger(GetAssignmentHTMLHelper.class);
 
-	private static final String PROB_STMT_DIV_OPEN_FMT = "<div class='prob-stmt' style='margin-bottom:%dem;'>";
+	private static final String PROB_STMT_DIV_OPEN_FMT = "<div id='prob-%d' class='prob-stmt' style='margin-bottom:%dem;'>";
 	private static final String DIV_CLOSE = "</div>";
 	private static final int DEFAULT_WORK_LINES = 15;
+
+	private static final String ASSIGNMENT_TEMPLATE_1 =
+		"<html><head>" +
+        "<meta http-equiv='content-type' content='text/html; charset=iso-8859-1'>" +
+        "<title>Assignment</title>" +
+        "<link rel='stylesheet' type='text/css' href='/gwt-resources/css/CatchupMath.css></head>" +
+        "<body><div id='main-content' class='assignment'>";
+	private static final String ASSIGNMENT_TEMPLATE_2 = 
+        "</div>"+
+        "<script type='text/javascript' language='javascript' src='/gwt-resources/js/CatchupMath_combined.js'></script>" +
+        "</body></html>";
 
 	private AssignmentDao assignmentDao;
 	private CmSolutionManagerDao solutionDao;
@@ -38,22 +49,24 @@ public class GetAssignmentHTMLHelper {
 
 		List<ProblemDto> probs = assignmentDao.getProblemsForAssignment(assignKey);
 
-		String divOpen = String.format(PROB_STMT_DIV_OPEN_FMT, (numWorkLines > 0) ? numWorkLines : DEFAULT_WORK_LINES);
-
 		StringBuilder htmlSb = new StringBuilder();
-			for(ProblemDto prob : probs) {
-				htmlSb.append(divOpen);
+		htmlSb.append(ASSIGNMENT_TEMPLATE_1);
+		int idx = 1;
+		for(ProblemDto prob : probs) {
 
-				TutorSolution solution = solutionDao.getTutorSolution(conn, prob.getPid());
+			String divOpen = String.format(PROB_STMT_DIV_OPEN_FMT, idx++, (numWorkLines > 0) ? numWorkLines : DEFAULT_WORK_LINES);
 
-				String html = solution.getProblem().getStatement();
-				
-				html = html.replaceAll("id=", "class=");
-				html = html.replaceAll(" class=MsoNormal", "");
+			htmlSb.append(divOpen);
 
-				htmlSb.append(html).append(DIV_CLOSE);
+			TutorSolution solution = solutionDao.getTutorSolution(conn, prob.getPid());
+			String html = solution.getProblem().getStatement();
+			html = html.replaceAll("id=", "class=");
+			html = html.replaceAll(" class=MsoNormal", "");
 
-			}
+			htmlSb.append(html).append(DIV_CLOSE);
+
+		}
+		htmlSb.append(ASSIGNMENT_TEMPLATE_2);
 		
 		return htmlSb.toString();
 	}
