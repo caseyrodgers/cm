58a9663a533a52c71e708757b3457f7a7158a1c8
Bob Hall <rfhall3@yahoo.com>
2014-07-01 08:48:39 +0000

    remove unused import

--- src/main/java/hotmath/gwt/shared/server/service/command/GetSolutionCommand.java
+++ src/main/java/hotmath/gwt/shared/server/service/command/GetSolutionCommand.java
@@ -15,7 +15,6 @@ import hotmath.solution.writer.SolutionHTMLCreator;
 import hotmath.solution.writer.TutorProperties;
 import hotmath.testset.ha.HaTestRunDao;
 import hotmath.testset.ha.SolutionDao;
-import hotmath.testset.ha.WhiteboardDao;
 import hotmath.util.HMConnectionPool;
 import hotmath.util.sql.SqlUtilities;
 
