4180491571d58b832a652e0504d48002780f3aa5
Bob Hall <rfhall3@yahoo.com>
2014-07-01 09:17:50 +0000

    add global solution context

--- src/main/java/hotmath/gwt/shared/server/service/command/helper/GetAssignmentHTMLHelper.java
+++ src/main/java/hotmath/gwt/shared/server/service/command/helper/GetAssignmentHTMLHelper.java
@@ -1,9 +1,18 @@
 package hotmath.gwt.shared.server.service.command.helper;
 
+import hotmath.HotMathLogger;
+import hotmath.HotMathUtilities;
+import hotmath.ProblemID;
 import hotmath.cm.assignment.AssignmentDao;
+import hotmath.gwt.cm_rpc.client.model.SolutionContext;
 import hotmath.gwt.cm_rpc_assignments.client.model.assignment.ProblemDto;
+import hotmath.gwt.shared.server.service.command.SolutionHTMLCreatorImplFileSystem;
 import hotmath.gwt.solution_editor.server.CmSolutionManagerDao;
 import hotmath.gwt.solution_editor.server.solution.TutorSolution;
+import hotmath.solution.SolutionParts;
+import hotmath.solution.writer.SolutionHTMLCreator;
+import hotmath.solution.writer.TutorProperties;
+import hotmath.testset.ha.SolutionDao;
 
 import org.apache.log4j.Logger;
 import org.htmlparser.Node;
@@ -25,19 +34,33 @@ public class GetAssignmentHTMLHelper {
 
 	private static final Logger LOGGER = Logger.getLogger(GetAssignmentHTMLHelper.class);
 
-	private static final String PROB_STMT_DIV_OPEN_FMT = "<div id='prob-%d' class='prob-stmt' style='margin-bottom:%dem;'>";
+	private static final String PROB_STMT_DIV_OPEN_FMT =
+			"<div id='prob-%d' class='prob-stmt' style='margin-bottom:%dem;' context='%s'>";
 	private static final String DIV_CLOSE = "</div>\n";
 	private static final int DEFAULT_WORK_LINES = 15;
 
 	private AssignmentDao assignmentDao;
-	private CmSolutionManagerDao solutionDao;
+	private SolutionDao  solutionDao;
+	//private CmSolutionManagerDao solnMgrDao;
 	private Parser htmlParser = new Parser();
 	private Node root;
 	private String baseDirectory = "";
 
+    public static SolutionHTMLCreator __creator;
+    static TutorProperties __tutorProps = new TutorProperties();
+    static {
+        try {
+            __creator = new SolutionHTMLCreatorImplFileSystem(__tutorProps.getTemplate(), __tutorProps.getTutor());
+        } catch (Exception hme) {
+            LOGGER.error("Error creating solution creator: ", hme);
+        }
+    }
+
+
 	public GetAssignmentHTMLHelper() throws Exception {
 		assignmentDao = AssignmentDao.getInstance();
-		solutionDao = new CmSolutionManagerDao();
+		//solnMgrDao = new CmSolutionManagerDao();
+		solutionDao = SolutionDao.getInstance();
 	}
 
 	public String getBaseDirectory() {
@@ -58,13 +81,27 @@ public class GetAssignmentHTMLHelper {
 		for(ProblemDto prob : probs) {
 
 			StringBuilder sb = new StringBuilder();
-			String divOpen = String.format(PROB_STMT_DIV_OPEN_FMT, idx++, (numWorkLines > 0) ? numWorkLines : DEFAULT_WORK_LINES);
-			sb.append(divOpen);
 
-			TutorSolution solution = solutionDao.getTutorSolution(conn, prob.getPid());
+            ProblemID ppid = new ProblemID(prob.getPid());
+
+            SolutionParts sp = __creator.getSolutionHTML(null,null, prob.getPid());
+            String solutionHtml= sp.getMainHtml();
+
+            String path = ppid.getSolutionPath_DirOnly("solutions");
+            solutionHtml = HotMathUtilities.makeAbsolutePaths(path, solutionHtml);
+
+            SolutionContext solutionContext = solutionDao.getGlobalSolutionContext(prob.getPid());
+            String pidFull = (solutionContext != null) ? solutionContext.getPid(): "";
+            String pidParts[] = pidFull.split("\\$");
+            String contextGuid = (pidParts.length > 1) ? pidParts[1] : "";
+			String divOpen = String.format(PROB_STMT_DIV_OPEN_FMT, idx++, (numWorkLines > 0) ? numWorkLines : DEFAULT_WORK_LINES, contextGuid);
+			sb.append(divOpen);
+/*
+			TutorSolution solution = solnMgrDao.getTutorSolution(conn, prob.getPid());
 			String html = solution.getProblem().getStatement();
 			sb.append(html).append(DIV_CLOSE);
-
+ */
+			sb.append(solutionHtml).append(DIV_CLOSE);
 			htmlSb.append(cleanupHtml(sb.toString()));
 			htmlSb.append("\n");
 		}
