7cf6f62b1619eebe7c958e128b9e2f07915fa8f3
Casey Rodgers <grodgers@gmail.com>
2014-08-16 07:20:32 +0000

    restoring MIA file

---
+++ src/main/java/hotmath/gwt/cm_rpc/client/rpc/GetChaptersForProgramSubjectAction.java
@@ -0,0 +1,34 @@
+package hotmath.gwt.cm_rpc.client.rpc.action;
+
+import hotmath.gwt.cm_rpc_core.client.rpc.Action;
+import hotmath.gwt.cm_rpc_core.client.rpc.CmList;
+import hotmath.gwt.cm_tools.client.model.ChapterModel;
+
+public class GetChaptersForProgramSubjectAction implements Action<CmList<ChapterModel>>{
+	
+	String progId;
+	String subjId;
+	
+	public GetChaptersForProgramSubjectAction() {}
+	
+	public GetChaptersForProgramSubjectAction(String progId, String subjId) {
+		this.progId = progId;
+		this.subjId = subjId;
+	}
+
+	public String getProgId() {
+		return progId;
+	}
+
+	public void setProgId(String progId) {
+		this.progId = progId;
+	}
+
+	public String getSubjId() {
+		return subjId;
+	}
+
+	public void setSubjId(String subjId) {
+		this.subjId = subjId;
+	}
+}
