cb45cf76f8147ab8e064b902248dc51a2e0ae55c
Casey Rodgers <grodgers@gmail.com>
2014-08-19 21:45:22 +0000

    *** empty log message ***

--- bin/comp_assignment_print.sh
+++ bin/comp_assignment_print.sh
@@ -1,3 +1,3 @@
 #cvs update -d -P src/main/java
 mvn gwt:compile -Dgwt.module=hotmath.gwt.assignment_print.AssignmentPrint -Dgwt.compiler.force=true -o
-cp -r src/main/webapp/cm_student target/cm-1.0-SNAPSHOT
+cp -r src/main/webapp/assignment_print target/cm-1.0-SNAPSHOT
