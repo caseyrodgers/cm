25d6f1bbb650cf60cdbad3a7a70e9753e7a1ef71
Casey Rodgers <grodgers@gmail.com>
2014-08-22 21:11:29 +0000

    *** empty log message ***

--- bin/comp_all.sh
+++ bin/comp_all.sh
@@ -3,6 +3,7 @@ cvs -q update -d -P
 bin/comp_server.sh
 bin/comp_cm_student.sh
 bin/comp_cm_admin.sh
+bin/comp_assignment_print.sh
 bin/comp_solution_editor.sh
 bin/comp_hm_mobile.sh
 bin/comp_mobile3.sh
