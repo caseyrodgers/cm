e727ebc7b748cee57e9563a751422b83382a328a
Bob Hall <rfhall3@yahoo.com>
2014-10-10 07:33:22 +0000

    CM-970: LoginCodeCheckServlet, initial commit

---
+++ src/main/java/hotmath/cm/signup/LoginCodeCheckServlet.java
@@ -0,0 +1,96 @@
+package hotmath.cm.signup;
+
+import hotmath.gwt.cm_admin.server.model.CmAdminDao;
+import hotmath.subscriber.HotMathSubscriber;
+import hotmath.subscriber.HotMathSubscriberManager;
+import hotmath.subscriber.HotMathSubscriberSignupInfo;
+import hotmath.testset.ha.HaAdmin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+public class LoginCodeCheckServlet extends CatchupSignupServlet {
+    
+	private static final long serialVersionUID = 632874215893149960L;
+
+	static Logger _logger = Logger.getLogger(LoginCodeCheckServlet.class.getName());
+
+    public LoginCodeCheckServlet() {
+    	/* empty */
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        _logger.info("Attempting to check a login code for: " + req.getRemoteAddr());
+        
+        HotMathSubscriber subscriber = null;
+        try {
+            /** Extract the data from the request 
+             * 
+             */
+            boolean isEmailRequired = false;
+            HotMathSubscriberSignupInfo sifo = getSignupInfo(req, isEmailRequired);
+            String subscriberId = sifo.getSubscriberId();
+
+            subscriber = HotMathSubscriberManager.findSubscriber(sifo.getSubscriberId());
+            boolean isPilot = true;
+            if (subscriber.getService("catchup") == null) {
+            	// not a CM subscriber
+            	isPilot = false;
+            }
+
+            if (isPilot == true) {
+            	// locate admin record
+            	HaAdmin admin = null;
+            	try {
+                	admin = CmAdminDao.getInstance().getAdmin(subscriberId);
+            	}
+            	catch(Exception e) {}
+
+            	if (admin == null) isPilot = false;
+            }
+
+            String maskedEmail = maskEmail(subscriber.getEmail());
+            String msg = String.format("{isPilot:'%s', email:'%s'}",  isPilot, maskedEmail);
+            resp.getWriter().write(msg);
+
+        } catch (Exception e) {
+            _logger.error("*** Error checking login code", e);
+            try {
+                if (subscriber != null) subscriber.addComment(e.getMessage());
+            } catch (Exception ee) {
+                _logger.error("*** Error adding comment", ee);
+            }
+            
+            resp.getWriter().write("{error:'" + e.getMessage() +"'}");
+        }
+    }
+
+	private String maskEmail(String email) {
+
+		if (email == null || email.trim().length() < 10) return email;
+		email = email.trim();
+		int offset = email.indexOf("@");
+		char[] maskedEmail = new char[email.length()];
+		char[] origEmail = email.toCharArray();
+		maskedEmail[0] = origEmail[0];
+		for (int i=1; i<offset-1; i++) {
+			maskedEmail[i] = '*';
+		}
+		for (int i=offset-1; i<email.length(); i++) {
+			maskedEmail[i] = origEmail[i];
+		}
+		return new String(maskedEmail);
+	}
+
+}
