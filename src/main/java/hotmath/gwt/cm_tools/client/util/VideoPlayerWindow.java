dbc0e687f5f44092ac8268f2615baa7742587c17
Bob Hall <rfhall3@yahoo.com>
2014-10-28 07:03:53 +0000

    CM-976: play Assignments videos in a GWindow.

--- src/main/java/hotmath/gwt/cm_tools/client/util/VideoPlayerWindow.java
+++ src/main/java/hotmath/gwt/cm_tools/client/util/VideoPlayerWindow.java
@@ -1,6 +1,9 @@
 package hotmath.gwt.cm_tools.client.util;
 
 import com.google.gwt.user.client.ui.HTML;
+import com.sencha.gxt.core.client.dom.ScrollSupport.ScrollMode;
+import com.sencha.gxt.widget.core.client.FramedPanel;
+import com.sencha.gxt.widget.core.client.container.FlowLayoutContainer;
 
 import hotmath.gwt.cm_core.client.util.CmAlertify;
 import hotmath.gwt.cm_tools.client.ui.GWindow;
@@ -18,21 +21,26 @@ public class VideoPlayerWindow extends GWindow {
 
 		super(true);
 
-		title = (title != null) ? title : "Catchup Math Video";
+		title = (title != null) ? title : "Catchup Math";
 		setHeadingText(title);
-		setPixelSize(640, 500);
+		setPixelSize(650, 500);
 		setResizable(false);
 		setModal(true);
 
 		if (videoURI != null) {
-			String htmlStr = HTML_PART_1 + videoURI + HTML_PART_2;
-			HTML html = new HTML(htmlStr);
-	        
-	        setWidget(html);
+	        FlowLayoutContainer flow = new FlowLayoutContainer();
+	        flow.setScrollMode(ScrollMode.NONE);
+	        flow.add(new HTML(HTML_PART_1 + videoURI + HTML_PART_2));
+
+	        FramedPanel fp = new FramedPanel();
+	        fp.setHeaderVisible(false);
+	        fp.setWidget(flow);
+
+	        setWidget(fp);
 	        setVisible(true);
 		}
 		else {
-			new CmAlertify().alert("Invalid resource for " + title + " video.");
+			new CmAlertify().alert("Invalid resource for " + title + " Video.");
 		}
 	}
 }
