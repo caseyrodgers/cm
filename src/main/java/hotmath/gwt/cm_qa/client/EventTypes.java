019ef3defecdef22d6fa914b0e7b8345d3813a5e
Casey Rodgers <grodgers@gmail.com>
2014-11-04 04:00:24 +0000

    *** empty log message ***

--- src/main/java/hotmath/gwt/cm_qa/client/EventTypes.java
+++
@@ -1,7 +0,0 @@
-package hotmath.gwt.cm_qa.client;
-
-import hotmath.gwt.cm_core.client.EventType;
-
-public class EventTypes {
-    static public EventType CATEGORY_CHANGED = new EventType("CATEGORY_CHANGED");
-}
