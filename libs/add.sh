affc17ce17039f913e2af23f159756f1bfc450d4
Casey Rodgers <grodgers@gmail.com>
2014-11-21 05:14:08 +0000

    *** empty log message ***

--- libs/add.sh
+++ libs/add.sh
@@ -32,7 +32,9 @@ mvn install:install-file -DgroupId=com.bramosystems.oss.player -DartifactId=bst-
 mvn install:install-file -DgroupId=htmlparser -DartifactId=htmlparser -Dversion=1.6 -Dpackaging=jar -Dfile=htmlparser.jar -DgeneratePom=true
 mvn install:install-file -DgroupId=org.json -DartifactId=json -Dversion=1.0 -Dpackaging=jar -Dfile=json-1.0.jar -DgeneratePom=true
 mvn install:install-file -DgroupId=sbserver -DartifactId=sbserver -Dversion=2.0 -Dpackaging=jar -Dfile=sqlkitchen_server.jar -DgeneratePom=true
+
 mvn install:install-file -DgroupId=gwt2swf -DartifactId=gwt2swf -Dversion=0.6.1 -Dpackaging=jar -Dfile=gwt2swf-0.6.1.jar -DgeneratePom=true
+
 mvn install:install-file -DgroupId=jade -DartifactId=jade -Dversion=1.0 -Dpackaging=jar -Dfile=jade.jar -DgeneratePom=true
 mvn install:install-file -DgroupId=gwt-log -DartifactId=gwt-log -Dversion=3.1.7 -Dpackaging=jar -Dfile=gwt-log-3.1.7.jar  -DgeneratePom=true
 mvn install:install-file -DgroupId=itext -DartifactId=iText -Dversion=2.1.6 -Dpackaging=jar -Dfile=iText-2.1.6.jar -DgeneratePom=true
