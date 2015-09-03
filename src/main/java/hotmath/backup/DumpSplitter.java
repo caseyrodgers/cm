32f64df288307f8e99f85b469fcada432cb17229
Hotmath <root@hotmath.com>
2015-09-02 23:54:19 -0700

    output newline

--- src/main/java/hotmath/backup/DumpSplitter.java
+++ src/main/java/hotmath/backup/DumpSplitter.java
@@ -11,17 +11,17 @@ import com.google.common.io.Files;
 
 /** Take an mysql dump file creates individial files
  *  for each table.
- *  
+ *
  * @author casey
  *
  */
 public class DumpSplitter {
-    
+
     Logger logger = Logger.getLogger(DumpSplitter.class);
     FileWriter fileWriter;
     public DumpSplitter(String dumpFile) throws Exception {
         File outputHead = new File("dump_splitter");
-        
+
         if(outputHead.exists()) {
             Files.deleteRecursively(outputHead);
         }
@@ -32,23 +32,23 @@ public class DumpSplitter {
                 String line = it.nextLine();
                 // do something with line
                 if(line.indexOf("Table structure for table") > -1) {
-                    
+
                     if(fileWriter != null) {
                         logger.info("closing table");
                         fileWriter.close();
                     }
-                    
+
                     String tableName = line.split("`")[1];
                     logger.info("New table: " + tableName);
-                    
+
                     fileWriter = new FileWriter(new File(outputHead, tableName + ".dump"));
                 }
-                
+
                 if(fileWriter != null) {
-                    fileWriter.write(line);
+                    fileWriter.write(line + "\n");
                 }
             }
-        } 
+        }
         catch(Exception e) {
             logger.error("Error", e);
         }
@@ -59,8 +59,8 @@ public class DumpSplitter {
             LineIterator.closeQuietly(it);
         }
     }
-    
-    
+
+
     static public void main(String as[]) {
         try {
             new DumpSplitter(as[0]);
@@ -68,8 +68,8 @@ public class DumpSplitter {
         catch(Exception e) {
             e.printStackTrace();
         }
-        
+
         System.out.println("Dump Complete");
     }
-    
+
 }
