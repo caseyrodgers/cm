a5161bf9aa4730a122338e26bfe9b1fc89301de4
casey <caseyrodgers@gmail.com>
2016-05-10 13:25:19 -0700

    updated rule

--- src/main/java/hotmath/mathml/MathMlTransform.java
+++ src/main/java/hotmath/mathml/MathMlTransform.java
@@ -43,7 +43,6 @@ public class MathMlTransform {
 			   MathTemplates.MsupWithExactlyMiMn,
 			   MathTemplates.MsupWithExactlyMnMi,
 			   MathTemplates.MsupMi,
-			   MathTemplates.EveryMnMi,
 			   MathTemplates.AllMi
 	};
 
