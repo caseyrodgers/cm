c4c3c8b5f714a6281a55e2e434bd7257e8a8f57c
casey <caseyrodgers@gmail.com>
2016-05-10 10:39:17 -0700

    added the Every MnMi rule

--- src/main/java/hotmath/mathml/MathMlTransform.java
+++ src/main/java/hotmath/mathml/MathMlTransform.java
@@ -43,6 +43,7 @@ public class MathMlTransform {
 			   MathTemplates.MsupWithExactlyMiMn,
 			   MathTemplates.MsupWithExactlyMnMi,
 			   MathTemplates.MsupMi,
+			   MathTemplates.EveryMnMi,
 			   MathTemplates.AllMi
 	};
 
