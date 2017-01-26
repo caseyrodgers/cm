ad4843c3f8dd6a0e7c57cf88551d8d9f58b2b6ea
bobhall <rfhall3@yahoo.com>
2017-01-26 00:28:28 -0800

    CM-1229: add Pre-algebra activities page

---
+++ src/main/webapp/assets/js/prealgebra-activities.js
@@ -0,0 +1,43 @@
+function setupPreAlgActivities() {
+   new YAHOO.widget.Tooltip("algebra", {
+        context:"algebratiles",  
+        text:'<p class="tooltip_2" >Write an expression for the perimeter in terms of <i>x</i>. Evaluate the expression when <i>x</i> equals a number. Use the slider to check your result.</p>', 
+        showDelay:400 }
+   );
+   new YAHOO.widget.Tooltip("acrobat", {
+        context:"acrobatnumberline",  
+        text:'<p class="tooltip_2" >Help Cecil the Acrobat cross the tight rope to the ladder by adding integers. Click on the arrows to create a sum.</p>', 
+        showDelay:400 }
+   );
+   new YAHOO.widget.Tooltip("circle", {
+        context:"circlearea",  
+        text:'<p class="tooltip_2" >Decompose a circle into an even number of sectors, and fit them together to make a shape that approximates a parallelogram. As you use a greater number of pieces, the shape starts to approximate a rectangle, and you can see why the formula for the area of a circle works.</p>',
+        showDelay:400 }
+   );
+   new YAHOO.widget.Tooltip("data", {
+       context:"histogram",  
+       text:'<p class="tooltip_2" >Enter a set of data. Then explore various representations of a data: as a histogram, a box-and-whisker plot, and a stem-and-leaf plot.</p>',
+       showDelay:400 }
+  );
+   new YAHOO.widget.Tooltip("area", {
+       context:"areadecomposer",  
+       text:'<p class="tooltip_2" >Use the tools to cut the shape into pieces, and then rearrange them to find new ways to think about area.</p>',
+       showDelay:400 }
+  );     
+   new YAHOO.widget.Tooltip("fractions", {
+       context:"fractech",  
+       text:'<p class="tooltip_2" >Add fractions together and measure them on a number line. Change the partition of the number line to find the exact value of the sum.</p>',
+       showDelay:400 }
+  );     
+   new YAHOO.widget.Tooltip("key", {
+       context:"keylock",  
+       text:'<p class="tooltip_2" >Use translations, rotations, and reflections to line up a key with a lock.</p>',
+       showDelay:400 }
+  );     
+   new YAHOO.widget.Tooltip("frog", {
+       context:"hoptoit",  
+       text:'<p class="tooltip_2" >Add integers to move the frog on the number line.</p>',
+       showDelay:400 }
+  );     
+	   
+}
