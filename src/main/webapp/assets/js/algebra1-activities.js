5584d3fe24ee9bd422e0fc3a9b33fe66794f6373
bobhall <rfhall3@yahoo.com>
2017-02-01 00:49:17 -0800

    CM-1229: add Algebra 1 activities page

---
+++ src/main/webapp/assets/js/algebra1-activities.js
@@ -0,0 +1,78 @@
+function setupAlg1Activities() {
+   new YAHOO.widget.Tooltip("exponential", {
+        context:"exponential",  
+        text:'<p class="tooltip_2" >Explore the effects of the parameters a, b and c on the graph of the exponential function.</p>', 
+        showDelay:400 }
+   );
+   new YAHOO.widget.Tooltip("exponential_logarithmic", {
+        context:"exponential_logarithmic",  
+        text:'<p class="tooltip_2" >Explore the effect of the parameter b on the graph of the exponential function: y = b<sup>x</sup> and logarithmic function: y = log<sub>b</sub> x.</p>', 
+        showDelay:400 }
+   );
+   new YAHOO.widget.Tooltip("quadratics", {
+        context:"quadratics",  
+        text:'<p class="tooltip_2" >Explore the graphs of quadratic equations in factored form, y = a(x - r1)(x - r2).',
+        showDelay:400 }
+   );
+   new YAHOO.widget.Tooltip("diamond", {
+       context:"diamond",  
+       text:'<p class="tooltip_2" >Use these flash cards to practice diamond problems. The number at the top of the diamond is the product, the number at the bottom is the sum.</p>',
+       showDelay:400 }
+  );
+  new YAHOO.widget.Tooltip("parabolas", {
+       context:"parabolas",  
+       text:'<p class="tooltip_2" >Investigate the vertex form of the equation for a parabola.</p>',
+       showDelay:400 }
+  );
+  new YAHOO.widget.Tooltip("guess", {
+       context:"guess",  
+       text:'<p class="tooltip_2" >Enter x-values to find the corresponding y-values. Then guess the function rule.</p>',
+       showDelay:400 }
+  );
+  new YAHOO.widget.Tooltip("relations_functions", {
+       context:"relations_functions",  
+       text:'<p class="tooltip_2" >Investigate when a relation is a function.</p>',
+       showDelay:400 }
+  );
+  new YAHOO.widget.Tooltip("transforming_functions", {
+       context:"transforming_functions",  
+       text:'<p class="tooltip_2" >Explore translations, stretches, and &ldquo;squishes&rdquo; of quadratic, trigonometric, and absolute value functions.</p>',
+       showDelay:400 }
+  );
+  new YAHOO.widget.Tooltip("function_machine", {
+       context:"function_machine",  
+       text:'<p class="tooltip_2" >Enter a function rule, and explore the resulting table of values and graph.</p>',
+       showDelay:400 }
+  );
+  new YAHOO.widget.Tooltip("inequalities", {
+       context:"inequalities",  
+       text:'<p class="tooltip_2" >Investigate the relationship between the absolute value function and inequalities.</p>',
+       showDelay:400 }
+  );
+  new YAHOO.widget.Tooltip("inequality_grapher", {
+       context:"inequality_grapher",  
+       text:'<p class="tooltip_2" >Investigate the graphs of inequalities in two variables.</p>',
+       showDelay:400 }
+  );
+  new YAHOO.widget.Tooltip("equating_expressions", {
+       context:"equating_expressions",  
+       text:'<p class="tooltip_2" >Investigate when two expressions in x are equal.</p>',
+       showDelay:400 }
+  );
+  new YAHOO.widget.Tooltip("scientific_notation", {
+       context:"scientific_notation",  
+       text:"<p class='tooltip_2' >Investigate scientific notation. Move a number's decimal point to change its order of magnitude.</p>",
+       showDelay:400 }
+  );
+  new YAHOO.widget.Tooltip("slope_intercept", {
+       context:"slope_intercept",  
+       text:'<p class="tooltip_2" >Explore the graphs of linear equations in slope-intercept form.</p>',
+       showDelay:400 }
+  );
+  new YAHOO.widget.Tooltip("function_grapher", {
+       context:"function_grapher",  
+       text:'<p class="tooltip_2" >Use this tool to graph lines, polynomial functions, inequalities, trig functions, and more!</p>',
+       showDelay:400 }
+  );
+  
+}
